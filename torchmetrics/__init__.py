f3747e36d08b3ec2ad417312538b9fb68a22c2e0
Jirka Borovec <jirka.borovec@seznam.cz>
2021-01-05 09:47:16 +0100

    update install & tests & requirements
    * update requirements
    
    * tests
    
    * install
    
    * CI
    
    (cherry picked from commit bdab86dda609f82c922363a1a17469e5f43fa8f1)

--- torchmetrics/__init__.py
+++ torchmetrics/__init__.py
@@ -10,37 +10,47 @@ __docs__ = "PyTorch Lightning Sample project."
 __long_doc__ = """
 What is it?
 -----------
-This is starter project template which shall simplify initial steps for each new PL project...
-
-Except the implemented sections:
- - sample package
- - setting CI
- - setting Docs
+metrics...
 """
 
-from torchmetrics.classification import (  # noqa: F401
-    Accuracy,
-    AUC,
-    AUROC,
-    AveragePrecision,
-    ConfusionMatrix,
-    F1,
-    FBeta,
-    HammingDistance,
-    IoU,
-    Precision,
-    PrecisionRecallCurve,
-    Recall,
-    ROC,
-    StatScores,
-)
-from torchmetrics.metric import Metric, MetricCollection  # noqa: F401
-from torchmetrics.regression import (  # noqa: F401
-    ExplainedVariance,
-    MeanAbsoluteError,
-    MeanSquaredError,
-    MeanSquaredLogError,
-    PSNR,
-    R2Score,
-    SSIM,
-)
+try:
+    # This variable is injected in the __builtins__ by the build
+    # process. It used to enable importing subpackages of skimage when
+    # the binaries are not built
+    _ = None if __LIGHTNING_SETUP__ else None
+except NameError:
+    __LIGHTNING_SETUP__: bool = False
+
+if __LIGHTNING_SETUP__:
+    import sys  # pragma: no-cover
+
+    sys.stdout.write(f'Partial import of `{__name__}` during the build process.\n')  # pragma: no-cover
+    # We are not importing the rest of the lightning during the build process, as it may not be compiled yet
+else:
+
+    from torchmetrics.classification import (  # noqa: F401
+        Accuracy,
+        AUC,
+        AUROC,
+        AveragePrecision,
+        ConfusionMatrix,
+        F1,
+        FBeta,
+        HammingDistance,
+        IoU,
+        Precision,
+        PrecisionRecallCurve,
+        Recall,
+        ROC,
+        StatScores,
+    )
+    from torchmetrics.metric import Metric, MetricCollection  # noqa: F401
+    from torchmetrics.regression import (  # noqa: F401
+        ExplainedVariance,
+        MeanAbsoluteError,
+        MeanSquaredError,
+        MeanSquaredLogError,
+        PSNR,
+        R2Score,
+        SSIM,
+    )
