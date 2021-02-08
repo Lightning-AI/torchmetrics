5ead53dff1aed591db5d8f1a790993cb633e8c7a
Teddy Koker <teddy.koker@gmail.com>
2021-02-08 12:18:59 -0500

    init

--- torchmetrics/__init__.py
+++ torchmetrics/__init__.py
@@ -1,39 +1,51 @@
-# Copyright The PyTorch Lightning team.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-from torchmetrics.classification import (  # noqa: F401
-    Accuracy,
-    AUC,
-    AUROC,
-    AveragePrecision,
-    ConfusionMatrix,
-    F1,
-    FBeta,
-    HammingDistance,
-    IoU,
-    Precision,
-    PrecisionRecallCurve,
-    Recall,
-    ROC,
-    StatScores,
-)
-from torchmetrics.metric import Metric, MetricCollection  # noqa: F401
-from torchmetrics.regression import (  # noqa: F401
-    ExplainedVariance,
-    MeanAbsoluteError,
-    MeanSquaredError,
-    MeanSquaredLogError,
-    PSNR,
-    R2Score,
-    SSIM,
-)
+
+"""Root package info."""
+
+__version__ = '0.0.0rc1'
+__author__ = 'PyTorchLightning et al.'
+__author_email__ = 'name@pytorchlightning.ai'
+__license__ = 'TBD'
+__copyright__ = 'Copyright (c) 2020-2020, %s.' % __author__
+__homepage__ = 'https://github.com/PyTorchLightning/torchmetrics'
+__docs__ = "PyTorch Lightning Sample project."
+__long_doc__ = """
+What is it?
+-----------
+metrics...
+"""
+
+try:
+    # This variable is injected in the __builtins__ by the build
+    # process. It used to enable importing subpackages of skimage when
+    # the binaries are not built
+    _ = None if __LIGHTNING_SETUP__ else None
+except NameError:
+    __LIGHTNING_SETUP__: bool = False
+
+if __LIGHTNING_SETUP__:
+    import sys  # pragma: no-cover
+
+    sys.stdout.write(f'Partial import of `{__name__}` during the build process.\n')  # pragma: no-cover
+    # We are not importing the rest of the lightning during the build process, as it may not be compiled yet
+else:
+
+    from torchmetrics.classification import (  # noqa: F401
+        F,
+        ROC,
+        Accuracy,
+        AveragePrecision,
+        ConfusionMatrix,
+        FBeta,
+        Precision,
+        PrecisionRecallCurve,
+        Recall,
+    )
+    from torchmetrics.metric import Metric  # noqa: F401
+    from torchmetrics.regression import (  # noqa: F401
+        PSNR,
+        SSIM,
+        ExplainedVariance,
+        MeanAbsoluteError,
+        MeanSquaredError,
+        MeanSquaredLogError,
+    )1
