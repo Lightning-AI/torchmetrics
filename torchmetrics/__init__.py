d946faec9dc04a5570ef3f1c96955303c3c77723
Jirka Borovec <jirka.borovec@seznam.cz>
2021-03-11 00:56:25 +0100

    v0.2.0rc2

--- torchmetrics/__init__.py
+++ torchmetrics/__init__.py
@@ -2,7 +2,7 @@
 import logging as __logging
 import os
 
-__version__ = '0.2.0rc1'
+__version__ = '0.2.0rc2'
 __author__ = 'PyTorchLightning et al.'
 __author_email__ = 'name@pytorchlightning.ai'
 __license__ = 'Apache-2.0'
