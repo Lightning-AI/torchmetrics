1f623d555204ee9d280930302a9e35c71df62c1e
Nicki Skafte <skaftenicki@gmail.com>
2021-03-14 17:46:35 +0100

    correct naming

--- tests/classification/test_cohen_kappa.py
+++ tests/classification/test_cohen_kappa.py
@@ -19,56 +19,56 @@ from torchmetrics.functional.classification.cohen_kappa import cohen_kappa
 torch.manual_seed(42)
 
 
-def _sk_cm_binary_prob(preds, target, weights=None):
+def _sk_cohen_kappa_binary_prob(preds, target, weights=None):
     sk_preds = (preds.view(-1).numpy() >= THRESHOLD).astype(np.uint8)
     sk_target = target.view(-1).numpy()
 
     return sk_cohen_kappa(y1=sk_target, y2=sk_preds, weights=weights)
 
 
-def _sk_cm_binary(preds, target, weights=None):
+def _sk_cohen_kappa_binary(preds, target, weights=None):
     sk_preds = preds.view(-1).numpy()
     sk_target = target.view(-1).numpy()
 
     return sk_cohen_kappa(y1=sk_target, y2=sk_preds, weights=weights)
 
 
-def _sk_cm_multilabel_prob(preds, target, weights=None):
+def _sk_cohen_kappa_multilabel_prob(preds, target, weights=None):
     sk_preds = (preds.view(-1).numpy() >= THRESHOLD).astype(np.uint8)
     sk_target = target.view(-1).numpy()
 
     return sk_cohen_kappa(y1=sk_target, y2=sk_preds, weights=weights)
 
 
-def _sk_cm_multilabel(preds, target, weights=None):
+def _sk_cohen_kappa_multilabel(preds, target, weights=None):
     sk_preds = preds.view(-1).numpy()
     sk_target = target.view(-1).numpy()
 
     return sk_cohen_kappa(y1=sk_target, y2=sk_preds, weights=weights)
 
 
-def _sk_cm_multiclass_prob(preds, target, weights=None):
+def _sk_cohen_kappa_multiclass_prob(preds, target, weights=None):
     sk_preds = torch.argmax(preds, dim=len(preds.shape) - 1).view(-1).numpy()
     sk_target = target.view(-1).numpy()
 
     return sk_cohen_kappa(y1=sk_target, y2=sk_preds, weights=weights)
 
 
-def _sk_cm_multiclass(preds, target, weights=None):
+def _sk_cohen_kappa_multiclass(preds, target, weights=None):
     sk_preds = preds.view(-1).numpy()
     sk_target = target.view(-1).numpy()
 
     return sk_cohen_kappa(y1=sk_target, y2=sk_preds, weights=weights)
 
 
-def _sk_cm_multidim_multiclass_prob(preds, target, weights=None):
+def _sk_cohen_kappa_multidim_multiclass_prob(preds, target, weights=None):
     sk_preds = torch.argmax(preds, dim=len(preds.shape) - 2).view(-1).numpy()
     sk_target = target.view(-1).numpy()
 
     return sk_cohen_kappa(y1=sk_target, y2=sk_preds, weights=weights)
 
 
-def _sk_cm_multidim_multiclass(preds, target, weights=None):
+def _sk_cohen_kappa_multidim_multiclass(preds, target, weights=None):
     sk_preds = preds.view(-1).numpy()
     sk_target = target.view(-1).numpy()
 
@@ -78,21 +78,21 @@ def _sk_cm_multidim_multiclass(preds, target, weights=None):
 @pytest.mark.parametrize("weights", ['linear', 'quadratic', None])
 @pytest.mark.parametrize(
     "preds, target, sk_metric, num_classes",
-    [(_input_binary_prob.preds, _input_binary_prob.target, _sk_cm_binary_prob, 2),
-     (_input_binary.preds, _input_binary.target, _sk_cm_binary, 2),
-     (_input_mlb_prob.preds, _input_mlb_prob.target, _sk_cm_multilabel_prob, 2),
-     (_input_mlb.preds, _input_mlb.target, _sk_cm_multilabel, 2),
-     (_input_mcls_prob.preds, _input_mcls_prob.target, _sk_cm_multiclass_prob, NUM_CLASSES),
-     (_input_mcls.preds, _input_mcls.target, _sk_cm_multiclass, NUM_CLASSES),
-     (_input_mdmc_prob.preds, _input_mdmc_prob.target, _sk_cm_multidim_multiclass_prob, NUM_CLASSES),
-     (_input_mdmc.preds, _input_mdmc.target, _sk_cm_multidim_multiclass, NUM_CLASSES)]
+    [(_input_binary_prob.preds, _input_binary_prob.target, _sk_cohen_kappa_binary_prob, 2),
+     (_input_binary.preds, _input_binary.target, _sk_cohen_kappa_binary, 2),
+     (_input_mlb_prob.preds, _input_mlb_prob.target, _sk_cohen_kappa_multilabel_prob, 2),
+     (_input_mlb.preds, _input_mlb.target, _sk_cohen_kappa_multilabel, 2),
+     (_input_mcls_prob.preds, _input_mcls_prob.target, _sk_cohen_kappa_multiclass_prob, NUM_CLASSES),
+     (_input_mcls.preds, _input_mcls.target, _sk_cohen_kappa_multiclass, NUM_CLASSES),
+     (_input_mdmc_prob.preds, _input_mdmc_prob.target, _sk_cohen_kappa_multidim_multiclass_prob, NUM_CLASSES),
+     (_input_mdmc.preds, _input_mdmc.target, _sk_cohen_kappa_multidim_multiclass, NUM_CLASSES)]
 )
-class TestConfusionMatrix(MetricTester):
+class TestCohenKappa(MetricTester):
     atol = 1e-5
 
     @pytest.mark.parametrize("ddp", [True, False])
     @pytest.mark.parametrize("dist_sync_on_step", [True, False])
-    def test_confusion_matrix(self, weights, preds, target, sk_metric, num_classes, ddp, dist_sync_on_step):
+    def test_cohen_kappa(self, weights, preds, target, sk_metric, num_classes, ddp, dist_sync_on_step):
         self.run_class_metric_test(
             ddp=ddp,
             preds=preds,
@@ -107,7 +107,7 @@ class TestConfusionMatrix(MetricTester):
             }
         )
 
-    def test_confusion_matrix_functional(self, weights, preds, target, sk_metric, num_classes):
+    def test_cohen_kappa_functional(self, weights, preds, target, sk_metric, num_classes):
         self.run_functional_metric_test(
             preds,
             target,
