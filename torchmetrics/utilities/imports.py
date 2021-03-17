ec788065e9a4d926bed23e0605504c0f69cf972e
Nicki Skafte <skaftenicki@gmail.com>
2021-03-17 15:47:18 +0100

    change compare function

--- torchmetrics/utilities/imports.py
+++ torchmetrics/utilities/imports.py
@@ -12,15 +12,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Import utilities"""
+import importlib
 import operator
 from distutils.version import LooseVersion
+from importlib.util import find_spec
 
-from pkg_resources import DistributionNotFound, get_distribution
+from pkg_resources import DistributionNotFound
+
+
+def _module_available(module_path: str) -> bool:
+    """
+    Check if a path is available in your environment
+    >>> _module_available('os')
+    True
+    >>> _module_available('bla.bla')
+    False
+    """
+    try:
+        return find_spec(module_path) is not None
+    except AttributeError:
+        # Python 3.6
+        return False
+    except ModuleNotFoundError:
+        # Python 3.7+
+        return False
 
 
 def _compare_version(package: str, op, version) -> bool:
+    """Compare package version with some requirements
+    >>> _compare_version("torch", operator.ge, "0.1")
+    True
+    """
+    if not _module_available(package):
+        return False
     try:
-        pkg_version = LooseVersion(get_distribution(package).version)
+        pkg = importlib.import_module(package)
+        assert hasattr(pkg, '__version__')
+        pkg_version = pkg.__version__
         return op(pkg_version, LooseVersion(version))
     except DistributionNotFound:
         return False
