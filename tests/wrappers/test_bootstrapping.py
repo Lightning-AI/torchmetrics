93c30a8a66e2afae32abb3a3ad801cd1212a98d2
Nicki Skafte <skaftenicki@gmail.com>
2021-03-17 17:13:28 +0100

    pep8

--- tests/wrappers/test_bootstrapping.py
+++ tests/wrappers/test_bootstrapping.py
@@ -11,54 +11,86 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import pytest
-from functools import partial
-
 import numpy as np
+import pytest
 import torch
-
-from tests.helpers.testers import MetricTester
-
 from sklearn.metrics import precision_score, recall_score
 
 from torchmetrics.classification import Precision, Recall
-from torchmetrics.wrappers.bootstrapping import BootStrapper
-
-_ = torch.manual_seed(0)
+from torchmetrics.utilities import _TORCH_GREATER_EQUAL_1_7, apply_to_collection
+from torchmetrics.wrappers.bootstrapping import BootStrapper, _bootstrap_sampler
 
 _preds = torch.randint(10, (10, 32))
 _target = torch.randint(10, (10, 32))
 
-def _sk_bootstrap(preds, target, func, num_bootstraps=10):
-    preds = preds.numpy()
-    target = target.numpy()
-    
-    scores = [ ]
-    for i in range(num_bootstraps):
-        idx = torch.multinomial(torch.ones(preds.shape[0]), num_samples=preds.shape[0], replacement=True)
-        print('numpy', idx)
-        preds_idx = preds[idx]
-        target_idx = target[idx]
-        scores.append(func(target_idx, preds_idx, average='micro'))
-    scores = np.stack(scores)
-    return [scores.mean(), scores.std()]
 
-@pytest.mark.parametrize("metric, sk_metric", [
-    [Precision(), precision_score],
-    [Recall(), recall_score],
-])
-class TestBootStrapper(MetricTester):
+class TestBootStrapper(BootStrapper):
+    """ For testing purpose, we subclass the bootstrapper class so we can get the exact permutation
+        the class is creating
+    """
 
-    @pytest.mark.parametrize("ddp", [True, False])
-    @pytest.mark.parametrize("dist_sync_on_step", [True, False])
-    def test_bootstrapper(self, metric, sk_metric, ddp, dist_sync_on_step):
-        self.run_class_metric_test(
-            ddp,
-            _preds,
-            _target,
-            metric_class=partial(BootStrapper, base_metric=metric),
-            sk_metric=partial(_sk_bootstrap, func=sk_metric),
-            dist_sync_on_step=dist_sync_on_step,
+    def update(self, *args, **kwargs):
+        self.out = []
+        for idx in range(self.num_bootstraps):
+            new_args = apply_to_collection(args, torch.Tensor, _bootstrap_sampler, generator=self.generator)
+            new_kwargs = apply_to_collection(kwargs, torch.Tensor, _bootstrap_sampler, generator=self.generator)
+            self.metrics[idx].update(*new_args, **new_kwargs)
+            self.out.append(new_args)
+
+
+def test_bootstrap_sampler():
+    """ make sure that the bootstrap sampler works as intended """
+    old_samples = torch.randn(5, 2)
+
+    # make sure that the new samples are only made up of old samples
+    new_samples = _bootstrap_sampler(old_samples)
+    for ns in new_samples:
+        assert ns in old_samples
+
+    # make sure some samples are also sampled twice
+    found_one = False
+    for os in old_samples:
+        cond = os == new_samples
+        print(cond.sum())
+        if cond.sum() > 2:
+            found_one = True
+    assert found_one, "resampling did not work because no samples were sampled twice"
+
+
+@pytest.mark.parametrize(
+    "metric, sk_metric", [[Precision(average='micro'), precision_score], [Recall(average='micro'), recall_score]]
+)
+def test_bootstrap(metric, sk_metric):
+    """ Test that the different bootstraps gets updated as we expected and that the compute method works """
+    bootstrapper = TestBootStrapper(metric)
+
+    collected_preds = [[] for _ in range(10)]
+    collected_target = [[] for _ in range(10)]
+    for p, t in zip(_preds, _target):
+        bootstrapper.update(p, t)
+
+        for i, o in enumerate(bootstrapper.out):
+
+            collected_preds[i].append(o[0])
+            collected_target[i].append(o[1])
+
+    collected_preds = [torch.cat(cp) for cp in collected_preds]
+    collected_target = [torch.cat(ct) for ct in collected_target]
+
+    sk_scores = [sk_metric(ct, cp, average='micro') for ct, cp in zip(collected_target, collected_preds)]
+
+    # quantile only avaible for pytorch v1.7 and forward
+    if _TORCH_GREATER_EQUAL_1_7:
+        pl_mean, pl_std, pl_quantile, pl_raw = bootstrapper.compute(
+            mean=True, std=True, quantile=torch.tensor([0.05, 0.95]), raw=True
         )
+        assert np.allclose(pl_quantile[0], np.quantile(sk_scores, 0.05))
+        assert np.allclose(pl_quantile[1], np.quantile(sk_scores, 0.95))
+    else:
+        pl_mean, pl_std, pl_raw = bootstrapper.compute(mean=True, std=True, raw=True)
 
-    
+    assert np.allclose(pl_mean, np.mean(sk_scores))
+    import pdb
+    pdb.set_trace()
+    assert np.allclose(pl_std, np.std(sk_scores, ddof=1))
+    assert np.allclose(pl_raw, sk_scores)
--- torchmetrics/wrappers/bootstrapping.py
+++ torchmetrics/wrappers/bootstrapping.py
@@ -11,49 +11,69 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Any, Optional, Union, List, Callable
 from copy import deepcopy
+from typing import Any, Callable, List, Optional, Union
 
 import torch
 from torch import nn
 
 from torchmetrics.metric import Metric
-from torchmetrics.utilities import apply_to_collection
+from torchmetrics.utilities import _TORCH_GREATER_EQUAL_1_7, apply_to_collection
 
 
-def _bootstrap_sampler(tensor: torch.Tensor, size: Optional[int] = None) -> torch.Tensor:
-    """  """
+def _bootstrap_sampler(
+        tensor: torch.Tensor,
+        size: Optional[int] = None,
+        generator: Optional[torch.Generator] = None
+) -> torch.Tensor:
+    """ Resample a tensor along its first dimension with replacement
+    Args:
+        tensor: tensor to resample
+        size: number of samples in new tensor. Defauls to same size as input tensor
+        generator: a instance of ``torch.Generator`` that controls the sampling
+
+    Returns:
+        resampled tensor
+
+    """
     if size is None:
         size = tensor.shape[0]
     idx = torch.multinomial(
         torch.ones(tensor.shape[0], device=tensor.device),
         num_samples=size,
-        replacement=True
+        replacement=True,
+        generator=generator
     )
-    print('pytorch', idx)
     return tensor[idx]
 
 
 class BootStrapper(Metric):
-    def __init__(self, base_metric: Metric, 
-                 num_bootstraps: int = 10,
-                 compute_on_step: bool = True,
-                 dist_sync_on_step: bool = False,
-                 process_group: Optional[Any] = None,
-                 dist_sync_fn: Callable = None
+    def __init__(
+            self,
+            base_metric: Metric,
+            num_bootstraps: int = 10,
+            generator: Optional[torch.Generator] = None,
+            compute_on_step: bool = True,
+            dist_sync_on_step: bool = False,
+            process_group: Optional[Any] = None,
+            dist_sync_fn: Callable = None
     ) -> None:
-        """ 
+        """
         Use to turn a metric into a bootstrapped metric that can automate the process of getting confidence
         intervals for metric values. This wrapper class basically keeps multiple copies of the same base metric
         in memory and whenever ``update`` or ``forward`` is called, all input tensors are resampled
         (with replacement) along the first dimension.
-     
+
         .. note:: Different from all other metrics, bootstrapped metrics has additional
             arguments in its ``compute``  method determining what should be returned.
-        
+
         Args:
-            base_metric: 
+            base_metric:
                 base metric class to wrap
+            num_bootstraps:
+                number of copies to make of the base metric for bootstrapping
+            generator:
+                A pytorch random number generator for the bootstrap sampler
             compute_on_step:
                 Forward only calls ``update()`` and return ``None`` if this is set to ``False``.
             dist_sync_on_step:
@@ -65,7 +85,7 @@ class BootStrapper(Metric):
             dist_sync_fn:
                 Callback that performs the allgather operation on the metric state. When ``None``, DDP
                 will be used to perform the allgather.
-        
+
         Example::
             >>> from torchmetrics.wrappers import BootStrapper
             >>> from torchmetrics import Accuracy
@@ -77,7 +97,7 @@ class BootStrapper(Metric):
             >>> mean, std = output
             >>> print(mean, std)
             tensor(0.4950) tensor(0.1677)
-        
+
         """
         super().__init__(
             compute_on_step,
@@ -87,32 +107,39 @@ class BootStrapper(Metric):
         )
         if not isinstance(base_metric, Metric):
             raise ValueError("Expected base metric to be an instance of torchmetrics.Metric"
-                            f" but received {base_metric}")
+                             f" but received {base_metric}")
 
         self.metrics = nn.ModuleList([deepcopy(base_metric) for _ in range(num_bootstraps)])
         self.num_bootstraps = num_bootstraps
-        
+
+        if generator is not None and not isinstance(generator, torch.Generator):
+            raise ValueError("Expected argument ``generator`` to be an instance of ``torch.Generator``"
+                             f"but received {generator}")
+        self.generator = generator
+
     def update(self, *args, **kwargs):
         """ Updates the state of the base metric. Any tensor passed in will be bootstrapped
         along dimension 0
         """
         for idx in range(self.num_bootstraps):
-            args = apply_to_collection(args, torch.Tensor, _bootstrap_sampler)
-            kwargs = apply_to_collection(kwargs, torch.Tensor, _bootstrap_sampler)
-            self.metrics[idx].update(*args, **kwargs)
+            new_args = apply_to_collection(args, torch.Tensor, _bootstrap_sampler, generator=self.generator)
+            new_kwargs = apply_to_collection(kwargs, torch.Tensor, _bootstrap_sampler, generator=self.generator)
+            self.metrics[idx].update(*new_args, **new_kwargs)
 
     def compute(
-            self, 
-            mean: bool = True, 
-            std: bool = True, 
+            self,
+            mean: bool = True,
+            std: bool = True,
             quantile: Optional[Union[float, torch.Tensor]] = None,
             raw: bool = False
-        ) -> List[torch.Tensor]:
+    ) -> List[torch.Tensor]:
         """ Computes the metric value.
         Args:
             mean: if `True` return the mean of the bootstraps
             std: if `True` return the standard diviation of the bootstraps
-            quantile: if given, returns the quantile of the bootstraps
+            quantile:
+                if given, returns the quantile of the bootstraps. Can only be used when pytorch version
+                1.6 or higher
             raw: if `True`, return all bootstrapped values
         """
         computed_vals = torch.stack([m.compute() for m in self.metrics], dim=0)
@@ -122,6 +149,8 @@ class BootStrapper(Metric):
         if std:
             output.append(computed_vals.std(dim=0))
         if quantile is not None:
+            if not _TORCH_GREATER_EQUAL_1_7:
+                raise ValueError('quantial argument can only be used with pytorch v1.6 or higher')
             output.append(torch.quantile(computed_vals, quantile))
         if raw:
             output.append(computed_vals)
