7caabbe646637d1b13f962e674bae3bf6d47d6d5
Nicki Skafte <skaftenicki@gmail.com>
2021-03-17 17:13:28 +0100

    pep8

--- torchmetrics/utilities/__init__.py
+++ torchmetrics/utilities/__init__.py
@@ -1,3 +1,4 @@
 from torchmetrics.utilities.data import apply_to_collection  # noqa: F401
 from torchmetrics.utilities.distributed import class_reduce, reduce  # noqa: F401
+from torchmetrics.utilities.imports import _TORCH_GREATER_EQUAL_1_6, _TORCH_GREATER_EQUAL_1_7  # noqa: F401
 from torchmetrics.utilities.prints import rank_zero_debug, rank_zero_info, rank_zero_warn  # noqa: F401
