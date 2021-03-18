93f00b1d164f14936eeb975799523e6c018b223e
Jirka Borovec <jirka.borovec@seznam.cz>
2021-03-19 00:21:06 +0100

    importing

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import importlib
 import os
 import sys
 
@@ -10,7 +9,8 @@ _PATH_ROOT = os.path.realpath(os.path.dirname(__file__))
 try:
     from torchmetrics import info, setup_tools
 except ImportError:
-    sys.path.append(os.path.join(_PATH_ROOT, "torchmetrics"))
+    sys.path.append(os.path.join(_PATH_ROOT, "torchmetrics", "info.py"))
+    sys.path.append(os.path.join(_PATH_ROOT, "torchmetrics", "setup_tools.py"))
     import info, setup_tools
 
 long_description = setup_tools._load_readme_description(
