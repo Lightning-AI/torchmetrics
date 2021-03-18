98fc89a9691761a4640453a05582e84f40eabb3b
Jirka Borovec <jirka.borovec@seznam.cz>
2021-03-19 00:51:55 +0100

    flake8

--- torchmetrics/__init__.py
+++ torchmetrics/__init__.py
@@ -2,23 +2,6 @@
 import logging as __logging
 import os
 
-from torchmetrics.info import (
-    __author__,
-    __author_email__,
-    __copyright__,
-    __docs__,
-    __homepage__,
-    __license__,
-    __version__,
-)
-
-_logger = __logging.getLogger("torchmetrics")
-_logger.addHandler(__logging.StreamHandler())
-_logger.setLevel(__logging.INFO)
-
-_PACKAGE_ROOT = os.path.dirname(__file__)
-_PROJECT_ROOT = os.path.dirname(_PACKAGE_ROOT)
-
 from torchmetrics.classification import (  # noqa: F401
     AUC,
     AUROC,
@@ -37,6 +20,15 @@ from torchmetrics.classification import (  # noqa: F401
     StatScores,
 )
 from torchmetrics.collections import MetricCollection  # noqa: F401
+from torchmetrics.info import (  # noqa: F401
+    __author__,
+    __author_email__,
+    __copyright__,
+    __docs__,
+    __homepage__,
+    __license__,
+    __version__,
+)
 from torchmetrics.metric import Metric  # noqa: F401
 from torchmetrics.regression import (  # noqa: F401
     PSNR,
@@ -47,3 +39,10 @@ from torchmetrics.regression import (  # noqa: F401
     MeanSquaredLogError,
     R2Score,
 )
+
+_logger = __logging.getLogger("torchmetrics")
+_logger.addHandler(__logging.StreamHandler())
+_logger.setLevel(__logging.INFO)
+
+_PACKAGE_ROOT = os.path.dirname(__file__)
+_PROJECT_ROOT = os.path.dirname(_PACKAGE_ROOT)
