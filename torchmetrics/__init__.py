b32ad40fbfb4bf5f68326d9635360b00e7fbb05f
Jirka Borovec <jirka.borovec@seznam.cz>
2021-03-19 01:00:08 +0100

    imports

--- torchmetrics/__init__.py
+++ torchmetrics/__init__.py
@@ -2,7 +2,24 @@
 import logging as __logging
 import os
 
-from torchmetrics.classification import (  # noqa: F401
+from torchmetrics.info import (  # noqa: F401
+    __author__,
+    __author_email__,
+    __copyright__,
+    __docs__,
+    __homepage__,
+    __license__,
+    __version__,
+)
+
+_logger = __logging.getLogger("torchmetrics")
+_logger.addHandler(__logging.StreamHandler())
+_logger.setLevel(__logging.INFO)
+
+_PACKAGE_ROOT = os.path.dirname(__file__)
+_PROJECT_ROOT = os.path.dirname(_PACKAGE_ROOT)
+
+from torchmetrics.classification import (  # noqa: F401 E402
     AUC,
     AUROC,
     F1,
@@ -19,18 +36,9 @@ from torchmetrics.classification import (  # noqa: F401
     Recall,
     StatScores,
 )
-from torchmetrics.collections import MetricCollection  # noqa: F401
-from torchmetrics.info import (  # noqa: F401
-    __author__,
-    __author_email__,
-    __copyright__,
-    __docs__,
-    __homepage__,
-    __license__,
-    __version__,
-)
-from torchmetrics.metric import Metric  # noqa: F401
-from torchmetrics.regression import (  # noqa: F401
+from torchmetrics.collections import MetricCollection  # noqa: F401 E402
+from torchmetrics.metric import Metric  # noqa: F401 E402
+from torchmetrics.regression import (  # noqa: F401 E402
     PSNR,
     SSIM,
     ExplainedVariance,
@@ -39,10 +47,3 @@ from torchmetrics.regression import (  # noqa: F401
     MeanSquaredLogError,
     R2Score,
 )
-
-_logger = __logging.getLogger("torchmetrics")
-_logger.addHandler(__logging.StreamHandler())
-_logger.setLevel(__logging.INFO)
-
-_PACKAGE_ROOT = os.path.dirname(__file__)
-_PROJECT_ROOT = os.path.dirname(_PACKAGE_ROOT)
