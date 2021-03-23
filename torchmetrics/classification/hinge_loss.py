d5c8f33b560150cd4eee521a35b7090c43ba2dfa
Ethan Harris <ewah1g13@soton.ac.uk>
2021-03-23 17:13:56 +0000

    Add doc strings

--- torchmetrics/classification/hinge_loss.py
+++ torchmetrics/classification/hinge_loss.py
@@ -20,6 +20,61 @@ from torchmetrics.metric import Metric
 
 
 class HingeLoss(Metric):
+    r"""
+    Computes the mean `Hinge loss <https://en.wikipedia.org/wiki/Hinge_loss>`_, typically used for Support Vector
+    Machines (SVMs). In the binary case it is defined as:
+
+    .. math::
+        \text{Hinge loss} = \max(0, 1 - y \times \hat{y})
+
+    Where :math:`y \in {-1, 1}` is the target, and :math:`\hat{y} \in \mathbb{R}` is the prediction.
+
+    In the multi-class case, when ``multiclass_mode=None`` (default) or ``multiclass_mode="crammer_singer"``, this
+    metric will compute the multi-class hinge loss defined by Crammer and Singer as:
+
+    .. math::
+        \text{Hinge loss} = \max(0, 1 - \hat{y}_y + \max_{i \ne y} \hat{y}_i)
+
+    Where :math:`y \in {0, ..., C}` is the target class, and :math:`\hat{y} \in \mathbb{R}^C` is the predicted output
+    per class.
+
+    In the multi-class case when ``multiclass_mode='one_vs_all'``, this metric will use a one-vs-all approach to compute
+    the hinge loss, giving a vector of C outputs where each entry pits that class against all remaining classes.
+
+    This metric can optionally output the mean of the squared hinge loss by setting ``squared=True``
+
+    Only accepts inputs with preds shape of (N) (binary) or (N, C) (multi-class) and target shape of (N).
+
+    Args:
+        preds: Predictions from model (as float outputs from decision function).
+        target: Ground truth labels.
+        squared:
+            If True, this will compute the squared hinge loss. Otherwise, computes the regular hinge loss (default).
+        multiclass_mode:
+            Which approach to use for multi-class inputs (has no effect in the binary case). ``None`` (default) or
+            ``"crammer_singer"``, uses the Crammer Singer multi-class hinge loss. ``"one_vs_all"`` computes the hinge
+            loss in a one-vs-all fashion.
+
+    Raises:
+        ValueError:
+            If preds shape is not of size (N) or (N, C).
+        ValueError:
+            If target shape is not of size (N).
+        ValueError:
+            If ``multiclass_mode`` is not: None, ``"crammer_singer"``, or ``"one_vs_all"``.
+
+    Example:
+        >>> from torchmetrics.functional import hinge_loss
+        >>> target = torch.tensor([0, 1, 1])
+        >>> preds = torch.tensor([-2.2, 2.4, 0.1])
+        >>> hinge_loss(preds, target)
+        tensor(0.3000)
+
+        >>> target = torch.tensor([0, 1, 2])
+        >>> preds = torch.tensor([[-1.0, 0.9, 0.2], [0.5, -1.1, 0.8], [2.2, -0.5, 0.3]])
+        >>> hinge_loss(preds, target)
+        tensor(2.9000)
+    """
 
     def __init__(
         self,
