fd6b9905fc229d3e7ebb3740513b9cd1a62647f1
Ethan Harris <ewah1g13@soton.ac.uk>
2021-03-25 11:02:55 +0000

    Update torchmetrics/classification/hinge.py
    Co-authored-by: Nicki Skafte <skaftenicki@gmail.com>

--- torchmetrics/classification/hinge.py
+++ torchmetrics/classification/hinge.py
@@ -76,6 +76,8 @@ class Hinge(Metric):
         >>> hinge(preds, target)
         tensor(2.9000)
 
+
+        # multiclass example, one vs all mode
         >>> target = torch.tensor([0, 1, 2])
         >>> preds = torch.tensor([[-1.0, 0.9, 0.2], [0.5, -1.1, 0.8], [2.2, -0.5, 0.3]])
         >>> hinge = Hinge(multiclass_mode="one-vs-all")
