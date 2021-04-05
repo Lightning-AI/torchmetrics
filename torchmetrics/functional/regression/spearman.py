75d180889a5cea87b11651520aea580a217b9812
Nicki Skafte <skaftenicki@gmail.com>
2021-04-05 16:01:40 +0200

    ranking

---
+++ torchmetrics/functional/regression/spearman.py
@@ -0,0 +1,53 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from torch import Tensor
+
+
+def _find_repeats(data: Tensor):
+    temp = data.detach().clone()
+    temp = temp.sort()[0]
+    
+    change = torch.cat([torch.tensor([True]), temp[1:] != temp[:-1]])
+    unique = temp[change]
+    change_idx = torch.cat([torch.nonzero(change), torch.tensor([[n]])]).flatten()
+    freq = change_idx[1:] - change_idx[:-1]
+    atleast2 = freq > 1
+    return unique[atleast2]
+
+def _rank_data(data: Tensor):
+    n = data.numel()
+    rank = torch.empty_like(data)
+    idx = data.argsort()
+    rank[idx[:n]] = torch.arange(1, n+1, dtype=torch.float)
+    
+    repeats = _find_repeats(data)
+    for r in repeats:
+        condition = (data == r).filled(False)
+        rank[condition] = rank[condition].mean()
+    return rank
+
+def _spearman_corrcoef_update(preds: Tensor, target: Tensor):
+
+    
+def _spearman_corrcoef_compute(preds: Tensor, target: Tensor):
+    rank_preds = _rank_data(preds)
+    rank_target = _rank_data(target)
+    
+    cov = ((rank_preds - rank_preds.mean()) * (rank_target - rank_target.mean())).sum()
+    return cov / (rank_preds.std() * rank_target.std())
+    
+    
+def spearman_corrcoef()
+    
+    
