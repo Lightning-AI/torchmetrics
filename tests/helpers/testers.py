ad5abd3c5f6300da637a51362446214fa859384a
Nicki Skafte <skaftenicki@gmail.com>
2021-04-13 13:49:17 +0200

    pep8

--- tests/helpers/testers.py
+++ tests/helpers/testers.py
@@ -458,7 +458,7 @@ class MetricTester:
                     partial(metric_functional, **metric_args),
                     (preds[0].double(), target[0])
                 )
-            
+
             # reset as else it will carry over to other tests
             preds.requires_grad = False
 
