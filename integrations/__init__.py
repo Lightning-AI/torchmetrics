d30d368523368e213c795d3d1098e885b5899618
jirka <jirka.borovec@seznam.cz>
2021-04-21 13:45:56 +0200

    flake8

--- integrations/__init__.py
+++ integrations/__init__.py
@@ -1,5 +1,5 @@
 import operator
 
-from torchmetrics.utilities.imports import _LIGHTNING_AVAILABLE, _compare_version
+from torchmetrics.utilities.imports import _compare_version
 
 _LIGHTNING_GREATER_EQUAL_1_3 = _compare_version("pytorch_lightning", operator.ge, "1.3.0")
