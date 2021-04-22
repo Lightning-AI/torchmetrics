fix auc calculation and add tests

tests/classification/test_auc.py
@@ -32,19 +32,29 @@ def sk_auc(x, y):
     return _sk_auc(x, y)
 
 
+def sk_auc_reorder(x, y):
+    x = x.flatten()
+    y = y.flatten()
+    idx = np.argsort(x, kind='stable')
+    x = x[idx]
+    y = y[idx]
+    return _sk_auc(x, y)
+
+
 Input = namedtuple('Input', ["x", "y"])
 
 _examples = []
 # generate already ordered samples, sorted in both directions
-for i in range(4):
-    x = np.random.randint(0, 5, (NUM_BATCHES * 8))
-    y = np.random.randint(0, 5, (NUM_BATCHES * 8))
-    idx = np.argsort(x, kind='stable')
-    x = x[idx] if i % 2 == 0 else x[idx[::-1]]
-    y = y[idx] if i % 2 == 0 else x[idx[::-1]]
-    x = x.reshape(NUM_BATCHES, 8)
-    y = y.reshape(NUM_BATCHES, 8)
-    _examples.append(Input(x=tensor(x), y=tensor(y)))
+for batch_size in (8, 4049):
+    for i in range(4):
+        x = np.random.randint(0, 5, (NUM_BATCHES * batch_size))
+        y = np.random.randint(0, 5, (NUM_BATCHES * batch_size))
+        idx = np.argsort(x, kind='stable')
+        x = x[idx] if i % 2 == 0 else x[idx[::-1]]
+        y = y[idx] if i % 2 == 0 else x[idx[::-1]]
+        x = x.reshape(NUM_BATCHES, batch_size)
+        y = y.reshape(NUM_BATCHES, batch_size)
+        _examples.append(Input(x=tensor(x), y=tensor(y)))
 
 
 @pytest.mark.parametrize("x, y", _examples)
@@ -62,8 +72,14 @@ class TestAUC(MetricTester):
             dist_sync_on_step=dist_sync_on_step,
         )
 
-    def test_auc_functional(self, x, y):
-        self.run_functional_metric_test(x, y, metric_functional=auc, sk_metric=sk_auc, metric_args={"reorder": False})
+    @pytest.mark.parametrize("reorder", [True, False])
+    def test_auc_functional(self, x, y, reorder):
+        if reorder:
+            self.run_functional_metric_test(x, y, metric_functional=auc,
+                                            sk_metric=sk_auc_reorder, metric_args={"reorder": reorder})
+        else:
+            self.run_functional_metric_test(x, y, metric_functional=auc, sk_metric=sk_auc,
+                                            metric_args={"reorder": reorder})
 
 
 @pytest.mark.parametrize(['x', 'y', 'expected'], [

torchmetrics/utilities/data.py
@@ -151,7 +151,7 @@ def get_num_classes(
     return num_classes
 
 
-def _stable_1d_sort(x: torch, nb: int = 2049):
+def _stable_1d_sort(x: torch):
     """
     Stable sort of 1d tensors. Pytorch defaults to a stable sorting algorithm
     if number of elements are larger than 2048. This function pads the tensors,
@@ -166,18 +166,16 @@ def _stable_1d_sort(x: torch, nb: int = 2049):
         >>> data = torch.tensor([8, 7, 2, 6, 4, 5, 3, 1, 9, 0])
         >>> _stable_1d_sort(data)
         (tensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]), tensor([9, 7, 2, 6, 4, 5, 3, 1, 0, 8]))
-        >>> _stable_1d_sort(data, nb=5)
-        (tensor([0, 1, 2, 3, 4]), tensor([9, 7, 2, 6, 4]))
     """
     if x.ndim > 1:
         raise ValueError('Stable sort only works on 1d tensors')
     n = x.numel()
+    nb = 2049
     if n < nb:
         x_max = x.max()
         x = torch.cat([x, (x_max + 1) * torch.ones(nb - n, dtype=x.dtype, device=x.device)], 0)
     x_sort = x.sort()
-    i = min(nb, n)
-    return x_sort.values[:i], x_sort.indices[:i]
+    return x_sort.values[:n], x_sort.indices[:n]
 
 
 def apply_to_collection(