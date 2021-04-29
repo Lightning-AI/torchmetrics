7976167bf8d2a24bab906e4a166e25e9b2c82d2c
Maxim Grechkin <maximsch2@gmail.com>
2021-04-28 18:03:09 -0700

    add type

--- tests/retrieval/helpers.py
+++ tests/retrieval/helpers.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from functools import partial
-from typing import Callable, Dict, Tuple, Type, Union
+from typing import Callable, Dict, List, Tuple, Type, Union
 
 import numpy as np
 import pytest
