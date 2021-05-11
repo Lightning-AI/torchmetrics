d3982466ef4ca2d886cbe7064d3598c31e66c119
jirka <jirka.borovec@seznam.cz>
2021-05-11 16:32:14 +0200

    .

--- torchmetrics/functional/classification/auc.py
+++ torchmetrics/functional/classification/auc.py
@@ -26,13 +26,11 @@ def _auc_update(x: Tensor, y: Tensor) -> Tuple[Tensor, Tensor]:
 
     if x.ndim > 1 or y.ndim > 1:
         raise ValueError(
-            f'Expected both `x` and `y` tensor to be 1d, but got'
-            f' tensors with dimension {x.ndim} and {y.ndim}'
+            f'Expected both `x` and `y` tensor to be 1d, but got tensors with dimension {x.ndim} and {y.ndim}'
         )
     if x.numel() != y.numel():
         raise ValueError(
-            f'Expected the same number of elements in `x` and `y`'
-            f' tensor but received {x.numel()} and {y.numel()}'
+            f'Expected the same number of elements in `x` and `y` tensor but received {x.numel()} and {y.numel()}'
         )
     return x, y
 
@@ -55,8 +53,7 @@ def _auc_compute(x: Tensor, y: Tensor, reorder: bool = False) -> Tensor:
                 direction = -1.
             else:
                 raise ValueError(
-                    "The `x` tensor is neither increasing or decreasing."
-                    " Try setting the reorder argument to `True`."
+                    "The `x` tensor is neither increasing or decreasing. Try setting the reorder argument to `True`."
                 )
         else:
             direction = 1.
