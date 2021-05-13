c25bc659a542db2619a8ceffae8e247000e7e582
hassiahk <haswanth.kumar.39@gmail.com>
2021-05-13 17:22:23 +0530

    Added Minimal Implementation

---
+++ torchmetrics/functional/classification/kldivergence.py
@@ -0,0 +1,38 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from torch import Tensor
+import torch
+
+from torchmetrics.utilities.data import METRIC_EPS
+
+
+def _kld_update(preds: Tensor, target: Tensor):
+    preds = torch.clamp(preds, METRIC_EPS)
+    target = torch.clamp(target, METRIC_EPS)
+
+    total = preds.numel()
+
+    measures = torch.sum(target * torch.log(target / preds), axis=-1)
+
+    return measures, total
+
+
+def _kld_compute(measures: Tensor, total: Tensor):
+    return measures / total
+
+
+def kldivergence(preds: Tensor, target: Tensor):
+    measures, total = _kld_update(preds, target)
+    return _kld_compute(measures, total)
