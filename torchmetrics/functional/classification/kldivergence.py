987db8272b0a36b0b36b5c37a4be2e85745839a4
hassiahk <haswanth.kumar.39@gmail.com>
2021-05-17 17:03:23 +0530

    Requested Changes

--- torchmetrics/functional/classification/kldivergence.py
+++ torchmetrics/functional/classification/kldivergence.py
@@ -12,13 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import Tuple
+
 import torch
 from torch import Tensor
 
+from torchmetrics.utilities.checks import _check_same_shape
 from torchmetrics.utilities.data import METRIC_EPS
 
 
-def _kld_update(preds: Tensor, target: Tensor):
+def _kld_update(preds: Tensor, target: Tensor) -> Tuple[Tensor, int]:
+    _check_same_shape(preds, target)
+
+    preds = preds / preds.sum(axis=-1)
+    target = target / target.sum(axis=-1)
+
     preds = torch.clamp(preds, METRIC_EPS)
     target = torch.clamp(target, METRIC_EPS)
 
@@ -29,10 +37,10 @@ def _kld_update(preds: Tensor, target: Tensor):
     return measures, total
 
 
-def _kld_compute(measures: Tensor, total: Tensor):
+def _kld_compute(measures: Tensor, total: Tensor) -> Tensor:
     return measures / total
 
 
-def kldivergence(preds: Tensor, target: Tensor):
+def kldivergence(preds: Tensor, target: Tensor) -> Tensor:
     measures, total = _kld_update(preds, target)
     return _kld_compute(measures, total)
