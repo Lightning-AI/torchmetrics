6de666ff079a0e81132a5be829bfd2601bf9a392
quancs <quancs@qq.com>
2021-06-16 04:37:07 +0800

    update test_si_sdr

--- tests/audio/test_si_sdr.py
+++ tests/audio/test_si_sdr.py
@@ -16,13 +16,15 @@ from functools import partial
 
 import pytest
 import torch
-from asteroid.losses import PairwiseNegSDR
+from torch.tensor import Tensor
 
 from tests.helpers import seed_all
 from tests.helpers.testers import BATCH_SIZE, NUM_BATCHES, MetricTester
 from torchmetrics.audio import SI_SDR
 from torchmetrics.functional import si_sdr
 from torchmetrics.utilities.imports import _TORCH_GREATER_EQUAL_1_6
+
+import speechmetrics
 
 seed_all(42)
 
@@ -35,40 +37,58 @@ inputs = Input(
     target=torch.rand(NUM_BATCHES, BATCH_SIZE, 1, Time),
 )
 
+speechmetrics_sisdr = speechmetrics.load('sisdr')
 
-def asteroid_metric(preds, target, asteroid_loss_func):
+
+def speechmetrics_si_sdr(preds: Tensor, target: Tensor, zero_mean: bool):
     # shape: preds [BATCH_SIZE, 1, Time] , target [BATCH_SIZE, 1, Time]
-    metric = -asteroid_loss_func(preds, target)
-    return metric.view(BATCH_SIZE, 1)
+    # or shape: preds [NUM_BATCHES*BATCH_SIZE, 1, Time] , target [NUM_BATCHES*BATCH_SIZE, 1, Time]
+    if zero_mean:
+        preds = preds - preds.mean(dim=2, keepdim=True)
+        target = target - target.mean(dim=2, keepdim=True)
+    mss = []
+    for i in range(preds.shape[0]):
+        ms = []
+        for j in range(preds.shape[1]):
+            metric = speechmetrics_sisdr(preds[i, j].numpy(),
+                                         target[i, j].numpy(),
+                                         rate=16000)
+            ms.append(metric['sisdr'][0])
+        mss.append(ms)
+    return torch.tensor(mss)
 
 
 def average_metric(preds, target, metric_func):
     # shape: preds [BATCH_SIZE, 1, Time] , target [BATCH_SIZE, 1, Time]
+    # or shape: preds [NUM_BATCHES*BATCH_SIZE, 1, Time] , target [NUM_BATCHES*BATCH_SIZE, 1, Time]
     return metric_func(preds, target).mean()
 
 
-asteroid_sisdr_zero_mean = partial(asteroid_metric, asteroid_loss_func=PairwiseNegSDR("sisdr"))
-asteroid_sisdr_no_zero_mean = partial(asteroid_metric, asteroid_loss_func=PairwiseNegSDR("sisdr", zero_mean=False))
+speechmetrics_si_sdr_zero_mean = partial(speechmetrics_si_sdr, zero_mean=True)
+speechmetrics_si_sdr_no_zero_mean = partial(speechmetrics_si_sdr,
+                                            zero_mean=False)
 
 
 @pytest.mark.parametrize(
     "preds, target, sk_metric, zero_mean",
     [
-        (inputs.preds, inputs.target, asteroid_sisdr_zero_mean, True),
-        (inputs.preds, inputs.target, asteroid_sisdr_no_zero_mean, False),
+        (inputs.preds, inputs.target, speechmetrics_si_sdr_zero_mean, True),
+        (inputs.preds, inputs.target, speechmetrics_si_sdr_no_zero_mean,
+         False),
     ],
 )
 class TestSISDR(MetricTester):
 
     @pytest.mark.parametrize("ddp", [True, False])
     @pytest.mark.parametrize("dist_sync_on_step", [True, False])
-    def test_si_sdr(self, preds, target, sk_metric, zero_mean, ddp, dist_sync_on_step):
+    def test_si_sdr(self, preds, target, sk_metric, zero_mean, ddp,
+                    dist_sync_on_step):
         self.run_class_metric_test(
             ddp,
             preds,
             target,
             SI_SDR,
-            sk_metric=sk_metric,
+            sk_metric=partial(average_metric, metric_func=sk_metric),
             dist_sync_on_step=dist_sync_on_step,
             metric_args=dict(zero_mean=zero_mean),
         )
@@ -82,39 +102,35 @@ class TestSISDR(MetricTester):
             metric_args=dict(zero_mean=zero_mean),
         )
 
-    def test_si_sdr_differentiability(self, preds, target, sk_metric, zero_mean):
-        self.run_differentiability_test(
-            preds=preds,
-            target=target,
-            metric_module=SI_SDR,
-            metric_functional=si_sdr,
-            metric_args={'zero_mean': zero_mean}
-        )
+    def test_si_sdr_differentiability(self, preds, target, sk_metric,
+                                      zero_mean):
+        self.run_differentiability_test(preds=preds,
+                                        target=target,
+                                        metric_module=SI_SDR,
+                                        metric_functional=si_sdr,
+                                        metric_args={'zero_mean': zero_mean})
 
     @pytest.mark.skipif(
-        not _TORCH_GREATER_EQUAL_1_6, reason='half support of core operations on not support before pytorch v1.6'
-    )
+        not _TORCH_GREATER_EQUAL_1_6,
+        reason=
+        'half support of core operations on not support before pytorch v1.6')
     def test_si_sdr_half_cpu(self, preds, target, sk_metric, zero_mean):
-        self.run_precision_test_cpu(
-            preds=preds,
-            target=target,
-            metric_module=SI_SDR,
-            metric_functional=si_sdr,
-            metric_args={'zero_mean': zero_mean}
-        )
+        pytest.xfail("SI-SDR metric does not support cpu + half precision")
 
-    @pytest.mark.skipif(not torch.cuda.is_available(), reason='test requires cuda')
+    @pytest.mark.skipif(not torch.cuda.is_available(),
+                        reason='test requires cuda')
     def test_si_sdr_half_gpu(self, preds, target, sk_metric, zero_mean):
-        self.run_precision_test_gpu(
-            preds=preds,
-            target=target,
-            metric_module=SI_SDR,
-            metric_functional=si_sdr,
-            metric_args={'zero_mean': zero_mean}
-        )
+        self.run_precision_test_gpu(preds=preds,
+                                    target=target,
+                                    metric_module=SI_SDR,
+                                    metric_functional=si_sdr,
+                                    metric_args={'zero_mean': zero_mean})
 
 
 def test_error_on_different_shape(metric_class=SI_SDR):
     metric = metric_class()
-    with pytest.raises(RuntimeError, match='Predictions and targets are expected to have the same shape'):
-        metric(torch.randn(100, ), torch.randn(50, ))
+    with pytest.raises(
+            RuntimeError,
+            match='Predictions and targets are expected to have the same shape'
+    ):
+        metric(torch.randn(100,), torch.randn(50,))
