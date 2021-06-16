9d11ab02b0855d6762c3187bbd94c1bb2d146332
SkafteNicki <skaftenicki@gmail.com>
2021-06-16 15:44:30 +0200

    implementation

---
+++ torchmetrics/image/kid.py
@@ -0,0 +1,233 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import Any, Callable, Optional, Tuple, Union
+
+import torch
+from torch import Tensor
+
+from torchmetrics.image.fid import NoTrainInceptionV3
+from torchmetrics.metric import Metric
+from torchmetrics.utilities import rank_zero_warn
+from torchmetrics.utilities.imports import _TORCH_FIDELITY_AVAILABLE
+
+def mmd2(k_xx: Tensor, k_xy: Tensor, k_yy: Tensor) -> Tensor:
+    """
+    Adapted from https://github.com/toshas/torch-fidelity/blob/v0.3.0/torch_fidelity/metric_kid.py
+    """
+    m = k_xx.shape[0]
+
+    diag_x = torch.diag(k_xx)
+    sum_diag = diag_x.sum()
+
+    diag_y = torch.diag(k_yy)
+    sum_diag = diag_y.sum()
+
+    kt_xx_sums = k_xx.sum(dim=-1) - diag_x
+    kt_yy_sums = k_yy.sum(dim=-1) - diag_y
+    k_xy_sums = k_xy.sum(dim=0)
+
+    kt_xx_sum = kt_xx_sums.sum()
+    kt_yy_sum = kt_yy_sums.sum()
+    k_xy_sum = k_xy_sums.sum()
+
+    value = (kt_xx_sum + kt_yy_sum) / (m * (m-1))
+    value -= 2 * k_xy_sum / (m * m)
+    return mmd2
+
+
+def poly_kernel(
+    f1: Tensor, 
+    f2: Tensor, 
+    degree: int = 3, 
+    gamma: float = Optional[None], 
+    coef: float = 1.0
+) -> Tensor:
+    """
+    Adapted from https://github.com/toshas/torch-fidelity/blob/v0.3.0/torch_fidelity/metric_kid.py
+    """
+    if gamma is None:
+        gamma = 1.0 / f1.shape[1]
+    kernel = (f1 @ f2.T * gamma + coef) ** degree
+    return kernel
+
+
+def poly_mmd(
+    f_real: Tensor, 
+    f_fake: Tensor, 
+    degree: int = 3, 
+    gamma: float = Optional[None], 
+    coef: float = 1.0
+) -> Tensor:
+    """
+    Adapted from https://github.com/toshas/torch-fidelity/blob/v0.3.0/torch_fidelity/metric_kid.py
+    """
+    k_11 = poly_kernel(f_real, f_real, degree, gamma, coef)
+    k_22 = poly_kernel(f_fake, f_fake, degree, gamma, coef)
+    k_12 = poly_kernel(f_real, f_fake, degree, gamma, coef)
+    return mmd2(k_11, k_12, k_22)
+
+
+class KID(Metric):
+    r"""
+    Calculates the Kernel Inception Score (KID) Inception Score (IS) which is used to access how realistic generated images are.
+    It is defined as
+
+    .. math::
+        IS = exp(\mathbb{E}_x KL(p(y | x ) || p(y)))
+
+    where :math:`KL(p(y | x) || p(y))` is the KL divergence between the conditional distribution :math:`p(y|x)`
+    and the margianl distribution :math:`p(y)`. Both the conditional and marginal distribution is calculated
+    from features extracted from the images. The score is calculated on random splits of the images such that
+    both a mean and standard deviation of the score are returned. The metric was originally proposed in [1].
+
+    Using the default feature extraction (Inception v3 using the original weights from [2]), the input is
+    expected to be mini-batches of 3-channel RGB images of shape (3 x H x W) with dtype uint8. All images
+    will be resized to 299 x 299 which is the size of the original training data.
+
+    .. note:: using this metric with the default feature extractor requires that ``torch-fidelity``
+        is installed. Either install as ``pip install torchmetrics[image]`` or
+        ``pip install torch-fidelity``
+
+    .. note:: the ``forward`` method can be used but ``compute_on_step`` is disabled by default (oppesit of
+        all other metrics) as this metric does not really make sense to calculate on a single batch. This
+        means that by default ``forward`` will just call ``update`` underneat.
+
+    Args:
+        feature:
+            Either an str, integer or ``nn.Module``:
+
+            - an str or integer will indicate the inceptionv3 feature layer to choose. Can be one of the following:
+              'logits_unbiased', 64, 192, 768, 2048
+            - an ``nn.Module`` for using a custom feature extractor. Expects that its forward method returns
+              an ``[N,d]`` matrix where ``N`` is the batch size and ``d`` is the feature size.
+
+        splits: integer determining how many splits the inception score calculation should be split among
+
+        compute_on_step:
+            Forward only calls ``update()`` and return ``None`` if this is set to ``False``.
+        dist_sync_on_step:
+            Synchronize metric state across processes at each ``forward()``
+            before returning the value at the step
+        process_group:
+            Specify the process group on which synchronization is called.
+            default: ``None`` (which selects the entire world)
+        dist_sync_fn:
+            Callback that performs the allgather operation on the metric state. When ``None``, DDP
+            will be used to perform the allgather
+
+    [1] Demystifying MMD GANs
+    Mikołaj Bińkowski, Danica J. Sutherland, Michael Arbel, Arthur Gretton
+    https://arxiv.org/abs/1801.01401
+
+    [2] GANs Trained by a Two Time-Scale Update Rule Converge to a Local Nash Equilibrium,
+    Martin Heusel, Hubert Ramsauer, Thomas Unterthiner, Bernhard Nessler, Sepp Hochreiter
+    https://arxiv.org/abs/1706.08500
+    """
+
+    def __init__(
+        self,
+        feature: Union[str, int, torch.nn.Module] = 2048,
+        subsets: int = 100,
+        subset_size: int = 1000,
+        degree: int = 3,
+        gamma: float = Optional[None],
+        coef: float = 1.0,
+        compute_on_step: bool = False,
+        dist_sync_on_step: bool = False,
+        process_group: Optional[Any] = None,
+        dist_sync_fn: Callable = None
+    ):
+        super().__init__(
+            compute_on_step=compute_on_step,
+            dist_sync_on_step=dist_sync_on_step,
+            process_group=process_group,
+            dist_sync_fn=dist_sync_fn,
+        )
+
+        rank_zero_warn(
+            'Metric `IS` will save all extracted features in buffer.'
+            ' For large datasets this may lead to large memory footprint.', UserWarning
+        )
+
+        if isinstance(feature, (str, int)):
+            if not _TORCH_FIDELITY_AVAILABLE:
+                raise ValueError(
+                    'IS metric requires that Torch-fidelity is installed.'
+                    'Either install as `pip install torchmetrics[image-quality]`'
+                    ' or `pip install torch-fidelity`'
+                )
+            valid_int_input = ['logits_unbiased', 64, 192, 768, 2048]
+            if feature not in valid_int_input:
+                raise ValueError(
+                    f'Integer input to argument `feature` must be one of {valid_int_input},'
+                    f' but got {feature}.'
+                )
+
+            self.inception = NoTrainInceptionV3(name='inception-v3-compat', features_list=[str(feature)])
+        elif isinstance(feature, torch.nn.Module):
+            self.inception = feature
+        else:
+            raise ValueError('Got unknown input to argument `feature`')
+
+        self.subsets = subsets
+        self.subset_size = subset_size
+        self.degree = degree
+        self.gamma = gamma
+        self.coef = coef
+        self.add_state("features", [], dist_reduce_fx=None)
+
+    def update(self, imgs: Tensor, real: bool) -> None:  # type: ignore
+        """ Update the state with extracted features
+
+        Args:
+            imgs: tensor with images feed to the feature extractor
+            real: bool indicating if imgs belong to the real or the fake distribution
+        """
+        features = self.inception(imgs)
+
+        if real:
+            self.real_features.append(features)
+        else:
+            self.fake_features.append(features)
+
+    def compute(self) -> Tuple[Tensor, Tensor]:
+        """ Calculate KID score based on accumulated extracted features from the two distributions 
+            Implementation inspired by https://github.com/toshas/torch-fidelity/blob/v0.3.0/torch_fidelity/metric_kid.py
+        """
+        real_features = torch.cat(self.real_features, dim=0)
+        fake_features = torch.cat(self.fake_features, dim=0)
+        
+        n_samples_real = real_features.shape[0]
+        if n_samples_real < self.subset_size:
+            raise ValueError()
+        n_samples_fake = fake_features.shape[0]
+        if n_samples_fake < self.subset_size:
+            raise ValueError()
+
+        kid_scores = []
+        for i in range(self.subsets):
+            perm = torch.randperm(n_samples_real)
+            f_real = real_features[perm[:self.subset_size]]
+            perm = torch.randperm(n_samples_fake)
+            f_fake = fake_features[perm[:self.subset_size]]
+
+            o = poly_mmd(f_real, f_fake self.degree, self.gamma, self.coef)
+            kid_scores.append(o)
+        kid_scores = torch.stack(kid_scores)
+        return kid_scores.mean(), kid_scores.std()
+
+        
+
+        
+        
