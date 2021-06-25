02479d238b679e0e027c3897ffee1f5f9ed1dde1
Jirka <jirka.borovec@seznam.cz>
2021-06-25 15:36:49 +0200

    deprecate

---
+++ torchmetrics/regression/psnr.py
@@ -0,0 +1,49 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import Any, Optional, Tuple, Union
+from warnings import warn
+
+from torchmetrics.image.psnr import PSNR as _PSNR
+
+
+class PSNR(_PSNR):
+    """
+    .. deprecated:: 0.4
+        The PSNR was moved to `torchmetrics.image.psnr`.
+
+    """
+
+    def __init__(
+        self,
+        data_range: Optional[float] = None,
+        base: float = 10.0,
+        reduction: str = 'elementwise_mean',
+        dim: Optional[Union[int, Tuple[int, ...]]] = None,
+        compute_on_step: bool = True,
+        dist_sync_on_step: bool = False,
+        process_group: Optional[Any] = None,
+    ):
+        warn(
+            "This `PSNR` was deprecated in v0.4 and will be removed in v0.5. Use `torchmetrics.image.psnr` instead.",
+            DeprecationWarning
+        )
+        super().__init__(
+            data_range=data_range,
+            base=base,
+            reduction=reduction,
+            dim=dim,
+            compute_on_step=compute_on_step,
+            dist_sync_on_step=dist_sync_on_step,
+            process_group=process_group,
+        )
---
+++ torchmetrics/regression/ssim.py
@@ -0,0 +1,53 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import Any, Optional, Sequence
+from warnings import warn
+
+from torchmetrics.image.ssim import SSIM as _SSIM
+
+
+class SSIM(_SSIM):
+    """
+    .. deprecated:: 0.4
+        The SSIM was moved to `torchmetrics.image.ssim`.
+
+    """
+
+    def __init__(
+        self,
+        kernel_size: Sequence[int] = (11, 11),
+        sigma: Sequence[float] = (1.5, 1.5),
+        reduction: str = "elementwise_mean",
+        data_range: Optional[float] = None,
+        k1: float = 0.01,
+        k2: float = 0.03,
+        compute_on_step: bool = True,
+        dist_sync_on_step: bool = False,
+        process_group: Optional[Any] = None,
+    ):
+        warn(
+            "This `SIIM` was deprecated in v0.4 and will be removed in v0.5. Use `torchmetrics.image.ssim` instead.",
+            DeprecationWarning
+        )
+        super().__init__(
+            kernel_size=kernel_size,
+            sigma=sigma,
+            reduction=reduction,
+            data_range=data_range,
+            k1=k1,
+            k2=k2,
+            compute_on_step=compute_on_step,
+            dist_sync_on_step=dist_sync_on_step,
+            process_group=process_group,
+        )
