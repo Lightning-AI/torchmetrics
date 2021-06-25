04d91edd252dbb6a8d512e199559b01333845b0e
Jirka <jirka.borovec@seznam.cz>
2021-06-25 15:51:13 +0200

    prune

--- torchmetrics/utilities/__init__.py
+++ torchmetrics/utilities/__init__.py
@@ -1,6 +1,3 @@
-from typing import Any, Optional
-from warnings import warn
-
 from torchmetrics.utilities.data import apply_to_collection  # noqa: F401
 from torchmetrics.utilities.distributed import class_reduce, reduce  # noqa: F401
 from torchmetrics.utilities.prints import rank_zero_debug, rank_zero_info, rank_zero_warn  # noqa: F401
