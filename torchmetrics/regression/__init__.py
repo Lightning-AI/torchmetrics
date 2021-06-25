8cb77b2dcd14782bb0cc91a98f2155d38d813742
Jirka <jirka.borovec@seznam.cz>
2021-06-25 15:23:29 +0200

    move psnr & ssim

--- torchmetrics/regression/__init__.py
+++ torchmetrics/regression/__init__.py
@@ -17,7 +17,7 @@ from torchmetrics.regression.mean_absolute_percentage_error import MeanAbsoluteP
 from torchmetrics.regression.mean_squared_error import MeanSquaredError  # noqa: F401
 from torchmetrics.regression.mean_squared_log_error import MeanSquaredLogError  # noqa: F401
 from torchmetrics.regression.pearson import PearsonCorrcoef  # noqa: F401
-from torchmetrics.regression.psnr import PSNR  # noqa: F401
+from torchmetrics.image.psnr import PSNR  # noqa: F401
 from torchmetrics.regression.r2score import R2Score  # noqa: F401
 from torchmetrics.regression.spearman import SpearmanCorrcoef  # noqa: F401
-from torchmetrics.regression.ssim import SSIM  # noqa: F401
+from torchmetrics.image.ssim import SSIM  # noqa: F401
