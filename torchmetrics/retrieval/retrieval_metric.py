fe726c5bc0f6f1a6ecc59ef888be3c2263eda41f
Jirka <jirka.borovec@seznam.cz>
2021-06-30 10:37:14 +0200

    ...

--- torchmetrics/retrieval/retrieval_metric.py
+++ torchmetrics/retrieval/retrieval_metric.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from abc import ABC, abstractmethod
-from typing import Any, Callable, Optional, overload
+from typing import Any, Callable, Optional
 
 import torch
 from torch import Tensor, tensor
