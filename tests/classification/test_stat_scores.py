f7e7ba3f291c4ef3a829241d033730418e51aa6a
Jirka <jirka.borovec@seznam.cz>
2021-07-06 20:04:30 +0200

    flake8

--- tests/classification/test_stat_scores.py
+++ tests/classification/test_stat_scores.py
@@ -29,7 +29,7 @@ from tests.classification.inputs import _input_multilabel as _input_mcls
 from tests.classification.inputs import _input_multilabel_logits as _input_mlb_logits
 from tests.classification.inputs import _input_multilabel_prob as _input_mlb_prob
 from tests.helpers import seed_all
-from tests.helpers.testers import NUM_CLASSES, THRESHOLD, MetricTester
+from tests.helpers.testers import NUM_CLASSES, MetricTester
 from torchmetrics import StatScores
 from torchmetrics.functional import stat_scores
 from torchmetrics.utilities.checks import _input_format_classification
