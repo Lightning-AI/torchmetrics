a0540a1d8d3837c3f3fff543c23cbd6b96817b9e
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-12 23:31:50 -0400

    Create wer.py

---
+++ torchmetrics/text/wer/wer.py
@@ -0,0 +1,26 @@
+from typing import Any
+
+from jiwer import compute_measures
+from torchmetrics.metric import Metric
+
+
+class WER(Metric):
+    def __init__(self, concatenate_texts=False):
+        super().__init__()
+        self.concatenate_texts = concatenate_texts
+
+    def update(self, preds: Any, target: Any) -> None:
+        self.preds = preds
+        self.target = target
+
+    def compute(self) -> Any:
+        if self.concatenate_texts:
+            return compute_measures(self.target, self.preds)["wer"]
+        else:
+            incorrect = 0
+            total = 0
+            for prediction, reference in zip(self.preds, self.target):
+                measures = compute_measures(reference, prediction)
+                incorrect += measures["substitutions"] + measures["deletions"] + measures["insertions"]
+                total += measures["substitutions"] + measures["deletions"] + measures["hits"]
+            return incorrect / total
