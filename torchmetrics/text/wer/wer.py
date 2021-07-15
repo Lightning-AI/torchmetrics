34a4bbb7a73a522b1ba7eda51598b172b7e036a3
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-15 14:14:38 -0400

    Adding Docstring

--- torchmetrics/text/wer/wer.py
+++ torchmetrics/text/wer/wer.py
@@ -6,7 +6,37 @@ from torchmetrics.metric import Metric
 
 
 class WER(Metric):
+    """
+    Word error rate (WER) is a common metric of the performance of an automatic speech recognition system.
 
+    WER's output is always a number between 0 and 1. This value indicates the percentage of words that were incorrectly predicted. The lower the value, the better the
+    performance of the ASR system with a WER of 0 being a perfect score.
+
+    Word error rate can then be computed as:
+    WER = (S + D + I) / N = (S + D + I) / (S + D + C)
+    where
+    S is the number of substitutions,
+    D is the number of deletions,
+    I is the number of insertions,
+    C is the number of correct words,
+    N is the number of words in the reference (N=S+D+C).
+
+    Compute WER score of transcribed segments against references.
+
+    Args:
+        references: List of references for each speech input.
+        predictions: List of transcriptions to score.
+        concatenate_texts (bool, default=False): Whether to concatenate all input texts or compute WER iteratively.
+    Returns:
+        (float): the word error rate
+    Examples:
+        >>> predictions = ["this is the prediction", "there is an other sample"]
+        >>> references = ["this is the reference", "there is another one"]
+        >>> wer = WER()
+        >>> wer_score = wer.compute(predictions=predictions, references=references)
+        >>> print(wer_score)
+        0.5
+    """
     def __init__(self, concatenate_texts: bool = False):
         super().__init__()
         self.concatenate_texts = concatenate_texts
