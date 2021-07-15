d781caeb5ef00872da27859a8ea9af3f2f2a9325
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-15 18:42:53 -0400

    Update __init__.py

--- torchmetrics/functional/text/__init__.py
+++ torchmetrics/functional/text/__init__.py
@@ -1,8 +1,8 @@
 from torchmetrics.functional.text.squadv2 import (
-    apply_no_ans_threshold,
-    find_all_best_thresh,
-    get_raw_scores,
-    make_eval_dict,
-    make_qid_to_has_ans,
-    merge_eval,
-)
+    apply_no_ans_threshold, # noqa: F401
+    find_all_best_thresh, # noqa: F401
+    get_raw_scores, # noqa: F401
+    make_eval_dict, # noqa: F401
+    make_qid_to_has_ans, # noqa: F401
+    merge_eval, # noqa: F401
+) # noqa: F401
