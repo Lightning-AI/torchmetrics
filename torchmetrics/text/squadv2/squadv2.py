a71754412816fb31091c94b957efe79c7d81a36b
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-15 13:59:53 -0400

    Update squadv2.py

--- torchmetrics/text/squadv2/squadv2.py
+++ torchmetrics/text/squadv2/squadv2.py
@@ -16,46 +16,45 @@ class SQuADv2(Metric):
     This metric wrap the official scoring script for version 2 of the Stanford Question
     Answering Dataset (SQuAD).
     Computes SQuAD v2 scores (F1 and EM).
-Args:
-    predictions: List of triple for question-answers to score with the following elements:
-        - the question-answer 'id' field as given in the references (see below)
-        - the text of the answer
-        - the probability that the question has no answer
-    references: List of question-answers dictionaries with the following key-values:
+    Args:
+        predictions: List of triple for question-answers to score with the following elements:
+            - the question-answer 'id' field as given in the references (see below)
+            - the text of the answer
+            - the probability that the question has no answer
+        references: List of question-answers dictionaries with the following key-values:
             - 'id': id of the question-answer pair (see above),
             - 'answers': a list of Dict {'text': text of the answer as a string}
-    no_answer_threshold: float
-        Probability threshold to decide that a question has no answer.
-Returns:
-    'exact': Exact match (the normalized answer exactly match the gold answer)
-    'f1': The F-score of predicted tokens versus the gold answer
-    'total': Number of score considered
-    'HasAns_exact': Exact match (the normalized answer exactly match the gold answer)
-    'HasAns_f1': The F-score of predicted tokens versus the gold answer
-    'HasAns_total': Number of score considered
-    'NoAns_exact': Exact match (the normalized answer exactly match the gold answer)
-    'NoAns_f1': The F-score of predicted tokens versus the gold answer
-    'NoAns_total': Number of score considered
-    'best_exact': Best exact match (with varying threshold)
-    'best_exact_thresh': No-answer probability threshold associated to the best exact match
-    'best_f1': Best F1 (with varying threshold)
-    'best_f1_thresh': No-answer probability threshold associated to the best F1
+        no_answer_threshold: float Probability threshold to decide that a question has no answer.
+    Returns:
+        'exact': Exact match (the normalized answer exactly match the gold answer)
+        'f1': The F-score of predicted tokens versus the gold answer
+        'total': Number of score considered
+        'HasAns_exact': Exact match (the normalized answer exactly match the gold answer)
+        'HasAns_f1': The F-score of predicted tokens versus the gold answer
+        'HasAns_total': Number of score considered
+        'NoAns_exact': Exact match (the normalized answer exactly match the gold answer)
+        'NoAns_f1': The F-score of predicted tokens versus the gold answer
+        'NoAns_total': Number of score considered
+        'best_exact': Best exact match (with varying threshold)
+        'best_exact_thresh': No-answer probability threshold associated to the best exact match
+        'best_f1': Best F1 (with varying threshold)
+        'best_f1_thresh': No-answer probability threshold associated to the best F1
     Examples:
-    >>> predictions = [{'prediction_text': '1976', 'id': '56e10a3be3433e1400422b22', 'no_answer_probability': 0.}]
-    >>> references = [{'answers': {'answer_start': [97], 'text': ['1976']}, 'id': '56e10a3be3433e1400422b22'}]
-    >>> metrics = SQuADv2()
-    >>> results = metrics.compute(predictions=predictions, references=references)
-    >>> print(results)
-    {'exact': 100.0,
-     'f1': 100.0,
-     'total': 1,
-     'HasAns_exact': 100.0,
-     'HasAns_f1': 100.0,
-     'HasAns_total': 1,
-      'best_exact': 100.0,
-      'best_exact_thresh': 0.0,
-      'best_f1': 100.0,
-      'best_f1_thresh': 0.0}
+        >>> predictions = [{'prediction_text': '1976', 'id': '56e10a3be3433e1400422b22', 'no_answer_probability': 0.}]
+        >>> references = [{'answers': {'answer_start': [97], 'text': ['1976']}, 'id': '56e10a3be3433e1400422b22'}]
+        >>> metrics = SQuADv2()
+        >>> results = metrics.compute(predictions=predictions, references=references)
+        >>> print(results)
+        {'exact': 100.0,
+        'f1': 100.0,
+        'total': 1,
+        'HasAns_exact': 100.0,
+        'HasAns_f1': 100.0,
+        'HasAns_total': 1,
+        'best_exact': 100.0,
+        'best_exact_thresh': 0.0,
+        'best_f1': 100.0,
+        'best_f1_thresh': 0.0}
     """
 
     def __init__(self, n_gram: int = 4, smooth: bool = False, no_answer_threshold: float = 1.0):
