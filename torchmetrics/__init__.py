90d77910cc4669d9615607c7c540d09e7606f35f
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-16 14:33:26 -0400

    Update __init__.py

--- torchmetrics/__init__.py
+++ torchmetrics/__init__.py
@@ -62,3 +62,5 @@ from torchmetrics.retrieval import (  # noqa: F401 E402
     RetrievalRecall,
 )
 from torchmetrics.wrappers import BootStrapper  # noqa: F401 E402
+
+from torchmetrics.text import WER  # noqa: F401 E402
