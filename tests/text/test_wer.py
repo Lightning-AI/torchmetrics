93ad65e10454bef275b4748913d8bb07d624015b
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-16 14:39:03 -0400

    Create test_wer.py

---
+++ tests/text/test_wer.py
@@ -0,0 +1,13 @@
+import pytest
+
+from torchmetrics.text.wer import WER
+
+
+@pytest.mark.parametrize(
+    "hyp,ref,score",
+    [("hello world", "hello world", 0.0), ("hello world", "Firwww", 1.0)],
+)
+def test_wer_same(hyp, ref, score):
+    metric = WER()
+    metric.update(hyp, ref)
+    assert metric.compute() == score
