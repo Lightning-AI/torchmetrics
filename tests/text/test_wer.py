05d7fac60e50625e345c277ce44a6f995cef90cb
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-18 12:47:22 -0700

    Update test_wer.py

--- tests/text/test_wer.py
+++ tests/text/test_wer.py
@@ -2,6 +2,7 @@ import pytest
 
 from torchmetrics.functional.text.wer import wer
 from torchmetrics.text.wer import WER
+from jiwer import compute_measures
 
 
 @pytest.mark.parametrize(
@@ -16,7 +17,16 @@ def test_wer_same(hyp, ref, score):
 
 @pytest.mark.parametrize(
     "hyp,ref,score",
-    [("hello world", "hello world", 0.0), ("hello world", "Firwww", 1.0)],
+    [(["hello world"], ["hello world"], 0.0), (["hello world"], ["Firwww"], 1.0)],
 )
 def test_wer_functional(hyp, ref, score):
     assert wer(ref, hyp) == score
+
+
+@pytest.mark.parametrize(
+    "hyp,ref,score",
+    [(["hello world"], ["hello world"], 0.0), (["hello world"], ["Firwww"], 1.0)],
+)
+def test_jiwer(hyp,ref,score):
+    assert compute_measures(ref, hyp)["wer"] == score
+    
