1153f96a1582ef57ff9fbc6a01308ecd9b86e3c3
quancs <1017241746@qq.com>
2021-07-18 22:46:54 +0800

    pesq

--- torchmetrics/audio/__init__.py
+++ torchmetrics/audio/__init__.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from torchmetrics.audio.pesq import PESQ  # noqa: F401
 from torchmetrics.audio.si_sdr import SI_SDR  # noqa: F401
 from torchmetrics.audio.si_snr import SI_SNR  # noqa: F401
 from torchmetrics.audio.snr import SNR  # noqa: F401
