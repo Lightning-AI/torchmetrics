acdc481124e0f2de644d37e996cfd389e9137f6c
SeanNaren <sean@grid.ai>
2021-07-20 15:22:06 +0100

    Better explanation for tests, skip if JIWER not available

--- tests/text/test_wer.py
+++ tests/text/test_wer.py
@@ -1,5 +1,9 @@
 import pytest
-from jiwer import compute_measures
+
+from torchmetrics.utilities.imports import _JIWER_AVAILABLE
+
+if _JIWER_AVAILABLE:
+    from jiwer import compute_measures
 
 from torchmetrics.functional.text.wer import wer
 from torchmetrics.text.wer import WER
@@ -9,7 +13,11 @@ from torchmetrics.text.wer import WER
     "hyp,ref,score",
     [(["hello world"], ["hello world"], 0.0), (["hello world"], ["Firwww"], 1.0)],
 )
+@pytest.mark.skipif(not _JIWER_AVAILABLE, reason="test requires jiwer")
 def test_wer_same(hyp, ref, score):
+    """
+    Test to ensure that the torchmetric WER matches reference scores
+    """
     metric = WER()
     metric.update(hyp, ref)
     assert metric.compute() == score
@@ -19,15 +27,23 @@ def test_wer_same(hyp, ref, score):
     "hyp,ref,score",
     [(["hello world"], ["hello world"], 0.0), (["hello world"], ["Firwww"], 1.0)],
 )
+@pytest.mark.skipif(not _JIWER_AVAILABLE, reason="test requires jiwer")
 def test_wer_functional(hyp, ref, score):
+    """
+    Test to ensure that the torchmetric functional WER matches the jiwer reference
+    """
     assert wer(ref, hyp) == score
 
 
 @pytest.mark.parametrize(
-    "hyp,ref,score",
+    "hyp,ref",
     [(["hello world"], ["hello world"]), (["hello world"], ["Firwww"])],
 )
-def test_wer(hyp, ref):
+@pytest.mark.skipif(not _JIWER_AVAILABLE, reason="test requires jiwer")
+def test_wer_reference(hyp, ref):
+    """
+    Test to ensure that the torchmetric WER matches the jiwer reference
+    """
     metric = WER()
     metric.update(hyp, ref)
     assert metric.compute() == compute_measures(ref, hyp)['wer']
