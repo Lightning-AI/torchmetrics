39105aea7ed95d477ea9fc64897086a727d3dd2d
quancs <1017241746@qq.com>
2021-07-22 22:39:44 +0800

    Update torchmetrics/functional/audio/pit.py
    Co-authored-by: Nicki Skafte <skaftenicki@gmail.com>

--- torchmetrics/functional/audio/pit.py
+++ torchmetrics/functional/audio/pit.py
@@ -163,7 +163,6 @@ def pit(
     # find best
     if spk_num < 3 or _SCIPY_AVAILABLE is None:
         if spk_num >= 3 and _SCIPY_AVAILABLE is None:
-            import warnings
             warnings.warn(f"Speaker-num is {spk_num}>3, you'd better install scipy to improve pit's performance!")
 
         best_metric, best_perm = _find_best_perm_by_exhuastive_method(
