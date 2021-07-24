6ebb28352b18b6b03dac47047e495c99ebecb7bb
Jirka <jirka.borovec@seznam.cz>
2021-07-24 18:47:37 +0200

    flake8

--- torchmetrics/functional/text/wer.py
+++ torchmetrics/functional/text/wer.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import List, Tuple, Union
+from typing import List, Union
 
 from torchmetrics.utilities.imports import _JIWER_AVAILABLE
 
