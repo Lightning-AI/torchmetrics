0cbb9a4f43b498192010920ab739df0694bdbd64
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-03 16:04:22 -0700

    fixes

--- tests/text/test_bertscore.py
+++ tests/text/test_bertscore.py
@@ -3,6 +3,7 @@ import pytest
 
 from torchmetrics.functional import bertscore
 from torchmetrics.utilities.imports import _BERTSCORE_AVAILABLE
+from torchmetrics.text import BERTScore
 
 
 class CustomAssertions:
@@ -34,8 +35,24 @@ refs = [
     [(preds, refs)],
 )
 @pytest.mark.skipif(not _BERTSCORE_AVAILABLE, reason="test requires bert_score")
-def test_score(preds, refs):
+def test_score_fn(preds, refs):
     Score = bertscore(preds, refs, model_type="roberta-large", num_layers=17, idf=False, batch_size=3)
+    P, R, F, _ = Score["P"], Score["R"], Score["F"], Score["hash_code"]
+
+    CustomAssertions.assertTensorsAlmostEqual(P, [0.9843302369117737, 0.9832239747047424, 0.9120386242866516])
+    CustomAssertions.assertTensorsAlmostEqual(R, [0.9823839068412781, 0.9732863903045654, 0.920428991317749])
+    CustomAssertions.assertTensorsAlmostEqual(F, [0.9833561182022095, 0.9782299995422363, 0.916214644908905])
+
+@pytest.mark.parametrize(
+    "preds,refs",
+    [(preds, refs)],
+)
+@pytest.mark.skipif(not _BERTSCORE_AVAILABLE, reason="test requires bert_score")
+def test_score(preds, refs):
+
+    Scorer = BERTScore(model_type="roberta-large", num_layers=17, idf=False, batch_size=3)
+    Scorer.update(predictions=preds,references=refs)
+    Score = Scorer.compute()
     P, R, F, _ = Score["P"], Score["R"], Score["F"], Score["hash_code"]
 
     CustomAssertions.assertTensorsAlmostEqual(P, [0.9843302369117737, 0.9832239747047424, 0.9120386242866516])
