69fbf4d0f2f0145b389c66c554480005d7d45b9b
Jirka Borovec <Borda@users.noreply.github.com>
2021-08-04 10:31:29 +0200

    Apply suggestions from code review

--- tests/wrappers/test_tracker.py
+++ tests/wrappers/test_tracker.py
@@ -55,25 +55,25 @@ def test_raises_error_if_increment_not_called(method, method_input):
             MeanSquaredError,
             (
                 torch.randn(
-                    50,
+                    50
                 ),
                 torch.randn(
-                    50,
+                    50
                 ),
             ),
-            False,
+            False
         ),
         (
             MeanAbsoluteError,
             (
                 torch.randn(
-                    50,
+                    50
                 ),
                 torch.randn(
-                    50,
+                    50
                 ),
             ),
-            False,
+            False
         ),
     ],
 )
