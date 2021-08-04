14e6fab494175315617c4b98c67aaca7f62c1e2c
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-04 15:06:41 -0700

    Update test_bertscore.py

--- tests/text/test_bertscore.py
+++ tests/text/test_bertscore.py
@@ -3,7 +3,6 @@ from typing import Any
 import numpy as np
 import pytest
 
-from tests.helpers.testers import _assert_allclose
 from torchmetrics.functional import bert_score
 from torchmetrics.text import BERTScore
 from torchmetrics.utilities.imports import _BERTSCORE_AVAILABLE
@@ -35,8 +34,6 @@ def _assert_list(preds: Any, refs: Any, threshold: float = 1e-8):
 @pytest.mark.skipif(not _BERTSCORE_AVAILABLE, reason="test requires bert_score")
 def test_score_fn(preds, refs):
     Score = bert_score(preds, refs, model_type="roberta-large", num_layers=17, idf=False, batch_size=3)
-    print(type(Score))
-    print(type(Score["f1"]))
     _assert_list(Score["precision"], [0.9843302369117737, 0.9832239747047424, 0.9120386242866516])
     _assert_list(Score["recall"], [0.9823839068412781, 0.9732863903045654, 0.920428991317749])
     _assert_list(Score["f1"], [0.9833561182022095, 0.9782299995422363, 0.916214644908905])
@@ -51,8 +48,6 @@ def test_score(preds, refs):
     Scorer = BERTScore(model_type="roberta-large", num_layers=17, idf=False, batch_size=3)
     Scorer.update(predictions=preds, references=refs)
     Score = Scorer.compute()
-    print(type(Score))
-    print(type(Score["f1"]))
     _assert_list(Score["precision"], [0.9843302369117737, 0.9832239747047424, 0.9120386242866516])
     _assert_list(Score["recall"], [0.9823839068412781, 0.9732863903045654, 0.920428991317749])
     _assert_list(Score["f1"], [0.9833561182022095, 0.9782299995422363, 0.916214644908905])
