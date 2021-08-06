eabf02058d06fb15479d25041a4914090bf6de7f
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-06 09:43:04 -0700

    Update bert.py

--- torchmetrics/text/bert.py
+++ torchmetrics/text/bert.py
@@ -61,7 +61,7 @@ class BERTScore(Metric):
         >>> predictions = ["hello there", "general kenobi"]
         >>> references = ["hello there", "general kenobi"]
         >>> bertscore = BERTScore()
-        >>> BERTScore.update(predictions=predictions,references=references)
+        >>> bertscore.update(predictions=predictions,references=references)
         >>> results = bertscore.compute()
         {'f1': [1.0000004768371582, 1.0000003576278687],
         'hashcode': 'roberta-large_L17_no-idf_version=0.3.9(hug_trans=4.9.1)',
