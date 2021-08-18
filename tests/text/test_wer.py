b8d210e25990c89118c8f5b6f28a28d0f1cdcf3c
kingyiusuen <kingyiusuen@gmail.com>
2021-08-18 05:07:20 -0500

    Remove dependency on jiwer for WER (#446)
    * Remove dependency on jiwer for WER
    
    * Add deprecation warning for concatenate_texts and add documnetation
    
    * Use jiwer as reference in tests
    
    * Apply suggestions from review
    
    Co-authored-by: Jirka Borovec <Borda@users.noreply.github.com>
    Co-authored-by: SkafteNicki <skaftenicki@gmail.com>
    (cherry picked from commit 689b2189c6f2aff3968d94d8e5fcfdb85dc5b98a)

--- tests/text/test_wer.py
+++ tests/text/test_wer.py
@@ -8,76 +8,61 @@ if _JIWER_AVAILABLE:
 from torchmetrics.functional.text.wer import wer
 from torchmetrics.text.wer import WER
 
+PREDICTION1 = "hello world"
+REFERENCE1 = "hello world"
+
+PREDICTION2 = "what a day"
+REFERENCE2 = "what a wonderful day"
+
+BATCH_PREDICTIONS = [PREDICTION1, PREDICTION2]
+BATCH_REFERENCES = [REFERENCE1, REFERENCE2]
+
 
 @pytest.mark.parametrize(
-    "hyp,ref,score",
-    [(["hello world"], ["hello world"], 0.0), (["Firwww"], ["hello world"], 1.0)],
+    "prediction,reference",
+    [(PREDICTION1, REFERENCE1), (PREDICTION2, REFERENCE2)],
 )
-@pytest.mark.skipif(not _JIWER_AVAILABLE, reason="test requires jiwer")
-def test_wer_same(hyp, ref, score):
-    """Test to ensure that the torchmetric WER matches reference scores."""
+def test_wer_functional_single_sentence(prediction, reference):
+    """Test functional with strings as inputs."""
+    pl_output = wer(prediction, reference)
+    jiwer_output = compute_measures(reference, prediction)["wer"]
+    assert pl_output == jiwer_output
+
+
+def test_wer_functional_batch():
+    """Test functional with a batch of sentences."""
+    pl_output = wer(BATCH_PREDICTIONS, BATCH_REFERENCES)
+    jiwer_output = compute_measures(BATCH_REFERENCES, BATCH_PREDICTIONS)["wer"]
+    assert pl_output == jiwer_output
+
+
+@pytest.mark.parametrize(
+    "prediction,reference",
+    [(PREDICTION1, REFERENCE1), (PREDICTION2, REFERENCE2)],
+)
+def test_wer_class_single_sentence(prediction, reference):
+    """Test class with strings as inputs."""
     metric = WER()
-    metric.update(hyp, ref)
-    assert metric.compute() == score
+    metric.update(prediction, reference)
+    pl_output = metric.compute()
+    jiwer_output = compute_measures(reference, prediction)["wer"]
+    assert pl_output == jiwer_output
 
 
-@pytest.mark.parametrize(
-    "hyp,ref,expected_score,expected_incorrect,expected_total",
-    [
-        (["hello world"], ["hello world"], 0.0, 0, 2),
-        (["Firwww"], ["hello world"], 1.0, 2, 2),
-    ],
-)
-@pytest.mark.skipif(not _JIWER_AVAILABLE, reason="test requires jiwer")
-def test_wer_functional(ref, hyp, expected_score, expected_incorrect, expected_total):
-    """Test to ensure that the torchmetric functional WER matches the jiwer reference."""
-    assert wer(ref, hyp) == expected_score
-
-
-@pytest.mark.parametrize(
-    "hyp,ref",
-    [
-        (["hello world"], ["hello world"]),
-        (["Firwww"], ["hello world"]),
-    ],
-)
-@pytest.mark.skipif(not _JIWER_AVAILABLE, reason="test requires jiwer")
-def test_wer_reference_functional(hyp, ref):
-    """Test to ensure that the torchmetric functional WER matches the jiwer reference."""
-    assert wer(ref, hyp) == compute_measures(ref, hyp)["wer"]
-
-
-@pytest.mark.skipif(not _JIWER_AVAILABLE, reason="test requires jiwer")
-def test_wer_reference_functional_concatenate():
-    """Test to ensure that the torchmetric functional WER matches the jiwer reference when concatenating."""
-    ref = ["hello world", "hello world"]
-    hyp = ["hello world", "Firwww"]
-    assert wer(ref, hyp) == compute_measures(ref, hyp)["wer"]
-    assert wer(hyp, ref, concatenate_texts=True) == compute_measures("".join(ref), "".join(hyp))["wer"]
-
-
-@pytest.mark.parametrize(
-    "hyp,ref",
-    [
-        (["hello world"], ["hello world"]),
-        (["Firwww"], ["hello world"]),
-    ],
-)
-@pytest.mark.skipif(not _JIWER_AVAILABLE, reason="test requires jiwer")
-def test_wer_reference(hyp, ref):
-    """Test to ensure that the torchmetric WER matches the jiwer reference."""
+def test_wer_class_batch():
+    """Test class with a batch of sentences."""
     metric = WER()
-    metric.update(hyp, ref)
-    assert metric.compute() == compute_measures(ref, hyp)["wer"]
+    metric.update(BATCH_PREDICTIONS, BATCH_REFERENCES)
+    pl_output = metric.compute()
+    jiwer_output = compute_measures(BATCH_REFERENCES, BATCH_PREDICTIONS)["wer"]
+    assert pl_output == jiwer_output
 
 
-@pytest.mark.skipif(not _JIWER_AVAILABLE, reason="test requires jiwer")
-def test_wer_reference_batch():
-    """Test to ensure that the torchmetric WER matches the jiwer reference with accumulation."""
-    batches = [("hello world", "Firwww"), ("hello world", "hello world")]
+def test_wer_class_batches():
+    """Test class with two batches of sentences."""
     metric = WER()
-
-    for hyp, ref in batches:
-        metric.update(ref, hyp)
-    reference_score = compute_measures(truth=[x[0] for x in batches], hypothesis=[x[1] for x in batches])["wer"]
-    assert metric.compute() == reference_score
+    for prediction, reference in zip(BATCH_PREDICTIONS, BATCH_REFERENCES):
+        metric.update(prediction, reference)
+    pl_output = metric.compute()
+    jiwer_output = compute_measures(BATCH_REFERENCES, BATCH_PREDICTIONS)["wer"]
+    assert pl_output == jiwer_output
