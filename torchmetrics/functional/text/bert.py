f72661a845756189ef52f2faa4fcdce5fe6e9ac7
Stancl, Daniel <daniel.stancl@firma.seznam.cz>
2021-08-18 22:54:53 +0200

    [WIP] Start adding own BERTScore implementation

--- torchmetrics/functional/text/bert.py
+++ torchmetrics/functional/text/bert.py
@@ -11,12 +11,191 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Dict, List, Optional
+import math
+from collections import defaultdict
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
-from torchmetrics.utilities.imports import _BERTSCORE_AVAILABLE
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchmetrics.utilities.imports import _BERTSCORE_AVAILABLE, _TRANSFORMERS_AVAILABLE
 
 if _BERTSCORE_AVAILABLE:
     from bert_score import BERTScorer, get_hash, lang2model, model2layers
+
+if _TRANSFORMERS_AVAILABLE:
+    from transformers import AutoTokenizer, AutoModel
+
+
+def _preprocess_text(text: List[str], tokenizer: Any, max_length: int = 512) -> Dict[str, torch.Tensor]:
+    """Default text pre-processing function using `transformers` `AutoTokenizer` instance.
+
+    Args:
+        text: An iterable of sentences.
+        tokenizer: `AutoTokenizer` instance from `transformers` package.
+        max_length: A maximum sequence length.
+
+    Return:
+        A dictionary of tokenized sentences including input_ids and attention_mask.
+    """
+    return tokenizer(
+        text,
+        add_special_tokens=False,  # we don't want to add [CLS], [SEP] either other special tokens
+        padding=True,
+        max_length=max_length,
+        truncation=True,
+        return_tensors="pt",
+    )
+
+
+class TextDataset(Dataset):
+
+    def __init__(
+        self,
+        text: List[str],
+        tokenizer: Any,
+        max_length: int = 512,
+        preprocess_text_fn: Callable[[List[str], Any, int], Dict[str, torch.Tensor]] = _preprocess_text,
+        tokens_idf: Optional[Dict[int, float]] = None,
+    ) -> None:
+        """
+        Args:
+            text: An iterable of sentences.
+            tokenizer: `AutoTokenizer` instance from `transformers` package.
+            max_length: A maximum sequence length.
+            tokens_idf: Inverse document frequences (these should be calculated on reference sentences).
+        """
+        self.text = preprocess_text_fn(text, tokenizer, max_length)
+        self.num_sentences = len(text)
+        self.tokens_idf = tokens_idf if tokens_idf is not None else self._get_tokens_idf()
+
+    def __getitem__(self, idx: int) -> Dict[str, torch.Tensor]:
+        input_ids = self.text["input_ids"][idx, :]
+        attention_mask = self.text["attention_mask"][idx, :]
+        input_ids_idf = torch.tensor([self.tokens_idf[input_idx] for input_idx in input_ids.tolist()])
+        return {"input_ids": input_ids, "attention_mask": attention_mask, "input_ids_idf": input_ids_idf}
+
+    def __len__(self) -> int:
+        return self.num_sentences
+
+    def _get_tokens_idf(self) -> Dict[int, float]:
+        """Calculate token inverse document frequences.
+
+        Return:
+            A python dictionary containing inverse document frequences for token ids.
+        """
+        unique_ids, ids_occurrence = self.text["input_ids"].unique(return_counts=True)
+
+        tokens_idf: Dict[int, float] = defaultdict(lambda: math.log((self.num_sentences + 1) / 1))
+        tokens_idf.update(
+            {
+                idx: math.log((self.num_sentences + 1) / (occurrence + 1))
+                for idx, occurrence in zip(unique_ids.tolist(), ids_occurrence.tolist())
+            }
+        )
+        return tokens_idf
+
+
+def _get_embeddings_and_idf_scale(
+    dataloader: DataLoader,
+    model: torch.nn.Module,
+    device: Optional[Union[str, torch.device]] = None,
+    idf: bool = False,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    """Calculate sentence embeddings and the inverse-document-frequence scaling factor.
+    TODO:
+    Args:
+        dataloader:
+        model:
+        device:
+        idf:
+
+    Return:
+    """
+    EMBEDDINGS_LIST: List[torch.Tensor] = []
+    IDF_SCALE_LIST: List[torch.Tensor] = []
+    for batch in dataloader:
+        with torch.no_grad():
+            out = model(batch["input_ids"].to(device), batch["attention_mask"].to(device))[0]
+        out /= out.norm(dim=-1).unsqueeze(-1)  # normalize embeddings
+        # Multiply embeddings with attention_mask (b=batch_size, s=seq_len, d=emb_dim)
+        out = torch.einsum("bsd, bs -> bsd", out, batch["attention_mask"])
+        EMBEDDINGS_LIST.append(out.cpu())
+
+        # Calculate idf scaling factor if desired. Otherwise take a vector of ones
+        idf_scale = (batch["input_ids_idf"] * batch["attention_mask"]).sum(-1) if idf else torch.ones(out.shape[0])
+        IDF_SCALE_LIST.append(idf_scale)
+
+    EMBEDDINGS = torch.cat(EMBEDDINGS_LIST)
+    IDF_SCALE = torch.cat(IDF_SCALE_LIST)
+
+    return EMBEDDINGS, IDF_SCALE
+
+
+def _get_precision_recall_f1(
+    pred_embeddings: torch.Tensor,
+    ref_embeddings: torch.Tensor,
+    pred_idf_scale: torch.Tensor,
+    ref_idf_scale: torch.Tensor,
+) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    """Calculate precision, recall and F1 score over candidate and reference sentences.
+
+    Args:
+        pred_embeddings: Embeddings of candidate sentenecs.
+        ref_embeddings: Embeddings of reference sentences.
+        pred_idf_scale: An IDF scale factor for candidate sentences.
+        ref_idf_scale: An IDF scale factor for reference sentences.
+
+    Return:
+        Tensors containing precision, recall and F1 score, respectively.
+    """
+    cos_sim = torch.bmm(pred_embeddings, ref_embeddings.transpose(1, 2))
+    precision = cos_sim.max(dim=2).values.mean(-1) / pred_idf_scale
+    recall = cos_sim.max(dim=1).values.mean(-1) / ref_idf_scale
+    f1_score = 2 * precision * recall / (precision * recall)
+    f1_score = f1_score.masked_fill(torch.isnan(f1_score), 0.0)
+
+    return precision, recall, f1_score
+
+
+def new_bert_score(
+    predictions: List[str],
+    references: List[str],
+    lang: str = "en",
+    model_type: Optional[str] = None,
+    model_path: Optional[str] = None,
+    own_model: Optional[torch.nn.Module] = None,
+    idf: bool = False,
+    device: Optional[Union[str, torch.device]] = None,
+    max_length: int = 512,
+    batch_size: int = 64,
+    num_threads: int = 4,
+) -> Dict[str, List[float]]:
+    if not own_model:
+        if not _TRANSFORMERS_AVAILABLE:
+            raise ValueError("#TODO: Transformers must be installed.")
+        tokenizer = AutoTokenizer.from_pretrained(model_path)
+        model = AutoModel.from_pretrained(model_path)
+        model.eval()
+        model.to(device)
+
+    ref_dataset = TextDataset(references, tokenizer, max_length)
+    pred_dataset = TextDataset(predictions, tokenizer, max_length, tokens_idf=ref_dataset.tokens_idf)
+    ref_loader = DataLoader(ref_dataset, batch_size=batch_size, num_workers=num_threads)
+    pred_loader = DataLoader(pred_dataset, batch_size=batch_size, num_workers=num_threads)
+
+    ref_embeddings, ref_idf_scale = _get_embeddings_and_idf_scale(ref_loader, model, device, idf)
+    pred_embeddings, pred_idf_scale = _get_embeddings_and_idf_scale(pred_loader, model, device, idf)
+
+    precision, recall, f1_score = _get_precision_recall_f1(
+        pred_embeddings, ref_embeddings, pred_idf_scale, ref_idf_scale
+    )
+
+    output_dict = {
+        "f1": f1_score.tolist(),
+        "precision": precision.tolist(),
+        "recall": recall.tolist(),
+    }
+    return output_dict
 
 
 def bert_score(
