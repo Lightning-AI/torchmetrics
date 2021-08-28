5bbc2d95c1ea4bb5e5ff6f21549d584bfb466826
SkafteNicki <skaftenicki@gmail.com>
2021-08-28 11:19:00 +0200

    change

--- torchmetrics/__init__.py
+++ torchmetrics/__init__.py
@@ -2,7 +2,7 @@ r"""Root package info."""
 import logging as __logging
 import os
 
-from torchmetrics.__about__ import *  # noqa: F403
+from torchmetrics.__about__ import *  # noqa: F401, F403
 
 _logger = __logging.getLogger("torchmetrics")
 _logger.addHandler(__logging.StreamHandler())
@@ -11,18 +11,70 @@ _logger.setLevel(__logging.INFO)
 _PACKAGE_ROOT = os.path.dirname(__file__)
 _PROJECT_ROOT = os.path.dirname(_PACKAGE_ROOT)
 
+from torchmetrics import functional  # noqa: E402
+from torchmetrics.audio import PIT, SI_SDR, SI_SNR, SNR  # noqa: E402
 from torchmetrics.average import AverageMeter  # noqa: E402
+from torchmetrics.classification import (  # noqa: E402
+    AUC,
+    AUROC,
+    F1,
+    ROC,
+    Accuracy,
+    AveragePrecision,
+    BinnedAveragePrecision,
+    BinnedPrecisionRecallCurve,
+    BinnedRecallAtFixedPrecision,
+    CalibrationError,
+    CohenKappa,
+    ConfusionMatrix,
+    FBeta,
+    HammingDistance,
+    Hinge,
+    IoU,
+    KLDivergence,
+    MatthewsCorrcoef,
+    Precision,
+    PrecisionRecallCurve,
+    Recall,
+    Specificity,
+    StatScores,
+)
 from torchmetrics.collections import MetricCollection  # noqa: E402
-from torchmetrics.image import FID, IS, KID, LPIPS, PSNR, SSIM  # noqa: E402
+from torchmetrics.image import PSNR, SSIM  # noqa: E402
 from torchmetrics.metric import Metric  # noqa: E402
+from torchmetrics.regression import (  # noqa: E402
+    CosineSimilarity,
+    ExplainedVariance,
+    MeanAbsoluteError,
+    MeanAbsolutePercentageError,
+    MeanSquaredError,
+    MeanSquaredLogError,
+    PearsonCorrcoef,
+    R2Score,
+    SpearmanCorrcoef,
+    SymmetricMeanAbsolutePercentageError,
+)
+from torchmetrics.retrieval import (  # noqa: E402
+    RetrievalFallOut,
+    RetrievalMAP,
+    RetrievalMRR,
+    RetrievalNormalizedDCG,
+    RetrievalPrecision,
+    RetrievalRecall,
+)
+from torchmetrics.text import BLEUScore, WER  # noqa: E402
+from torchmetrics.wrappers import BootStrapper, MetricTracker  # noqa: E402
 
 __all__ = [
+    "functional",
+    "Accuracy",
+    "AUC",
+    "AUROC",
     "AverageMeter",
     "AveragePrecision",
     "BinnedAveragePrecision",
     "BinnedPrecisionRecallCurve",
     "BinnedRecallAtFixedPrecision",
-    "BERTScore",
     "BLEUScore",
     "BootStrapper",
     "CalibrationError",
@@ -32,14 +84,10 @@ __all__ = [
     "ExplainedVariance",
     "F1",
     "FBeta",
-    "FID",
     "HammingDistance",
     "Hinge",
     "IoU",
-    "IS",
-    "KID",
     "KLDivergence",
-    "LPIPS",
     "MatthewsCorrcoef",
     "MeanAbsoluteError",
     "MeanAbsolutePercentageError",
@@ -47,4 +95,28 @@ __all__ = [
     "MeanSquaredLogError",
     "Metric",
     "MetricCollection",
+    "MetricTracker",
+    "PearsonCorrcoef",
+    "PIT",
+    "Precision",
+    "PrecisionRecallCurve",
+    "PSNR",
+    "R2Score",
+    "Recall",
+    "RetrievalFallOut",
+    "RetrievalMAP",
+    "RetrievalMRR",
+    "RetrievalNormalizedDCG",
+    "RetrievalPrecision",
+    "RetrievalRecall",
+    "ROC",
+    "SI_SDR",
+    "SI_SNR",
+    "SNR",
+    "SpearmanCorrcoef",
+    "Specificity",
+    "SSIM",
+    "StatScores",
+    "SymmetricMeanAbsolutePercentageError",
+    "WER",
 ]
