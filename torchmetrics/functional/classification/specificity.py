24e5c6a5152e8c1d9ba99cb6305ce6257dcf8528
XFarooqi <58102434+XFarooqi@users.noreply.github.com>
2021-09-11 12:04:52 +0500

    Update specificity.py

--- torchmetrics/functional/classification/specificity.py
+++ torchmetrics/functional/classification/specificity.py
@@ -79,7 +79,7 @@ def specificity(
     multiclass: Optional[bool] = None,
 ) -> Tensor:
     r"""
-    Computes `Specificity <https://en.wikipedia.org/wiki/Sensitivity_and_specificity>`_:
+    `Specificity`_
 
     .. math:: \text{Specificity} = \frac{\text{TN}}{\text{TN} + \text{FP}}
 
