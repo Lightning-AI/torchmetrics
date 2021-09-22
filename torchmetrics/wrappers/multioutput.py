b0c0782b7bfd233b016fa42bcf96a0806e300663
Jirka Borovec <Borda@users.noreply.github.com>
2021-09-22 10:39:35 +0200

    Apply suggestions from code review

--- torchmetrics/wrappers/multioutput.py
+++ torchmetrics/wrappers/multioutput.py
@@ -37,32 +37,32 @@ class MultioutputWrapper(Metric):
     (parameter `remove_nans`) on a per-output basis. When `remove_nans` is passed the wrapper will remove all rows
 
     Args:
-        base_metric: Metric,
+        base_metric:
             Metric being wrapped.
-        num_outputs: int
+        num_outputs:
             Expected dimensionality of the output dimension. This parameter is
             used to determine the number of distinct metrics we need to track.
-        output_dim: int = -1
+        output_dim:
             Dimension on which output is expected. Note that while this provides some flexibility, the output dimension
             must be the same for all inputs to update. This applies even for metrics such as `Accuracy` where the labels
             can have a different number of dimensions than the predictions. This can be worked around if the output
             dimension can be set to -1 for both, even if -1 corresponds to different dimensions in different inputs.
-        remove_nans: bool = True
+        remove_nans:
             Whether to remove the intersection of rows containing NaNs from the values passed through to each underlying
             metric. Proper operation requires all tensors passed to update to have dimension `(N, ...)` where N
             represents the length of the batch or dataset being passed in.
-        squeeze_outputs: bool = True
+        squeeze_outputs:
             If true, will squeeze the 1-item dimensions left after `index_select` is applied.
             This is sometimes unnecessary but harmless for metrics such as `R2Score` but useful
             for certain classification metrics that can't handle additional 1-item dimensions.
-        compute_on_step: bool = True
+        compute_on_step:
             Whether to recompute the metric value on each update step.
-        dist_sync_on_step: bool = False
-            Required for distributed training support. See torchmetrics docs for additional details.
-        process_group: Optional[Any]
-            Specify the process group on which synchronization is called. default: None (which selects the entire world)
-        dist_sync_fn: Callable = None,
-            Required for distributed training support. See torchmetrics docs for additional details.
+        dist_sync_on_step:
+            Required for distributed training support.
+        process_group:
+            Specify the process group on which synchronization is called. The default: None (which selects the entire world)
+        dist_sync_fn:
+            Required for distributed training support.
 
     Example:
 
