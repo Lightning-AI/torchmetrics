c27c9d93121f16b44f8cf6ba1d358ad46ed31315
Daniel Stancl <46073029+stancld@users.noreply.github.com>
2021-09-30 20:29:12 +0200

    Add `SacreBLEUScore` (#546)
    * Add some basic code for SacreBLEU
    
    * Add lowercase argument, update docs + init test file
    
    * Add some minor updates
    
    * Add some basic tests
    
    * Change typing.Literal to typing_extensions.Literal
    
    * Fix a few bugs
    
    * Update test requirements and add import utility
    
    * Update doc + add regex to text requirements
    
    * Update docs
    
    * Fix decimal rounds in tests
    
    * Make regex import conditional
    
    * changelog
    
    * Apply SkafteNicki's suggestions
    
    * Add  tokenizer into the tests
    
    * Apply suggestions from code review
    
    * Apply the Borda's suggestion + fix some docs
    
    * Fix some import after the model name change
    
    * fix typing
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Jirka Borovec <Borda@users.noreply.github.com>
    Co-authored-by: SkafteNicki <skaftenicki@gmail.com>

--- torchmetrics/functional/text/__init__.py
+++ torchmetrics/functional/text/__init__.py
@@ -13,4 +13,5 @@
 # limitations under the License.
 
 from torchmetrics.functional.text.bleu import bleu_score  # noqa: F401
+from torchmetrics.functional.text.sacre_bleu import sacre_bleu_score  # noqa: F401
 from torchmetrics.functional.text.wer import wer  # noqa: F401
