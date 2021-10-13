26da7455fd4ae84446397635734ee55f25d88b64
Jan-Henrik Lambrechts <janhenrik.lambrechts@outlook.be>
2021-10-13 11:20:24 +0100

    implemented wrapper design and modified tests

--- tests/wrappers/test_minmax.py
+++ tests/wrappers/test_minmax.py
@@ -1,27 +1,35 @@
 import pytest
 import torch
+from torchmetrics.wrappers import MinMaxMetric
+from torchmetrics.classification import Accuracy
 
 
 @pytest.mark.parametrize("device", ["cpu", "cuda"])
 @pytest.mark.parametrize("type", ["min", "max"])
 def test_minmax(device, type):
     """test that both min and max versions of MinMaxMetric operate correctly after calling compute."""
-    m = MinMaxMetric()
     acc = Accuracy()
+    min_max_acc = MinMaxMetric(acc)
 
     preds_1 = torch.Tensor([[0.9, 0.1], [0.2, 0.8]])
     preds_2 = torch.Tensor([[0.1, 0.9], [0.2, 0.8]])
     preds_3 = torch.Tensor([[0.1, 0.9], [0.8, 0.2]])
     labels = torch.Tensor([[0, 1], [0, 1]]).long()
 
-    acc(preds_1, labels)  # acc is 0.5
-    m(acc.compute())  # max_metrix is 0.5
-    assert m.compute() == 0.5
+    min_max_acc(preds_1, labels)  
+    acc = min_max_acc.compute()  
+    assert acc["raw"] == 0.5
+    assert acc["max"] == 0.5
+    assert acc["min"] == 0.5
 
-    acc(preds_2, labels)  # acc is 1.
-    m(acc.compute())  # max_metrix is 1.
-    assert m.compute() == 1.0
+    min_max_acc(preds_2, labels)
+    acc = min_max_acc.compute()
+    assert acc["raw"] == 1.0
+    assert acc["max"] == 1.0
+    assert acc["min"] == 0.5
 
-    acc(preds_3, labels)  # acc is 0.5
-    m(acc.compute())  # max_metrix is 1.
-    assert m.compute() == 1.0
+    min_max_acc(preds_3, labels)
+    acc = min_max_acc.compute()
+    assert acc["raw"] == 0.5
+    assert acc["max"] == 1.0
+    assert acc["min"] == 0.5
--- torchmetrics/wrappers/minmax.py
+++ torchmetrics/wrappers/minmax.py
@@ -1,38 +1,48 @@
-from pytorch_lightning.utilities.distributed import gather_all_tensors
+ # Copyright The PyTorch Lightning team. 
+ # 
+ # Licensed under the Apache License, Version 2.0 (the "License"); 
+ # you may not use this file except in compliance with the License. 
+ # You may obtain a copy of the License at 
+ # 
+ #     http://www.apache.org/licenses/LICENSE-2.0 
+ # 
+ # Unless required by applicable law or agreed to in writing, software 
+ # distributed under the License is distributed on an "AS IS" BASIS, 
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ # See the License for the specific language governing permissions and 
+ # limitations under the License. 
 
+import torch
 from torchmetrics.metric import Metric
-
+from typing import Any
 
 class MinMaxMetric(Metric):
-    """Pytorch-Lightning Metric that tracks the maximum value of a scalar/tensor across an experiment."""
+    """Wrapper Metric that tracks both the minimum and maximum of a scalar/tensor across an experiment."""
 
-    def __init__(self, dist_sync_on_step=False):
+    def __init__(self, base_metric: Metric, dist_sync_on_step:bool=False, min_bound_init:float=1., max_bound_init:float=0.):
         super().__init__(dist_sync_on_step=dist_sync_on_step)
-        self.add_state("max_val", default=torch.tensor(0))
+        self._base_metric = base_metric
+        self.add_state("min_val", default=torch.tensor(min_bound_init))
+        self.add_state("max_val", default=torch.tensor(max_bound_init))
+        self.min_bound_init = min_bound_init
+        self.max_bound_init = max_bound_init
 
-    def _wrap_compute(self, compute):
-        def wrapped_func(*args, **kwargs):
-            # return cached value
-            if self._computed is not None:
-                return self._computed
-
-            dist_sync_fn = self.dist_sync_fn
-            if dist_sync_fn is None and torch.distributed.is_available() and torch.distributed.is_initialized():
-                # User provided a bool, so we assume DDP if available
-                dist_sync_fn = gather_all_tensors
-
-            if self._to_sync and dist_sync_fn is not None:
-                self._sync_dist(dist_sync_fn)
-
-            self._computed = compute(*args, **kwargs)
-            # removed the auto-reset
-
-            return self._computed
-
-        return wrapped_func
-
-    def update(self, val):
-        self.max_val = val if self.max_val < val else self.max_val
+    def update(self, *args: Any, **kwargs: Any):
+        "Update underlying metric"
+        self._base_metric.update(*args, **kwargs)
+        
 
     def compute(self):
-        return self.max_val
+        "Compute underlying metric as well as max and min values."
+        val = self._base_metric.compute()
+        self.max_val = val if self.max_val < val else self.max_val
+        self.min_val = val if self.min_val > val else self.min_val
+        return {"raw" : val, "max" : self.max_val, "min" : self.min_val}
+
+    def reset(self):
+        "Sets max_val and min_val to 0. and resets the base metric."
+        self.max_val = self.max_bound_init
+        self.min_val = self.min_bound_init
+        self._base_metric.reset()
+
+
