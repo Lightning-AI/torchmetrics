0d91116f64cdff79cb2b5faa1c51449a41290585
mahinlma <nmaheshwariece@gmail.com>
2021-10-23 00:49:13 +0530

    Update test_cer.py

--- tests/text/test_cer.py
+++ tests/text/test_cer.py
@@ -1,51 +1,66 @@
-from typing import Callable
+from typing import Callable, List, Union
 
 import pytest
 
 from tests.text.helpers import INPUT_ORDER, TextTester
+from torchmetrics.utilities.imports import _JIWER_AVAILABLE
+
+if _JIWER_AVAILABLE:
+    from jiwer import compute_measures
+else:
+    compute_measures = Callable
+
 from torchmetrics.functional.text.cer import char_error_rate
 from torchmetrics.text.cer import CharErrorRate
 
-BATCHES_1 = {"preds": [["hello world"], ["what a day"]], "targets": [["hello world"], ["what a wonderful day"]]}
 
-BATCHES_2 = {
+BATCHES = {
     "preds": [
-        ["i like python", "what you mean or swallow"],
-        ["hello duck", "i like python"],
+        ['A', ' ', 'q', 'u', 'i', 'c', 'k', ' ', 'b', 'r', 'o', 'w', 'n', ' ', 'f', 'o'],
     ],
     "targets": [
-        ["i like monthy python", "what do you mean, african or european swallow"],
-        ["hello world", "i like monthy python"],
+        ['A', ' ', 'q', 'u', 'i', 'c', 'k', ' ', 'b', 'r', 'o', 'w', 'n', ' ', 'f', 'o', 'x'],
     ],
 }
 
 
-class TestCER(TextTester):
+def _compute_wer_metric_jiwer(prediction: Union[str, List[str]], reference: Union[str, List[str]]):
+    return compute_measures(reference, prediction)["wer"]
+
+
+@pytest.mark.skipif(not _JIWER_AVAILABLE, reason="test requires jiwer")
+@pytest.mark.parametrize(
+    ["preds", "targets"],
+    [
+        pytest.param(BATCHES["preds"], BATCHES["targets"]),
+    ],
+)
+class TestWER(TextTester):
     @pytest.mark.parametrize("ddp", [False, True])
     @pytest.mark.parametrize("dist_sync_on_step", [False, True])
-    def test_cer_class(self, ddp, dist_sync_on_step, preds, targets):
+    def test_wer_class(self, ddp, dist_sync_on_step, preds, targets):
 
         self.run_class_metric_test(
             ddp=ddp,
             preds=preds,
             targets=targets,
             metric_class=CharErrorRate,
-            sk_metric=Callable,
+            sk_metric=_compute_wer_metric_jiwer,
             dist_sync_on_step=dist_sync_on_step,
             input_order=INPUT_ORDER.PREDS_FIRST,
         )
 
-    def test_cer_functional(self, preds, targets):
+    def test_wer_functional(self, preds, targets):
 
         self.run_functional_metric_test(
             preds,
             targets,
             metric_functional=char_error_rate,
-            sk_metric=Callable,
+            sk_metric=_compute_wer_metric_jiwer,
             input_order=INPUT_ORDER.PREDS_FIRST,
         )
 
-    def test_cer_differentiability(self, preds, targets):
+    def test_wer_differentiability(self, preds, targets):
 
         self.run_differentiability_test(
             preds=preds,
