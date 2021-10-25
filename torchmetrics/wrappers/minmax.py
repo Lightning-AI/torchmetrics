68698150339e0ac92484b3d9c9fa3db24bd70e02
SkafteNicki <skaftenicki@gmail.com>
2021-10-25 12:13:49 +0200

    fix implementation

--- torchmetrics/wrappers/minmax.py
+++ torchmetrics/wrappers/minmax.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Dict, Union
+from typing import Any, Callable, Dict, Union, Optional
 
 import torch
 from torch import Tensor
@@ -20,24 +20,27 @@ from torch import Tensor
 from torchmetrics.metric import Metric
 
 
-def _is_suitable_val(val: Union[int, float, Tensor]) -> bool:
-    """Utility function that checks whether min/max value."""
-    if (type(val) == int) or (type(val) == float):
-        return True
-    elif type(val) == torch.Tensor:
-        return val.numel() == 1
-    return False
-
-
 class MinMaxMetric(Metric):
-    """Wrapper Metric that tracks both the minimum and maximum of a scalar/tensor across an experiment.
+    """ Wrapper Metric that tracks both the minimum and maximum of a scalar/tensor across an experiment. The min/max
+    value will be updated each time `.compute` is called.
 
     Args:
         base_metric:
             The metric of which you want to keep track of its maximum and minimum values.
+        compute_on_step:
+            Forward only calls ``update()`` and return None if this is set to False. default: True
         dist_sync_on_step:
             Synchronize metric state across processes at each ``forward()``
-            before returning the value at the step.
+            before returning the value at the step. default: False
+        process_group:
+            Specify the process group on which synchronization is called. default: None (which selects the entire world)
+        dist_sync_fn:
+            Callback that performs the allgather operation on the metric state. When ``None``, DDP
+            will be used to perform the allgather
+
+    Raises:
+        ValueError
+            If ``base_metric` argument is not an subclasses instance of ``torchmetrics.Metric``
 
     Example::
             >>> import torch
@@ -60,18 +63,24 @@ class MinMaxMetric(Metric):
     def __init__(
         self,
         base_metric: Metric,
+        compute_on_step: bool = True,
         dist_sync_on_step: bool = False,
-    ):
-        super().__init__(dist_sync_on_step=dist_sync_on_step)
+        process_group: Optional[Any] = None,
+        dist_sync_fn: Callable = None,
+    ) -> None:
+        super().__init__(
+            compute_on_step=compute_on_step,
+            dist_sync_on_step=dist_sync_on_step,
+            process_group=process_group,
+            dist_sync_fn=dist_sync_fn,
+        )
         if not isinstance(base_metric, Metric):
             raise ValueError(
                 f"Expected base metric to be an instance of torchmetrics.Metric but received {base_metric}"
             )
         self._base_metric = base_metric
-        self.add_state("min_val", default=torch.tensor(min_bound_init), dist_reduce_fx="min")
-        self.add_state("max_val", default=torch.tensor(max_bound_init), dist_reduce_fx="max")
-        self.min_bound_init = float("inf")
-        self.max_bound_init = float("-inf")
+        self.add_state("min_val", default=torch.tensor(float("inf")), dist_reduce_fx="min")
+        self.add_state("max_val", default=torch.tensor(float("-inf")), dist_reduce_fx="max")
 
     def update(self, *args: Any, **kwargs: Any) -> None:  # type: ignore
         """Updates the underlying metric."""
@@ -84,7 +93,8 @@ class MinMaxMetric(Metric):
         (``max``) values.
         """
         val = self._base_metric.compute()
-        assert _is_suitable_val(val), "Computed Base Metric should be a scalar (Int, Float or Tensor of Size 1)"
+        if not self._is_suitable_val(val):
+            raise RuntimeError('Returned value from base metric should be a scalar (int, float or tensor of size 1, but got {val}')
         self.max_val = val if self.max_val < val else self.max_val
         self.min_val = val if self.min_val > val else self.min_val
         return {"raw": val, "max": self.max_val, "min": self.min_val}
@@ -93,3 +103,13 @@ class MinMaxMetric(Metric):
         """Sets ``max_val`` and ``min_val`` to the initialization bounds and resets the base metric."""
         super().reset()
         self._base_metric.reset()
+
+    @staticmethod
+    def _is_suitable_val(val: Union[int, float, Tensor]) -> bool:
+        """Utility function that checks whether min/max value."""
+        if (type(val) == int) or (type(val) == float):
+            return True
+        elif type(val) == torch.Tensor:
+            return val.numel() == 1
+        return False
+
