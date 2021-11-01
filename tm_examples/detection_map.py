fe4f1dc00cfb33d88ff583212a118ec49868971f
tobias-kupek-swarm <tobias.kupek@swarm-analytics.com>
2021-11-01 12:30:55 +0100

    simplify named tuple

--- tm_examples/detection_map.py
+++ tm_examples/detection_map.py
@@ -1,29 +1,24 @@
-from collections import namedtuple
-
 import torch
 
 from torchmetrics import MAP
 
-Input = namedtuple("Input", ["preds", "target"])
+preds = [
+    dict(
+        boxes=torch.Tensor([[258., 41., 606., 285.]]),
+        scores=torch.Tensor([0.536]),
+        labels=torch.IntTensor([0]),
+    )
+]
 
-inputs = Input(
-    preds=[
-        dict(
-            boxes=torch.Tensor([[258., 41., 606., 285.]]),
-            scores=torch.Tensor([0.536]),
-            labels=torch.IntTensor([0]),
-        )  # coco image id 42
-    ],
-    target=[
-        dict(
-            boxes=torch.Tensor([[214., 41., 562., 285.]]),
-            labels=torch.IntTensor([0]),
-        )  # coco image id 42
-    ]
-)
+target = [
+    dict(
+        boxes=torch.Tensor([[214., 41., 562., 285.]]),
+        labels=torch.IntTensor([0]),
+    )
+]
 
 if __name__ == "__main__":
-    metric = MAP(class_metrics=True)
-    metric.update(inputs.preds, inputs.target)
+    metric = MAP()
+    metric.update(preds, target)
     result = metric.compute()
-    print(result)
+    print(result['map'].cpu().item())
