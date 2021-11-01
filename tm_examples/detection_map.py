e118db83da8c3a6a2277338b8f6a39151778e62e
tobias-kupek-swarm <tobias.kupek@swarm-analytics.com>
2021-11-01 13:05:50 +0100

    add some more comments

--- tm_examples/detection_map.py
+++ tm_examples/detection_map.py
@@ -25,11 +25,14 @@ from torchmetrics import MAP
 preds = [
     dict(
         # The boxes keyword should contain an [N,4] tensor,
-        # where N is the number of detected boxes and ...
+        # where N is the number of detected boxes with boxes of the format
+        # [xmin, ymin, xmax, ymax] in absolute image coordinates
         boxes=torch.Tensor([[258.0, 41.0, 606.0, 285.0]]),
         # The scores keyword should contain an [N,] tensor where
         # each element is confidence score between 0 and 1
         scores=torch.Tensor([0.536]),
+        # The labels keyword should contain an [N,] tensor
+        # with integers of the predicted classes
         labels=torch.IntTensor([0]),
     )
 ]
@@ -37,7 +40,7 @@ preds = [
 # Target should be a list of elements, where each element is a dict
 # containing 2 keys: boxes and labels. Each keyword should be formatted
 # similar to the preds argument. The number of elements in preds and
-# target should match
+# target need to match
 target = [
     dict(
         boxes=torch.Tensor([[214.0, 41.0, 562.0, 285.0]]),
@@ -46,7 +49,12 @@ target = [
 ]
 
 if __name__ == "__main__":
+    # initialize metric
     metric = MAP()
+
+    # update metric with predictions and respective ground truth
     metric.update(preds, target)
+
+    # after adding the data, compute the results
     result = metric.compute()
     print(result)
