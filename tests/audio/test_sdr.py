9ba11d76125aa76581b8c0c75661104bf85c92b5
quancs <1017241746@qq.com>
2021-11-08 21:58:03 +0800

    Update tests/audio/test_sdr.py
    Co-authored-by: Nicki Skafte Detlefsen <skaftenicki@gmail.com>

--- tests/audio/test_sdr.py
+++ tests/audio/test_sdr.py
@@ -30,12 +30,12 @@ seed_all(42)
 Input = namedtuple("Input", ["preds", "target"])
 
 inputs_1spk = Input(
-    preds=torch.rand(3, 2, 1, 8000),
-    target=torch.rand(3, 2, 1, 8000),
+    preds=torch.rand(4, 2, 1, 8000),
+    target=torch.rand(4, 2, 1, 8000),
 )
 inputs_2spk = Input(
-    preds=torch.rand(3, 2, 2, 8000),
-    target=torch.rand(3, 2, 2, 8000),
+    preds=torch.rand(4, 2, 2, 8000),
+    target=torch.rand(4, 2, 2, 8000),
 )
 
 
