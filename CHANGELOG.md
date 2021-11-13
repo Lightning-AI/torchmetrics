7f92f04583ef3e9a4d2b094e644e9653c409d8ed
mahinlma <nmaheshwariece@gmail.com>
2021-11-13 23:02:48 +0530

    update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,7 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 - Added NLP metrics:
-  - `MatchErrorRate` ([]())
+  - `MatchErrorRate` ([#619](https://github.com/PyTorchLightning/metrics/pull/619))
 
 
 ### Changed
--- torchmetrics/functional/text/mer.py
+++ torchmetrics/functional/text/mer.py
@@ -88,7 +88,7 @@ def match_error_rate(
     predictions: Union[str, List[str]],
     references: Union[str, List[str]],
 ) -> Tensor:
-    """Match error rate (MatchErrorRate_) is a metric of the performance of an automatic speech recognition system.
+    """Match error rate is a metric of the performance of an automatic speech recognition system.
     This value indicates the percentage of words that were incorrectly predicted and inserted. The lower the value,
     the better the performance of the ASR system with a MatchErrorRate of 0 being a perfect score.
 
