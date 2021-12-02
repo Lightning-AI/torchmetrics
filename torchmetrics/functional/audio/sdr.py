154c463b4312beb318a13265fe8c202787f39c90
quancs <quancs@qq.com>
2021-12-02 17:13:59 +0800

    try fix doctest

--- torchmetrics/functional/audio/sdr.py
+++ torchmetrics/functional/audio/sdr.py
@@ -55,8 +55,8 @@ def sdr(
 
     .. note:: 1) using this metrics requires you to have ``fast-bss-eval`` install. Either install as ``pip install
         torchmetrics[audio]`` or ``pip install fast-bss-eval``
-        2) preds and target need to have the same dtype, otherwise target will be converted to preds' dtype
-        3) when pytorch<1.8.0, numpy will be used to calculate this metric, which causes ``sdr`` non-differentiable
+    .. note:: 2) preds and target need to have the same dtype, otherwise target will be converted to preds' dtype
+    .. note:: 3) when pytorch<1.8.0, numpy will be used to calculate this metric, which causes ``sdr`` non-differentiable
 
     Args:
         preds:
