f05549de006375ac3cae17a87f4cdff5bb69cde5
twsl <45483159+twsl@users.noreply.github.com>
2021-12-04 18:00:38 +0000

    Remove invalid test case

--- tests/detection/test_map.py
+++ tests/detection/test_map.py
@@ -206,15 +206,6 @@ def test_empty_preds():
 
     metric.update(
         [
-            dict(boxes=torch.Tensor([[]]), scores=torch.Tensor([]), labels=torch.IntTensor([])),
-        ],
-        [
-            dict(boxes=torch.Tensor([[214.1500, 41.2900, 562.4100, 285.0700]]), labels=torch.IntTensor([4])),
-        ],
-    )
-
-    metric.update(
-        [
             dict(boxes=torch.Tensor([]), scores=torch.Tensor([]), labels=torch.IntTensor([])),
         ],
         [
