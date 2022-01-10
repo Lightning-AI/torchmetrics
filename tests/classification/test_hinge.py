5c12358bed7e860651bbbb5b0e473522afe71af7
Gaurav Chawla <gc2713@columbia.edu>
2022-01-09 23:46:15 -0500

    hingeLoss correct function

--- tests/classification/test_hinge.py
+++ tests/classification/test_hinge.py
@@ -21,8 +21,8 @@ from sklearn.preprocessing import OneHotEncoder
 
 from tests.classification.inputs import Input
 from tests.helpers.testers import BATCH_SIZE, NUM_BATCHES, NUM_CLASSES, MetricTester
-from torchmetrics.functional import hinge_loss
-from torchmetrics.functional.classification.hinge import HingeLoss, MulticlassMode
+from torchmetrics.functional.classification.hinge import hinge_loss
+from torchmetrics.classification.hinge import HingeLoss, MulticlassMode
 
 torch.manual_seed(42)
 
