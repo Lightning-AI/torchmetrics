ea4f8b8a0e943aa0ded1f626311d896a73d11d44
Jirka <jirka.borovec@seznam.cz>
2022-01-14 18:48:25 +0100

    rev domain imports

--- torchmetrics/detection/__init__.py
+++ torchmetrics/detection/__init__.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 from torchmetrics.utilities.imports import _TORCHVISION_GREATER_EQUAL_0_8
 
 if _TORCHVISION_GREATER_EQUAL_0_8:
