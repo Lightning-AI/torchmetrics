57cb782b5a2c862fb82076acb31df1a32eb61584
Scott Cronin <j.scott.cronin@gmail.com>
2022-01-16 16:32:51 -0500

    first attempt for detection.iou

--- torchmetrics/detection/__init__.py
+++ torchmetrics/detection/__init__.py
@@ -11,7 +11,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from torchmetrics.utilities.imports import _TORCHVISION_GREATER_EQUAL_0_8
-
-if _TORCHVISION_GREATER_EQUAL_0_8:
-    from torchmetrics.detection.map import MeanAveragePrecision  # noqa: F401
+from torchmetrics.detection.iou import IOU  # noqa: F401
