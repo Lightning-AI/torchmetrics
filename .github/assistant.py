8c2767c1446c9c6550f7fb9b03724a9d8adf69f1
Jirka <jirka.borovec@seznam.cz>
2022-01-17 14:15:01 +0100

    Copyright

--- .github/assistant.py
+++ .github/assistant.py
@@ -1,3 +1,16 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import glob
 import json
 import logging
@@ -51,7 +64,7 @@ class AssistantCLI:
 
     @staticmethod
     def set_min_torch_by_python(fpath: str = "requirements.txt") -> None:
-        """Set minimal torch version accrding to Python actual version."""
+        """Set minimal torch version according to Python actual version."""
         py_ver = f"{sys.version_info.major}.{sys.version_info.minor}"
         if py_ver not in LUT_PYTHON_TORCH:
             return
