3187c561e8235f519e8b2ca765833682d7ee3155
SkafteNicki <skaftenicki@gmail.com>
2022-01-31 13:41:12 +0100

    flake8

--- integrations/__init__.py
+++ integrations/__init__.py
@@ -1,7 +1,5 @@
-import operator
 import os
 
-from torchmetrics.utilities.imports import _compare_version
 
 _INTEGRATION_ROOT = os.path.realpath(os.path.dirname(__file__))
 _PACKAGE_ROOT = os.path.dirname(_INTEGRATION_ROOT)
