44e3ffbe024980a07fc4e4f13b58e46f03e4f3ba
Nicki Skafte Detlefsen <skaftenicki@gmail.com>
2022-02-18 09:43:45 +0100

    Fix compatibility between ClasswiseWrapper and prefix/postfix arg in MetricCollection (#843)
    * fix compatibility
    * Update CHANGELOG.md

--- tests/wrappers/test_classwise.py
+++ tests/wrappers/test_classwise.py
@@ -38,20 +38,32 @@ def test_output_with_labels():
         assert f"accuracy_{lab}" in val
 
 
-def test_using_metriccollection():
+@pytest.mark.parametrize("prefix", [None, "pre_"])
+@pytest.mark.parametrize("postfix", [None, "_post"])
+def test_using_metriccollection(prefix, postfix):
     """Test wrapper in combination with metric collection."""
     labels = ["horse", "fish", "cat"]
     metric = MetricCollection(
         {
             "accuracy": ClasswiseWrapper(Accuracy(num_classes=3, average=None), labels=labels),
             "recall": ClasswiseWrapper(Recall(num_classes=3, average=None), labels=labels),
-        }
+        },
+        prefix=prefix,
+        postfix=postfix,
     )
     preds = torch.randn(10, 3).softmax(dim=-1)
     target = torch.randint(3, (10,))
     val = metric(preds, target)
     assert isinstance(val, dict)
     assert len(val) == 6
+
+    def _get_correct_name(base):
+        name = base if prefix is None else prefix + base
+        name = name if postfix is None else name + postfix
+        return name
+
     for lab in labels:
-        assert f"accuracy_{lab}" in val
-        assert f"recall_{lab}" in val
+        name = _get_correct_name(f"accuracy_{lab}")
+        assert name in val
+        name = _get_correct_name(f"recall_{lab}")
+        assert name in val
