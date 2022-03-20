1a7a3209abdba4a27a97cca8efa4aa16f54011b1
Jirka <jirka.borovec@seznam.cz>
2022-03-20 02:44:15 +0100

    Tensor

--- torchmetrics/detection/test_map.py
+++ torchmetrics/detection/test_map.py
@@ -1,6 +1,19 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import unittest
 
-import torch
+from torch import IntTensor, Tensor
 
 from torchmetrics.detection.map import MeanAveragePrecision
 
@@ -15,28 +28,18 @@ class TestMap(unittest.TestCase):
 
     def test_basic_correct_detection(self):
         """Basic test, 1 ground truth, 1 good prediction."""
-        gt = dict(boxes=torch.Tensor([[10, 20, 15, 25]]), labels=torch.IntTensor([0]))
-        pred = dict(
-                boxes=torch.Tensor([[10, 20, 15, 25]]),
-                scores=torch.Tensor([0.9]),
-                labels=torch.IntTensor([0]),
-            )
+        gt = dict(boxes=Tensor([[10, 20, 15, 25]]), labels=IntTensor([0]))
+        pred = dict(boxes=Tensor([[10, 20, 15, 25]]), scores=Tensor([0.9]), labels=IntTensor([0]))
 
         result = self._compute_metric([gt], [pred])
         self.assertEqual(result["map"], 1)
 
     def test_no_gt_no_pred(self):
         """Image should be ignored, so map result is -1 (the default value)"""
-        gt = dict(
-                boxes=torch.Tensor([]),  # Empty GT
-                labels=torch.IntTensor([]),
-            )
-
-        pred = dict(
-                boxes=torch.Tensor([]),
-                scores=torch.Tensor([]),
-                labels=torch.IntTensor([]),  # Empty prediction
-            )
+        # Empty GT
+        gt = dict(boxes=Tensor([]), labels=IntTensor([]))
+        # Empty prediction
+        pred = dict(boxes=Tensor([]), scores=Tensor([]), labels=IntTensor([]))
 
         result = self._compute_metric([gt], [pred])
         self.assertEqual(result["map"], -1)
@@ -44,24 +47,17 @@ class TestMap(unittest.TestCase):
     def test_missing_pred(self):
         """One good detection, one false negative.
 
-        Map should be lower than 1. Actually it is 0.5, but the exact value depends on where we are sampling (i.e. recall's
-        values)
+        Map should be lower than 1. Actually it is 0.5, but the exact value depends on where we are sampling (i.e.
+        recall's values)
         """
         gts = [
-            dict(boxes=torch.Tensor([[10, 20, 15, 25]]), labels=torch.IntTensor([0])),
-            dict(boxes=torch.Tensor([[10, 20, 15, 25]]), labels=torch.IntTensor([0])),
+            dict(boxes=Tensor([[10, 20, 15, 25]]), labels=IntTensor([0])),
+            dict(boxes=Tensor([[10, 20, 15, 25]]), labels=IntTensor([0])),
         ]
         preds = [
-            dict(
-                boxes=torch.Tensor([[10, 20, 15, 25]]),
-                scores=torch.Tensor([0.9]),
-                labels=torch.IntTensor([0]),
-            ),
-            dict(
-                boxes=torch.Tensor([]),
-                scores=torch.Tensor([]),
-                labels=torch.IntTensor([]),  # Empty prediction
-            ),
+            dict(boxes=Tensor([[10, 20, 15, 25]]), scores=Tensor([0.9]), labels=IntTensor([0])),
+            # Empty prediction
+            dict(boxes=Tensor([]), scores=Tensor([]), labels=IntTensor([])),
         ]
 
         result = self._compute_metric(gts, preds)
@@ -70,28 +66,16 @@ class TestMap(unittest.TestCase):
     def test_missing_gt(self):
         """The symmetric case of test_missing_pred.
 
-        One good detection, one false positive. Map should be lower than 1. Actually it is 0.5, but the exact value depends on
-        where we are sampling (i.e. recall's values)
+        One good detection, one false positive. Map should be lower than 1. Actually it is 0.5, but the exact value
+        depends on where we are sampling (i.e. recall's values)
         """
         gts = [
-            dict(boxes=torch.Tensor([[10, 20, 15, 25]]), labels=torch.IntTensor([0])),
-            dict(
-                boxes=torch.Tensor([]),
-                labels=torch.IntTensor([]),
-            ),
+            dict(boxes=Tensor([[10, 20, 15, 25]]), labels=IntTensor([0])),
+            dict(boxes=Tensor([]), labels=IntTensor([])),
         ]
-
         preds = [
-            dict(
-                boxes=torch.Tensor([[10, 20, 15, 25]]),
-                scores=torch.Tensor([0.9]),
-                labels=torch.IntTensor([0]),
-            ),
-            dict(
-                boxes=torch.Tensor([[10, 20, 15, 25]]),
-                scores=torch.Tensor([0.95]),
-                labels=torch.IntTensor([0]),
-            ),
+            dict(boxes=Tensor([[10, 20, 15, 25]]), scores=Tensor([0.9]), labels=IntTensor([0])),
+            dict(boxes=Tensor([[10, 20, 15, 25]]), scores=Tensor([0.95]), labels=IntTensor([0])),
         ]
 
         result = self._compute_metric(gts, preds)
