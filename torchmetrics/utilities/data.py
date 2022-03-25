f750888ff3c402d86e7618c0744992c30f3f50cd
rusty1s <matthias.fey@tu-dortmund.de>
2022-03-25 07:06:09 +0100

    remove get_num_classes

--- torchmetrics/utilities/data.py
+++ torchmetrics/utilities/data.py
@@ -145,37 +145,6 @@ def to_categorical(x: Tensor, argmax_dim: int = 1) -> Tensor:
     return torch.argmax(x, dim=argmax_dim)
 
 
-def get_num_classes(
-    preds: Tensor,
-    target: Tensor,
-    num_classes: Optional[int] = None,
-) -> int:
-    """Calculates the number of classes for a given prediction and target tensor.
-
-    Args:
-        preds: predicted values
-        target: true labels
-        num_classes: number of classes if known
-
-    Return:
-        An integer that represents the number of classes.
-    """
-    num_target_classes = int(target.max().detach().item() + 1)
-    num_pred_classes = int(preds.max().detach().item() + 1)
-    num_all_classes = max(num_target_classes, num_pred_classes)
-
-    if num_classes is None:
-        num_classes = num_all_classes
-    elif num_classes != num_all_classes:
-        rank_zero_warn(
-            f"You have set {num_classes} number of classes which is"
-            f" different from predicted ({num_pred_classes}) and"
-            f" target ({num_target_classes}) number of classes",
-            RuntimeWarning,
-        )
-    return num_classes
-
-
 def apply_to_collection(
     data: Any,
     dtype: Union[type, tuple],
