3c3a6d9c508fbc75efd3a322c3b0a7af5eaeecc6
Nicolas Berger <nicolas.berger@inait.ai>
2022-04-04 15:08:45 +0000

    fix PEP8 compliance of imports

--- torchmetrics/detection/__init__.py
+++ torchmetrics/detection/__init__.py
@@ -16,4 +16,4 @@ from torchmetrics.utilities.imports import _TORCHVISION_GREATER_EQUAL_0_8
 if _TORCHVISION_GREATER_EQUAL_0_8:
     from torchmetrics.detection.mean_ap import MeanAveragePrecision  # noqa: F401
 
-from torchmetrics.detection.panoptic_quality import PanopticQuality
+from torchmetrics.detection.panoptic_quality import PanopticQuality  # noqa: F401
