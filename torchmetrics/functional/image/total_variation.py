41de2093dd20888543a9e600d7c33c0d9fc23490
ragavv <ragavv@nvidia.com>
2022-05-10 01:43:41 -0700

    added functional and modular metric.

---
+++ torchmetrics/functional/image/total_variation.py
@@ -0,0 +1,25 @@
+import torch
+
+
+def total_variation(img: torch.Tensor) -> torch.Tensor:
+    """
+    Computes total variation loss.
+
+    Adapted from https://github.com/jxgu1016/Total_Variation_Loss.pytorch
+    Args:
+        img (torch.Tensor): A NCHW image batch.
+
+    Returns:
+        A loss scalar value.
+    """
+
+    def tensor_size(t):
+        return t.size()[1] * t.size()[2] * t.size()[3]
+
+    _height = img.size()[2]
+    _width = img.size()[3]
+    _count_height = tensor_size(img[:, :, 1:, :])
+    _count_width = tensor_size(img[:, :, :, 1:])
+    _height_tv = torch.pow((img[:, :, 1:, :] - img[:, :, : _height - 1, :]), 2).sum()
+    _width_tv = torch.pow((img[:, :, :, 1:] - img[:, :, :, : _width - 1]), 2).sum()
+    return (2 * (_height_tv / _count_height + _width_tv / _count_width)) / img.size()[0]
--- torchmetrics/image/total_variation.py
+++ torchmetrics/image/total_variation.py
@@ -1,23 +1,17 @@
-# Reference code: https://github.com/jxgu1016/Total_Variation_Loss.pytorch
 import torch
 
 from torchmetrics.metric import Metric
 
 
 class TotalVariation(Metric):
-    """A method to calculate total variation loss.
+    """Computes Total Variation loss.
 
-    .. note::
-
-        Because this loss uses sums, the value will be large. Use a weighting
-        of order e-5 to control it. Ensure to train with half-precision at least.
-
-    :param dist_sync_on_step: Synchronize metric state across processes at each ``forward()``
-        before returning the value at the step.
-    :type dist_sync_on_step: bool
-    :param compute_on_step: Forward only calls ``update()`` and returns None if this is set to
-        False.
-    :type compute_on_step: bool
+    Adapted from: https://github.com/jxgu1016/Total_Variation_Loss.pytorch
+    Args:
+        dist_sync_on_step: Synchronize metric state across processes at each ``forward()``
+            before returning the value at the step.
+        compute_on_step: Forward only calls ``update()`` and returns None if this is set to
+            False.
     """
 
     is_differentiable = True
@@ -30,23 +24,23 @@ class TotalVariation(Metric):
         self.add_state("current", default=torch.tensor(0, dtype=torch.float), dist_reduce_fx="sum")
         self.add_state("total", default=torch.tensor(0, dtype=torch.int), dist_reduce_fx="sum")
 
-    def update(self, sample: torch.Tensor) -> None:
+    def update(self, img: torch.Tensor) -> None:
         """Update method for TV Loss.
 
-        :param sample: A NCHW image batch.
-        :type sample: torch.Tensor
+        Args:
+            img (torch.Tensor): A NCHW image batch.
 
-        :returns: A loss scalar.
-        :rtype: torch.Tensor
+        Returns:
+            A loss scalar value.
         """
-        _height = sample.size()[2]
-        _width = sample.size()[3]
-        _count_height = self.tensor_size(sample[:, :, 1:, :])
-        _count_width = self.tensor_size(sample[:, :, :, 1:])
-        _height_tv = torch.pow((sample[:, :, 1:, :] - sample[:, :, : _height - 1, :]), 2).sum()
-        _width_tv = torch.pow((sample[:, :, :, 1:] - sample[:, :, :, : _width - 1]), 2).sum()
+        _height = img.size()[2]
+        _width = img.size()[3]
+        _count_height = self.tensor_size(img[:, :, 1:, :])
+        _count_width = self.tensor_size(img[:, :, :, 1:])
+        _height_tv = torch.pow((img[:, :, 1:, :] - img[:, :, : _height - 1, :]), 2).sum()
+        _width_tv = torch.pow((img[:, :, :, 1:] - img[:, :, :, : _width - 1]), 2).sum()
         self.current += 2 * (_height_tv / _count_height + _width_tv / _count_width)
-        self.total += sample.numel()
+        self.total += img.numel()
 
     def compute(self):
         return self.current.float() / self.total
