Fix nan in KL Divergence (#1030)
* fix nan
* changelog

torchmetrics/functional/classification/kl_divergence.py
@@ -19,7 +19,7 @@ from torch import Tensor
 from typing_extensions import Literal
 
 from torchmetrics.utilities.checks import _check_same_shape
-from torchmetrics.utilities.data import METRIC_EPS
+from torchmetrics.utilities.compute import _safe_xlogy
 
 
 def _kld_update(p: Tensor, q: Tensor, log_prob: bool) -> Tuple[Tensor, int]:
@@ -42,8 +42,7 @@ def _kld_update(p: Tensor, q: Tensor, log_prob: bool) -> Tuple[Tensor, int]:
     else:
         p = p / p.sum(axis=-1, keepdim=True)
         q = q / q.sum(axis=-1, keepdim=True)
-        q = torch.clamp(q, METRIC_EPS)
-        measures = torch.sum(p * torch.log(p / q), axis=-1)
+        measures = _safe_xlogy(p, p / q).sum(axis=-1)
 
     return measures, total
 

torchmetrics/functional/regression/tweedie_deviance.py
@@ -17,13 +17,7 @@ import torch
 from torch import Tensor
 
 from torchmetrics.utilities.checks import _check_same_shape
-
-
-def xlogy(x: Tensor, y: Tensor) -> Tensor:
-    """Computes x * log(y). Returns 0 if x=0"""
-    res = x * torch.log(y)
-    res[x == 0] = 0.0
-    return res
+from torchmetrics.utilities.compute import _safe_xlogy
 
 
 def _tweedie_deviance_score_update(preds: Tensor, targets: Tensor, power: float = 0.0) -> Tuple[Tensor, Tensor]:
@@ -58,7 +52,7 @@ def _tweedie_deviance_score_update(preds: Tensor, targets: Tensor, power: float 
                 f"For power={power}, 'preds' has to be strictly positive and 'targets' cannot be negative."
             )
 
-        deviance_score = 2 * (xlogy(targets, targets / preds) + preds - targets)
+        deviance_score = 2 * (_safe_xlogy(targets, targets / preds) + preds - targets)
     elif power == 2:
         # Gamma distribution
         if torch.any(preds <= 0) or torch.any(targets <= 0):