48fde65be2bb2dcb956a0353d6094e2e8f161689
Jirka <jirka.borovec@seznam.cz>
2022-05-20 04:10:36 +0200

    rename test _data

--- tests/audio/__init__.py
+++ tests/audio/__init__.py
@@ -2,6 +2,6 @@ import os
 
 from tests import _PATH_TESTS
 
-_SAMPLE_AUDIO_SPEECH = os.path.join(_PATH_TESTS, "data", "audio", "audio_speech.wav")
-_SAMPLE_AUDIO_SPEECH_BAB_DB = os.path.join(_PATH_TESTS, "data", "audio", "audio_speech_bab_0dB.wav")
-_SAMPLE_NUMPY_ISSUE_895 = os.path.join(_PATH_TESTS, "data", "audio", "issue_895.npz")
+_SAMPLE_AUDIO_SPEECH = os.path.join(_PATH_TESTS, "_data", "audio", "audio_speech.wav")
+_SAMPLE_AUDIO_SPEECH_BAB_DB = os.path.join(_PATH_TESTS, "_data", "audio", "audio_speech_bab_0dB.wav")
+_SAMPLE_NUMPY_ISSUE_895 = os.path.join(_PATH_TESTS, "_data", "audio", "issue_895.npz")
