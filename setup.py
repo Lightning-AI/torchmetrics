d4a5cff228c2fef850d81a95b37050a183f45d4f
Jirka <jirka.borovec@seznam.cz>
2022-06-01 00:00:42 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,38 +1,95 @@
 #!/usr/bin/env python
 import glob
 import os
+import re
 from functools import partial
 from importlib.util import module_from_spec, spec_from_file_location
-from typing import Tuple
+from itertools import chain
+from typing import List, Tuple
 
 from setuptools import find_packages, setup
 
 _PATH_ROOT = os.path.realpath(os.path.dirname(__file__))
+_PATH_SOURCE = os.path.join(_PATH_ROOT, "src")
 _PATH_REQUIRE = os.path.join(_PATH_ROOT, "requirements")
 
 
+def _load_requirements(path_dir: str, file_name: str = "requirements.txt", comment_char: str = "#") -> List[str]:
+    """Load requirements from a file.
+
+    >>> _load_requirements(_PATH_ROOT)
+    ['numpy...', 'torch..."]
+    """
+    with open(os.path.join(path_dir, file_name)) as file:
+        lines = [ln.strip() for ln in file.readlines()]
+    reqs = []
+    for ln in lines:
+        # filer all comments
+        if comment_char in ln:
+            char_idx = min(ln.index(ch) for ch in comment_char)
+            ln = ln[:char_idx].strip()
+        # skip directly installed dependencies
+        if ln.startswith("http") or ln.startswith("git") or ln.startswith("-r") or "@" in ln:
+            continue
+        if ln:  # if requirement is not empty
+            reqs.append(ln)
+    return reqs
+
+
+def _load_readme_description(path_dir: str, homepage: str, version: str) -> str:
+    """Load readme as decribtion.
+
+    >>> _load_readme_description(_PATH_ROOT, "",  "")
+    '<div align="center">...'
+    """
+    path_readme = os.path.join(path_dir, "README.md")
+    with open(path_readme, encoding="utf-8") as fp:
+        text = fp.read()
+
+    # https://github.com/PyTorchLightning/torchmetrics/raw/master/docs/source/_static/images/lightning_module/pt_to_pl.png
+    github_source_url = os.path.join(homepage, "raw", version)
+    # replace relative repository path to absolute link to the release
+    #  do not replace all "docs" as in the readme we replace some other sources with particular path to docs
+    text = text.replace("docs/source/_static/", f"{os.path.join(github_source_url, 'docs/source/_static/')}")
+
+    # readthedocs badge
+    text = text.replace("badge/?version=stable", f"badge/?version={version}")
+    text = text.replace("torchmetrics.readthedocs.io/en/stable/", f"torchmetrics.readthedocs.io/en/{version}")
+    # codecov badge
+    text = text.replace("/branch/master/graph/badge.svg", f"/release/{version}/graph/badge.svg")
+    # replace github badges for release ones
+    text = text.replace("badge.svg?branch=master&event=push", f"badge.svg?tag={version}")
+    # Azure...
+    text = text.replace("?branchName=master", f"?branchName=refs%2Ftags%2F{version}")
+    text = re.sub(r"\?definitionId=\d+&branchName=master", f"?definitionId=2&branchName=refs%2Ftags%2F{version}", text)
+
+    skip_begin = r"<!-- following section will be skipped from PyPI description -->"
+    skip_end = r"<!-- end skipping PyPI description -->"
+    # todo: wrap content as commented description
+    text = re.sub(rf"{skip_begin}.+?{skip_end}", "<!--  -->", text, flags=re.IGNORECASE + re.DOTALL)
+
+    return text
+
+
 def _load_py_module(fname, pkg="torchmetrics"):
-    spec = spec_from_file_location(os.path.join(pkg, fname), os.path.join(_PATH_ROOT, pkg, fname))
+    spec = spec_from_file_location(os.path.join(pkg, fname), os.path.join(_PATH_SOURCE, pkg, fname))
     py = module_from_spec(spec)
     spec.loader.exec_module(py)
     return py
 
 
-about = _load_py_module("__about__.py")
-setup_tools = _load_py_module("setup_tools.py")
-long_description = setup_tools._load_readme_description(
+ABOUT = _load_py_module("__about__.py")
+LONG_DESCRIPTION = _load_readme_description(
     _PATH_ROOT,
-    homepage=about.__homepage__,
-    version=f"v{about.__version__}",
+    homepage=ABOUT.__homepage__,
+    version=f"v{ABOUT.__version__}",
 )
-
-
-BASE_REQUIREMENTS = setup_tools._load_requirements(path_dir=_PATH_ROOT, file_name="requirements.txt")
+BASE_REQUIREMENTS = _load_requirements(path_dir=_PATH_ROOT, file_name="requirements.txt")
 
 
 def _prepare_extras(skip_files: Tuple[str] = ("devel.txt",)):
     # find all extra requirements
-    _load_req = partial(setup_tools._load_requirements, path_dir=_PATH_REQUIRE)
+    _load_req = partial(_load_requirements, path_dir=_PATH_REQUIRE)
     found_req_files = sorted(os.path.basename(p) for p in glob.glob(os.path.join(_PATH_REQUIRE, "*.txt")))
     # filter unwanted files
     found_req_files = [n for n in found_req_files if n not in skip_files]
@@ -46,8 +103,8 @@ def _prepare_extras(skip_files: Tuple[str] = ("devel.txt",)):
             extras_req["test"] += _load_req(file_name=fname)
     # filter the uniques
     extras_req = {n: list(set(req)) for n, req in extras_req.items()}
-    # create an 'all' keyword that install all possible denpendencies
-    extras_req["all"] = [pkg for reqs in extras_req.values() for pkg in reqs]
+    # create an 'all' keyword that install all possible dependencies
+    extras_req["all"] = list(chain([pkgs for k, pkgs in extras_req.items() if k not in ("test", "docs")]))
     return extras_req
 
 
@@ -58,15 +115,16 @@ def _prepare_extras(skip_files: Tuple[str] = ("devel.txt",)):
 # engineer specific practices
 setup(
     name="torchmetrics",
-    version=about.__version__,
-    description=about.__docs__,
-    author=about.__author__,
-    author_email=about.__author_email__,
-    url=about.__homepage__,
-    download_url=os.path.join(about.__homepage__, "archive", "master.zip"),
-    license=about.__license__,
-    packages=find_packages(exclude=["tests", "tests.*", "docs"]),
-    long_description=long_description,
+    version=ABOUT.__version__,
+    description=ABOUT.__docs__,
+    author=ABOUT.__author__,
+    author_email=ABOUT.__author_email__,
+    url=ABOUT.__homepage__,
+    download_url=os.path.join(ABOUT.__homepage__, "archive", "master.zip"),
+    license=ABOUT.__license__,
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     include_package_data=True,
     zip_safe=False,
@@ -76,9 +134,9 @@ setup(
     install_requires=BASE_REQUIREMENTS,
     extras_require=_prepare_extras(),
     project_urls={
-        "Bug Tracker": os.path.join(about.__homepage__, "issues"),
+        "Bug Tracker": os.path.join(ABOUT.__homepage__, "issues"),
         "Documentation": "https://torchmetrics.rtfd.io/en/latest/",
-        "Source Code": about.__homepage__,
+        "Source Code": ABOUT.__homepage__,
     },
     classifiers=[
         "Environment :: Console",
