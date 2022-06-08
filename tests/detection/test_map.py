Fix mAP calculation for areas with 0 predictions
The issue was first dicussed here: https://github.com/PyTorchLightning/metrics/issues/1061#issuecomment-1150106306

tests/detection/test_map.py
@@ -462,7 +462,7 @@ def test_missing_gt():
 
 
 @pytest.mark.skipif(_pytest_condition, reason="test requires that torchvision=>0.8.0 is installed")
-def test_segm_iou_empty_mask():
+def test_segm_iou_empty_gt_mask():
     """Test empty ground truths."""
     metric = MeanAveragePrecision(iou_type="segm")
 
@@ -476,6 +476,27 @@ def test_segm_iou_empty_mask():
         ],
         [
             dict(masks=Tensor([]), labels=IntTensor([])),
+        ],
+    )
+
+    metric.compute()
+
+
+@pytest.mark.skipif(_pytest_condition, reason="test requires that torchvision=>0.8.0 is installed")
+def test_segm_iou_empty_pred_mask():
+    """Test empty predictions."""
+    metric = MeanAveragePrecision(iou_type="segm")
+
+    metric.update(
+        [
+            dict(
+                masks=torch.BoolTensor([]),
+                scores=Tensor([]),
+                labels=IntTensor([]),
+            ),
+        ],
+        [
+            dict(masks=torch.randint(0, 1, (1, 10, 10)).bool(), labels=IntTensor([4])),
         ],
     )
 

torchmetrics/detection/mean_ap.py
@@ -482,9 +482,9 @@ class MeanAveragePrecision(Metric):
     ) -> Dict[str, Any]:
         """Some GT but no predictions."""
         # GTs
-        gt = gt[gt_label_mask]
+        gt = [gt[i] for i in gt_label_mask]
         nb_gt = len(gt)
-        areas = box_area(gt)
+        areas = compute_area(gt, iou_type=self.iou_type).to(self.device)
         ignore_area = (areas < area_range[0]) | (areas > area_range[1])
         gt_ignore, _ = torch.sort(ignore_area.to(torch.uint8))
         gt_ignore = gt_ignore.to(torch.bool)