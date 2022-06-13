fbb470ab7ddd02b3e0e1025e8d9f488aee8aa813
Nicki Skafte Detlefsen <skaftenicki@gmail.com>
2022-06-13 22:06:34 +0200

    Bugfix/avg prec auroc compute groups (#1086)
    * fix bugs
    * changelog
    
    (cherry picked from commit c2f55fa5f84bf1549a26bdab67d572503f06e18f)

--- tests/bases/test_composition.py
+++ tests/bases/test_composition.py
@@ -22,6 +22,8 @@ from torchmetrics.metric import CompositionalMetric, Metric
 
 
 class DummyMetric(Metric):
+    full_state_update = True
+
     def __init__(self, val_to_return):
         super().__init__()
         self.add_state("_num_updates", tensor(0), dist_reduce_fx="sum")
