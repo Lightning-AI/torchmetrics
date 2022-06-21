6baba5fa6a3708c00b7dd07beffff801446c1682
Cas <caswognum@hotmail.com>
2022-06-21 12:34:34 -0400

    Removed unused import

--- src/torchmetrics/functional/classification/average_precision.py
+++ src/torchmetrics/functional/classification/average_precision.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import warnings
-from typing import List, Optional, Sequence, Tuple, Union
+from typing import List, Optional, Tuple, Union
 
 import torch
 from torch import Tensor
