4fa716f8f8550470f7c8c890abf2996737e90ff3
Jirka Borovec <Borda@users.noreply.github.com>
2022-06-29 14:07:35 +0200

    simple

--- examples/detection_map.py
+++ examples/detection_map.py
@@ -22,6 +22,13 @@ from torchmetrics.detection.mean_ap import MeanAveragePrecision
 
 # Preds should be a list of elements, where each element is a dict
 # containing 3 keys: boxes, scores, labels
+mask_pred = [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 1, 1, 0],
+                    [0, 0, 1, 1, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                ]
 preds = [
     dict(
         # The boxes keyword should contain an [N,4] tensor,
@@ -37,17 +44,7 @@ preds = [
         # The masks keyword should contain an [N,H,W] tensor,
         # where H and W are the image height and width, respectively,
         # with boolean masks. This is only required when iou_type is `segm`.
-        masks=BoolTensor(
-            [
-                [
-                    [0, 0, 0, 0, 0],
-                    [0, 0, 1, 1, 0],
-                    [0, 0, 1, 1, 0],
-                    [0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0],
-                ],
-            ]
-        ),
+        masks=BoolTensor([ mask_pred ]),
     )
 ]
 
@@ -55,21 +52,18 @@ preds = [
 # containing 2 keys: boxes and labels (and masks, if iou_type is `segm`).
 # Each keyword should be formatted similar to the preds argument.
 # The number of elements in preds and target need to match
-target = [
-    dict(
-        boxes=Tensor([[214.0, 41.0, 562.0, 285.0]]),
-        labels=IntTensor([0]),
-        masks=BoolTensor(
-            [
-                [
+mask_tgt = [
                     [0, 0, 0, 0, 0],
                     [0, 0, 1, 0, 0],
                     [0, 0, 1, 1, 0],
                     [0, 0, 1, 0, 0],
                     [0, 0, 0, 0, 0],
-                ],
-            ]
-        ),
+                ]
+target = [
+    dict(
+        boxes=Tensor([[214.0, 41.0, 562.0, 285.0]]),
+        labels=IntTensor([0]),
+        masks=BoolTensor([mask_tgt]),
     )
 ]
 
