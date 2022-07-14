3e814a06236f59f9b958dfa47455d4d4146a8f85
SkafteNicki <skaftenicki@gmail.com>
2022-07-14 12:35:00 +0200

    typing

--- src/torchmetrics/classification/jaccard.py
+++ src/torchmetrics/classification/jaccard.py
@@ -11,10 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Any, Literal, Optional
+from typing import Any, Optional
 
 import torch
 from torch import Tensor
+from typing_extensions import Literal
 
 from torchmetrics.classification import BinaryConfusionMatrix, MulticlassConfusionMatrix, MultilabelConfusionMatrix
 from torchmetrics.classification.confusion_matrix import ConfusionMatrix
