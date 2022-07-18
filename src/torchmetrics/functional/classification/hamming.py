920be5ac23b3f58f219f3560c6004f55ba048a5a
SkafteNicki <skaftenicki@gmail.com>
2022-07-18 21:52:56 +0200

    hamming

--- src/torchmetrics/functional/classification/hamming.py
+++ src/torchmetrics/functional/classification/hamming.py
@@ -11,13 +11,107 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Tuple, Union
+from typing import Tuple, Union, Optional
 
 import torch
 from torch import Tensor
+from typing_extensions import Literal
 
 from torchmetrics.utilities.checks import _input_format_classification
+from torchmetrics.utilities.compute import _safe_divide
+from torchmetrics.functional.classification.stat_scores import (
+    _binary_stat_scores_arg_validation,
+    _binary_stat_scores_tensor_validation,
+    _binary_stat_scores_format,
+    _binary_stat_scores_update,
+    _multiclass_stat_scores_arg_validation,
+    _multiclass_stat_scores_tensor_validation,
+    _multiclass_stat_scores_format,
+    _multiclass_stat_scores_update,
+    _multilabel_stat_scores_arg_validation,
+    _multilabel_stat_scores_tensor_validation,
+    _multilabel_stat_scores_format,
+    _multilabel_stat_scores_update
+)
 
+
+def _hamming_distance_reduce(
+    tp: Tensor,
+    fp: Tensor,
+    tn: Tensor,
+    fn: Tensor,
+    average: Optional[Literal["binary", "micro", "macro", "weighted", "none"]],
+    multidim_average: Literal["global", "samplewise"] = "global",
+) -> Tensor:
+    if average == "binary" or average is None or average == 'none':
+        hamming_score = _safe_divide(tn, tn + fp)
+    elif average == "micro":
+        tn = tn.sum(dim=0 if multidim_average == "global" else 1)
+        fp = fp.sum(dim=0 if multidim_average == "global" else 1)
+        hamming_score = _safe_divide(tn, tn + fp)
+    else:
+        hamming_score = _safe_divide(tn, tn + fp)
+        if average == "weighted":
+            weights = tp + fn
+        else:
+            weights = torch.ones_like(hamming_score)
+        hamming_score = _safe_divide(weights * hamming_score, weights.sum(-1, keepdim=True)).sum(-1)
+    return 1 - hamming_score
+
+
+def binary_hamming_distance(
+    preds: Tensor,
+    target: Tensor,
+    threshold: float = 0.5,
+    multidim_average: Literal["global", "samplewise"] = "global",
+    ignore_index: Optional[int] = None,
+    validate_args: bool = True,
+) -> Tensor:
+    if validate_args:
+        _binary_stat_scores_arg_validation(threshold, multidim_average, ignore_index)
+        _binary_stat_scores_tensor_validation(preds, target, multidim_average, ignore_index)
+    preds, target = _binary_stat_scores_format(preds, target, threshold, ignore_index)
+    tp, fp, tn, fn = _binary_stat_scores_update(preds, target, multidim_average)
+    return _hamming_distance_reduce(tp, fp, tn, fn, average="binary", multidim_average=multidim_average)
+
+
+def multiclass_hamming_distance(
+        preds: Tensor,
+    target: Tensor,
+    num_classes: int,
+    average: Optional[Literal["micro", "macro", "weighted", "none"]] = "micro",
+    top_k: int = 1,
+    multidim_average: Literal["global", "samplewise"] = "global",
+    ignore_index: Optional[int] = None,
+    validate_args: bool = True,
+) -> Tensor:
+    if validate_args:
+        _multiclass_stat_scores_arg_validation(num_classes, top_k, average, multidim_average, ignore_index)
+        _multiclass_stat_scores_tensor_validation(preds, target, num_classes, multidim_average, ignore_index)
+    preds, target = _multiclass_stat_scores_format(preds, target, top_k)
+    tp, fp, tn, fn = _multiclass_stat_scores_update(preds, target, num_classes, top_k, multidim_average, ignore_index)
+    return _hamming_distance_reduce(tp, fp, tn, fn, average=average, multidim_average=multidim_average)
+
+
+def multilabel_hamming_distance(
+    preds: Tensor,
+    target: Tensor,
+    num_labels: int,
+    threshold: float = 0.5,
+    average: Optional[Literal["micro", "macro", "weighted", "none"]] = "micro",
+    multidim_average: Literal["global", "samplewise"] = "global",
+    ignore_index: Optional[int] = None,
+    validate_args: bool = True,
+) -> Tensor:
+    if validate_args:
+        _multilabel_stat_scores_arg_validation(num_labels, threshold, average, multidim_average, ignore_index)
+        _multilabel_stat_scores_tensor_validation(preds, target, num_labels, multidim_average, ignore_index)
+    preds, target = _multilabel_stat_scores_format(preds, target, num_labels, threshold, ignore_index)
+    tp, fp, tn, fn = _multilabel_stat_scores_update(preds, target, multidim_average)
+    return _hamming_distance_reduce(tp, fp, tn, fn, average=average, multidim_average=multidim_average)
+
+
+# -------------------------- Old stuff --------------------------
 
 def _hamming_distance_update(
     preds: Tensor,
