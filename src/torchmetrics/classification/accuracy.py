8681e662419494002d386a259a64b01f3e077fcd
SkafteNicki <skaftenicki@gmail.com>
2022-09-08 14:54:48 +0200

    fix one mistake

--- src/torchmetrics/classification/accuracy.py
+++ src/torchmetrics/classification/accuracy.py
@@ -11,10 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Any, Literal, Optional
+from typing import Any, Optional
 
 import torch
 from torch import Tensor, tensor
+from typing_extensions import Literal
 
 from torchmetrics.functional.classification.accuracy import (
     _accuracy_compute,
