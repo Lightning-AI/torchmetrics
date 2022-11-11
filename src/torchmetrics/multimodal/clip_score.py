711e34384c7cceeef05349756603506cafe8b660
SkafteNicki <skaftenicki@gmail.com>
2022-11-11 09:34:44 +0100

    fix

--- src/torchmetrics/multimodal/clip_score.py
+++ src/torchmetrics/multimodal/clip_score.py
@@ -11,10 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Any, List, Literal, Union
+from typing import Any, List, Union
 
 import torch
 from torch import Tensor
+from typing_extensions import Literal
 
 from torchmetrics.utilities.imports import _TRANSFORMERS_AVAILABLE
 
