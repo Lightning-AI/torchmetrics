c501a7f62eeae0afd77de0cb8e2ea3cf15193c6e
SkafteNicki <skaftenicki@gmail.com>
2022-11-16 08:29:36 +0100

    fx testing

--- examples/plotting.py
+++ examples/plotting.py
@@ -16,6 +16,68 @@ import argparse
 import matplotlib.pyplot as plt
 import torch
 
+
+def accuracy_example():
+    from torchmetrics.classification import MulticlassAccuracy
+
+    p = lambda: torch.randn(20, 5)
+    t = lambda: torch.randint(5, (20,))
+
+    # plot single value
+    metric = MulticlassAccuracy(num_classes=5)
+    metric.update(p(), t())
+    fig, ax = metric.plot()
+
+    # plot a value per class
+    metric = MulticlassAccuracy(num_classes=5, average=None)
+    metric.update(p(), t())
+    fig, ax = metric.plot()
+
+    # plot two values as an series
+    metric = MulticlassAccuracy(num_classes=5)
+    val1 = metric(p(), t())
+    val2 = metric(p(), t())
+    fig, ax = metric.plot([val1, val2])
+
+    # plot a series of values per class
+    metric = MulticlassAccuracy(num_classes=5, average=None)
+    val1 = metric(p(), t())
+    val2 = metric(p(), t())
+    fig, ax = metric.plot([val1, val2])
+    return fig, ax
+
+
+def mean_squared_error_example():
+    from torchmetrics.regression import MeanSquaredError
+
+    p = lambda: torch.randn(20)
+    t = lambda: torch.randn(20)
+
+    # single val
+    metric = MeanSquaredError()
+    metric.update(p(), t())
+    fig, ax = metric.plot()
+
+    # multiple values
+    metric = MeanSquaredError()
+    vals = [metric(p(), t()) for _ in range(10)]
+    fig, ax = metric.plot(vals)
+    return fig, ax
+
+
+def confusion_matrix_example():
+    from torchmetrics.classification import MulticlassConfusionMatrix
+
+    p = lambda: torch.randn(20, 5)
+    t = lambda: torch.randint(5, (20,))
+
+    # plot single value
+    metric = MulticlassConfusionMatrix(num_classes=5)
+    metric.update(p(), t())
+    fig, ax = metric.plot()
+    return fig, ax
+
+
 if __name__ == "__main__":
     list_of_choices = ["accuracy", "mean_squared_error", "confusion_matrix"]
     parser = argparse.ArgumentParser(description="Example script for plotting metrics.")
@@ -23,62 +85,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.metric == "accuracy":
-        from torchmetrics.classification import MulticlassAccuracy
-
-        p = lambda: torch.randn(20, 5)
-        t = lambda: torch.randint(5, (20,))
-
-        # plot single value
-        metric = MulticlassAccuracy(num_classes=5)
-        metric.update(p(), t())
-        fig, ax = metric.plot()
-
-        # plot a value per class
-        metric = MulticlassAccuracy(num_classes=5, average=None)
-        metric.update(p(), t())
-        fig, ax = metric.plot()
-
-        # plot two values as an series
-        metric = MulticlassAccuracy(num_classes=5)
-        val1 = metric(p(), t())
-        val2 = metric(p(), t())
-        fig, ax = metric.plot([val1, val2])
-
-        # plot a series of values per class
-        metric = MulticlassAccuracy(num_classes=5, average=None)
-        val1 = metric(p(), t())
-        val2 = metric(p(), t())
-        fig, ax = metric.plot([val1, val2])
+        fig, ax = accuracy_example()
 
     if args.metric == "mean_squared_error":
-        from torchmetrics.regression import MeanSquaredError
-
-        p = lambda: torch.randn(
-            20,
-        )
-        t = lambda: torch.randn(
-            20,
-        )
-
-        # single val
-        metric = MeanSquaredError()
-        metric.update(p(), t())
-        fig, ax = metric.plot()
-
-        # multiple values
-        metric = MeanSquaredError()
-        vals = [metric(p(), t()) for _ in range(10)]
-        fig, ax = metric.plot(vals)
+        fig, ax = mean_squared_error_example()
 
     if args.metric == "confusion_matrix":
-        from torchmetrics.classification import MulticlassConfusionMatrix
-
-        p = lambda: torch.randn(20, 5)
-        t = lambda: torch.randint(5, (20,))
-
-        # plot single value
-        metric = MulticlassConfusionMatrix(num_classes=5)
-        metric.update(p(), t())
-        fig, ax = metric.plot()
+        fig, ax = confusion_matrix_example()
 
     plt.show()
