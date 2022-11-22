1d92316ee8f2f8b938f9c674346f309b99ff5940
SkafteNicki <skaftenicki@gmail.com>
2022-11-22 08:22:25 +0100

    init files

--- src/torchmetrics/functional/classification/__init__.py
+++ src/torchmetrics/functional/classification/__init__.py
@@ -45,7 +45,7 @@ from torchmetrics.functional.classification.confusion_matrix import (  # noqa: F
     multiclass_confusion_matrix,
     multilabel_confusion_matrix,
 )
-from torchmetrics.functional.classification.dice import dice, dice_score  # noqa: F401
+from torchmetrics.functional.classification.dice import dice  # noqa: F401
 from torchmetrics.functional.classification.exact_match import multilabel_exact_match  # noqa: F401
 from torchmetrics.functional.classification.f_beta import (  # noqa: F401
     binary_f1_score,
