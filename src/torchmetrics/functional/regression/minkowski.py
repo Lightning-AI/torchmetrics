c7f4993ebc9cef09b8f33df6f5a0a4147e2015b1
epsilonator <somichat@gmail.com>
2022-11-26 18:41:23 +0530

    Added function docstring; Raw-stringified class docstring

--- src/torchmetrics/functional/regression/minkowski.py
+++ src/torchmetrics/functional/regression/minkowski.py
@@ -8,6 +8,15 @@ from torchmetrics.utilities.exceptions import TorchMetricsUserError
 
 
 def _minkowski_distance_update(preds: Tensor, targets: Tensor, p: float) -> Tensor:
+    """Updates and returns variables required to compute Minkowski distance.
+
+    Checks for same shape of input tensors.
+
+    Args:
+        preds: Predicted tensor
+        target: Ground truth tensor
+        p: Non-negative number acting as the exponent to the errors
+    """
     _check_same_shape(preds, targets)
 
     if p < 0:
@@ -20,6 +29,19 @@ def _minkowski_distance_update(preds: Tensor, targets: Tensor, p: float) -> Tens
 
 
 def _minkowski_distance_compute(distance: Tensor, p: float) -> Tensor:
+    """Computes Minkowski Distance.
+
+    Args:
+        distance: Sum of the p-th powers of errors over all observations
+        p: The non-negative numeric power the errors are to be raised to
+
+    Example:
+        >>> preds = torch.tensor([0., 1, 2, 3])
+        >>> target = torch.tensor([0., 2, 3, 1])
+        >>> distance_p_sum = _minkowski_distance_update(preds, target, 5)
+        >>> _minkowski_distance_compute(distance_p_sum, 5)
+        tensor(2.0244)
+    """
     return torch.pow(distance, 1.0 / p)
 
 
--- src/torchmetrics/regression/minkowski.py
+++ src/torchmetrics/regression/minkowski.py
@@ -22,7 +22,7 @@ from torchmetrics.metric import Metric
 
 
 class MinkowskiDistance(Metric):
-    """Computes `Minkowski Distance`
+    r"""Computes `Minkowski Distance`
 
     .. math:: d_{\text{Minkowski}} = \\sum_{i}^N (| y_i - \\hat{y_i} |^p)^\frac{1}{p}
 
