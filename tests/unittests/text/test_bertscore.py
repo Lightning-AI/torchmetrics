09ce9510f5348fb63a0ebf753417510df43aa05c
Jirka <jirka.borovec@seznam.cz>
2022-12-17 05:31:19 +0100

    tests: use smaller model?

--- tests/unittests/text/test_bertscore.py
+++ tests/unittests/text/test_bertscore.py
@@ -38,7 +38,7 @@ targets = [
 
 _METRICS = ["precision", "recall", "f1"]
 
-MODEL_NAME = "albert-base-v2"
+MODEL_NAME = "t5-small"
 
 
 def _assert_list(preds: Any, targets: Any, threshold: float = 1e-7):
