3e822eb1fb3721913da6a93889473e1358d202f5
Jirka <jirka.borovec@seznam.cz>
2022-12-23 07:50:18 +0100

    disable cudnn.allow_tf32

--- tests/unittests/__init__.py
+++ tests/unittests/__init__.py
@@ -1,6 +1,11 @@
 import os.path
 
+import torch
+
 from unittests.helpers.testers import BATCH_SIZE, NUM_BATCHES, NUM_PROCESSES, DummyMetric, MetricTester  # noqa: F401
 
 _PATH_TESTS = os.path.dirname(__file__)
 _PATH_ROOT = os.path.dirname(_PATH_TESTS)
+
+if torch.cuda.is_available():
+    torch.backends.cudnn.allow_tf32 = False
