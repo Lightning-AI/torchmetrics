2ca8826d9c358a25b018426a5007b551b3e21733
Jirka <jirka.borovec@seznam.cz>
2022-12-23 08:25:17 +0100

    disable cuda.allow_tf32

--- tests/unittests/__init__.py
+++ tests/unittests/__init__.py
@@ -8,4 +8,5 @@ _PATH_TESTS = os.path.dirname(__file__)
 _PATH_ROOT = os.path.dirname(_PATH_TESTS)
 
 if torch.cuda.is_available():
+    torch.backends.cuda.matmul.allow_tf32 = False
     torch.backends.cudnn.allow_tf32 = False
