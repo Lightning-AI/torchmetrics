2720af1f43da9324b80f93fee94da71f93fef921
Jirka <jirka.borovec@seznam.cz>
2023-02-05 02:30:30 +0100

    with error

--- src/torchmetrics/utilities/enums.py
+++ src/torchmetrics/utilities/enums.py
@@ -11,24 +11,44 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from typing import Optional
 
-from lightning_utilities.core.enums import StrEnum
+from lightning_utilities.core.enums import StrEnum as _StrEnum
 
 
-class DataType(StrEnum):
+class EnumStr(_StrEnum):
+    task: str = "Task"
+
+    @classmethod
+    def from_str(cls, value: str) -> Optional["EnumStr"]:
+        """
+        Raises:
+            ValueError:
+                If required information measure is not among the supported options.
+        """
+        _allowed_im = [im.lower() for im in cls._member_names_]
+
+        enum_key = super().from_str(value)
+        if enum_key is not None and enum_key in _allowed_im:
+            return enum_key
+        raise ValueError(f"Invalid {cls.task}: expected one of {_allowed_im}, but got {enum_key}.")
+
+
+class DataType(EnumStr):
     """Enum to represent data type.
 
     >>> "Binary" in list(DataType)
     True
     """
 
+    task = "Data type"
     BINARY = "binary"
     MULTILABEL = "multi-label"
     MULTICLASS = "multi-class"
     MULTIDIM_MULTICLASS = "multi-dim multi-class"
 
 
-class AverageMethod(StrEnum):
+class AverageMethod(EnumStr):
     """Enum to represent average method.
 
     >>> None in list(AverageMethod)
@@ -39,6 +59,7 @@ class AverageMethod(StrEnum):
     True
     """
 
+    task = "Average method"
     MICRO = "micro"
     MACRO = "macro"
     WEIGHTED = "weighted"
@@ -46,20 +67,22 @@ class AverageMethod(StrEnum):
     SAMPLES = "samples"
 
 
-class MDMCAverageMethod(StrEnum):
+class MDMCAverageMethod(EnumStr):
     """Enum to represent multi-dim multi-class average method."""
 
+    task = "MDMC Average method"
     GLOBAL = "global"
     SAMPLEWISE = "samplewise"
 
 
-class ClassificationTask(StrEnum):
+class ClassificationTask(EnumStr):
     """Enum to represent the different tasks in classification metrics.
 
     >>> "binary" in list(ClassificationTask)
     True
     """
 
+    task = "Classification"
     BINARY = "binary"
     MULTICLASS = "multiclass"
     MULTILABEL = "multilabel"
