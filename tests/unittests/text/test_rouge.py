8453b048bb17d44906fcc47d103078b3878b3be2
SkafteNicki <skaftenicki@gmail.com>
2023-02-18 16:41:47 +0100

    skip rouge if download fails

--- tests/unittests/text/test_rouge.py
+++ tests/unittests/text/test_rouge.py
@@ -24,7 +24,7 @@ from typing_extensions import Literal
 from torchmetrics.functional.text.rouge import rouge_score
 from torchmetrics.text.rouge import ROUGEScore
 from torchmetrics.utilities.imports import _NLTK_AVAILABLE, _ROUGE_SCORE_AVAILABLE
-from unittests.text.helpers import TextTester
+from unittests.text.helpers import TextTester, skip_on_connection_issues
 from unittests.text.inputs import Input, _inputs_multiple_references, _inputs_single_sentence_single_reference
 
 if _ROUGE_SCORE_AVAILABLE:
@@ -118,6 +118,7 @@ def _compute_rouge_score(
 class TestROUGEScore(TextTester):
     @pytest.mark.parametrize("ddp", [False, True])
     @pytest.mark.parametrize("dist_sync_on_step", [False, True])
+    @skip_on_connection_issues(reason="could not download nltl relevant data")
     def test_rouge_score_class(
         self, ddp, dist_sync_on_step, preds, targets, pl_rouge_metric_key, use_stemmer, accumulate
     ):
@@ -137,6 +138,7 @@ class TestROUGEScore(TextTester):
             key=pl_rouge_metric_key,
         )
 
+    @skip_on_connection_issues(reason="could not download nltl relevant data")
     def test_rouge_score_functional(self, preds, targets, pl_rouge_metric_key, use_stemmer, accumulate):
         metric_args = {"use_stemmer": use_stemmer, "accumulate": accumulate}
 
@@ -197,6 +199,7 @@ def test_rouge_metric_wrong_key_value_error():
         "rougeLsum_fmeasure",
     ],
 )
+@skip_on_connection_issues(reason="could not download nltl relevant data")
 def test_rouge_metric_normalizer_tokenizer(pl_rouge_metric_key):
     normalizer: Callable[[str], str] = lambda text: re.sub(r"[^a-z0-9]+", " ", text.lower())
     tokenizer: Callable[[str], Sequence[str]] = lambda text: re.split(r"\s+", text)
@@ -235,6 +238,7 @@ def test_rouge_metric_normalizer_tokenizer(pl_rouge_metric_key):
     ],
 )
 @pytest.mark.parametrize("use_stemmer", [False, True])
+@skip_on_connection_issues(reason="could not download nltl relevant data")
 def test_rouge_lsum_score(pl_rouge_metric_key, use_stemmer):
     """Specific tests to verify the correctness of Rouge-L and Rouge-LSum metric."""
     rouge_level, metric = pl_rouge_metric_key.split("_")
