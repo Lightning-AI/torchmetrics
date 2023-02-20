9266f750e9bbb79acccb14c3d13c304828c1ac50
Jirka <jirka.borovec@seznam.cz>
2023-02-20 22:28:08 +0100

    fixing formatting

--- src/torchmetrics/functional/text/bert.py
+++ src/torchmetrics/functional/text/bert.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import csv
-import os
 import urllib
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 from warnings import warn
