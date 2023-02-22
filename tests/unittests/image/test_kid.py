b08ce52c702995a63a3a0c58941f9b6291b14c58
Nicki Skafte Detlefsen <skaftenicki@gmail.com>
2023-02-22 11:26:23 +0100

    REsolves TODOs (#1522)
    * skipping
    * fix mypy
    * Apply suggestions from code review
    * reduce timeout
    * remove 11.1 rerun pytest
    * remove 11.1 rerun pytest in tests
    * version
    * try fix
    * another try
    * maxfail
    * retry
    * try again
    * less than 11.1
    * try removing problematic
    * increase port
    * revert changes
    * add to more metrics
    * stupid mistake
    * missing type
    * fix todos
    
    ---------
    
    Co-authored-by: Jirka Borovec <6035284+Borda@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Jirka <jirka.borovec@seznam.cz>

--- tests/unittests/image/test_kid.py
+++ tests/unittests/image/test_kid.py
@@ -77,12 +77,10 @@ def test_kid_raises_errors_and_warnings():
     with pytest.raises(TypeError, match="Got unknown input to argument `feature`"):
         KernelInceptionDistance(feature=[1, 2])
 
-    with pytest.raises(  # noqa: PT012 # todo
-        ValueError, match="Argument `subset_size` should be smaller than the number of samples"
-    ):
-        m = KernelInceptionDistance()
-        m.update(torch.randint(0, 255, (5, 3, 299, 299), dtype=torch.uint8), real=True)
-        m.update(torch.randint(0, 255, (5, 3, 299, 299), dtype=torch.uint8), real=False)
+    m = KernelInceptionDistance()
+    m.update(torch.randint(0, 255, (5, 3, 299, 299), dtype=torch.uint8), real=True)
+    m.update(torch.randint(0, 255, (5, 3, 299, 299), dtype=torch.uint8), real=False)
+    with pytest.raises(ValueError, match="Argument `subset_size` should be smaller than the number of samples"):
         m.compute()
 
 
