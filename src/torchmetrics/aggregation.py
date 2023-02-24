b84c9bc2a1986aea7fec19f044411bc6b670a6e1
Jirka <jirka.borovec@seznam.cz>
2023-02-24 14:51:52 +0100

    imports

--- src/torchmetrics/aggregation.py
+++ src/torchmetrics/aggregation.py
@@ -11,13 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import warnings
+
 from typing import Any, Callable, List, Union
 
 import torch
 from torch import Tensor
 
 from torchmetrics.metric import Metric
+from torchmetrics.utilities import rank_zero_warn
 from torchmetrics.utilities.data import dim_zero_cat
 
 
