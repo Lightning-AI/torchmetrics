431c6999bad2d3e3dff1b6db927f65739d476988
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-02-25 21:52:09 +0100

    typing 4/n: fix typing issues for `utilities` (#1555)

--- src/torchmetrics/wrappers/tracker.py
+++ src/torchmetrics/wrappers/tracker.py
@@ -120,12 +120,12 @@ class MetricTracker(ModuleList):
         self._increment_called = True
         self.append(deepcopy(self._base_metric))
 
-    def forward(self, *args, **kwargs) -> None:
+    def forward(self, *args: Any, **kwargs: Any) -> None:
         """Call forward of the current metric being tracked."""
         self._check_for_increment("forward")
         return self[-1](*args, **kwargs)
 
-    def update(self, *args, **kwargs) -> None:
+    def update(self, *args: Any, **kwargs: Any) -> None:
         """Update the current metric being tracked."""
         self._check_for_increment("update")
         self[-1].update(*args, **kwargs)
@@ -198,7 +198,7 @@ class MetricTracker(ModuleList):
         if isinstance(self._base_metric, Metric):
             fn = torch.max if self.maximize else torch.min
             try:
-                value, idx = fn(self.compute_all(), 0)
+                value, idx = fn(self.compute_all(), 0)  # type: ignore[arg-type]
                 if return_step:
                     return value.item(), idx.item()
                 return value.item()
@@ -217,7 +217,7 @@ class MetricTracker(ModuleList):
             res = self.compute_all()
             maximize = self.maximize if isinstance(self.maximize, list) else len(res) * [self.maximize]
             value, idx = {}, {}
-            for i, (k, v) in enumerate(res.items()):
+            for i, (k, v) in enumerate(res.items()):  # type: ignore[union-attr]
                 try:
                     fn = torch.max if maximize[i] else torch.min
                     out = fn(v, 0)
