09de1a202c201e7f76a0f4268b0b9db6894a6a01
twsl <45483159+twsl@users.noreply.github.com>
2023-03-01 20:02:15 +0000

    Fix input validator

--- src/torchmetrics/detection/helpers.py
+++ src/torchmetrics/detection/helpers.py
@@ -16,8 +16,17 @@ from typing import Dict, Sequence
 from torch import Tensor
 
 
-def _input_validator(preds: Sequence[Dict[str, Tensor]], targets: Sequence[Dict[str, Tensor]]) -> None:
+def _input_validator(
+    preds: Sequence[Dict[str, Tensor]], targets: Sequence[Dict[str, Tensor]], iou_type: str = "bbox"
+) -> None:
     """Ensure the correct input format of `preds` and `targets`."""
+    if iou_type == "bbox":
+        item_val_name = "boxes"
+    elif iou_type == "segm":
+        item_val_name = "masks"
+    else:
+        raise Exception(f"IOU type {iou_type} is not supported")
+
     if not isinstance(preds, Sequence):
         raise ValueError(f"Expected argument `preds` to be of type Sequence, but got {preds}")
     if not isinstance(targets, Sequence):
@@ -27,36 +36,36 @@ def _input_validator(preds: Sequence[Dict[str, Tensor]], targets: Sequence[Dict[
             f"Expected argument `preds` and `target` to have the same length, but got {len(preds)} and {len(targets)}"
         )
 
-    for k in ["boxes", "scores", "labels"]:
+    for k in [item_val_name, "scores", "labels"]:
         if any(k not in p for p in preds):
             raise ValueError(f"Expected all dicts in `preds` to contain the `{k}` key")
 
-    for k in ["boxes", "labels"]:
+    for k in [item_val_name, "labels"]:
         if any(k not in p for p in targets):
             raise ValueError(f"Expected all dicts in `target` to contain the `{k}` key")
 
-    if any(type(pred["boxes"]) is not Tensor for pred in preds):
-        raise ValueError("Expected all boxes in `preds` to be of type Tensor")
+    if any(type(pred[item_val_name]) is not Tensor for pred in preds):
+        raise ValueError(f"Expected all {item_val_name} in `preds` to be of type Tensor")
     if any(type(pred["scores"]) is not Tensor for pred in preds):
         raise ValueError("Expected all scores in `preds` to be of type Tensor")
     if any(type(pred["labels"]) is not Tensor for pred in preds):
         raise ValueError("Expected all labels in `preds` to be of type Tensor")
-    if any(type(target["boxes"]) is not Tensor for target in targets):
-        raise ValueError("Expected all boxes in `target` to be of type Tensor")
+    if any(type(target[item_val_name]) is not Tensor for target in targets):
+        raise ValueError(f"Expected all {item_val_name} in `target` to be of type Tensor")
     if any(type(target["labels"]) is not Tensor for target in targets):
         raise ValueError("Expected all labels in `target` to be of type Tensor")
 
     for i, item in enumerate(targets):
-        if item["boxes"].size(0) != item["labels"].size(0):
+        if item[item_val_name].size(0) != item["labels"].size(0):
             raise ValueError(
-                f"Input boxes and labels of sample {i} in targets have a"
-                f" different length (expected {item['boxes'].size(0)} labels, got {item['labels'].size(0)})"
+                f"Input {item_val_name} and labels of sample {i} in targets have a"
+                f" different length (expected {item[item_val_name].size(0)} labels, got {item['labels'].size(0)})"
             )
     for i, item in enumerate(preds):
-        if not (item["boxes"].size(0) == item["labels"].size(0) == item["scores"].size(0)):
+        if not (item[item_val_name].size(0) == item["labels"].size(0) == item["scores"].size(0)):
             raise ValueError(
-                f"Input boxes, labels and scores of sample {i} in predictions have a"
-                f" different length (expected {item['boxes'].size(0)} labels and scores,"
+                f"Input {item_val_name}, labels and scores of sample {i} in predictions have a"
+                f" different length (expected {item[item_val_name].size(0)} labels and scores,"
                 f" got {item['labels'].size(0)} labels and {item['scores'].size(0)})"
             )
 
