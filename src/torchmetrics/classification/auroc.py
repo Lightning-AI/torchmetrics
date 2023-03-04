19217ed2f06b49b6b199094aaef85bf677d42815
SkafteNicki <skaftenicki@gmail.com>
2023-03-04 16:07:25 +0100

    remove old imports

--- src/torchmetrics/classification/auroc.py
+++ src/torchmetrics/classification/auroc.py
@@ -33,7 +33,7 @@ from torchmetrics.metric import Metric
 from torchmetrics.utilities.data import dim_zero_cat
 from torchmetrics.utilities.enums import ClassificationTask
 from torchmetrics.utilities.imports import _MATPLOTLIB_AVAILABLE
-from torchmetrics.utilities.plot import _AX_TYPE, _PLOT_OUT_TYPE, plot_single_or_multi_val
+from torchmetrics.utilities.plot import _AX_TYPE, _PLOT_OUT_TYPE
 
 if not _MATPLOTLIB_AVAILABLE:
     __doctest_skip__ = ["BinaryAUROC.plot", "MulticlassAUROC.plot", "MultilabelAUROC.plot"]
