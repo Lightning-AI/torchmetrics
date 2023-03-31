b5ac44e8cb44c2357fb3b10cba774dc83188f410
Nicki Skafte Detlefsen <skaftenicki@gmail.com>
2023-03-31 10:25:13 +0200

    Add plotting 17/n (#1650)
    * impl + doctests
    * requirements
    * tests
    * changelog
    * fix
    * inference_mode
    
    ---------
    
    Co-authored-by: Jirka Borovec <6035284+Borda@users.noreply.github.com>
    Co-authored-by: Jirka <jirka.borovec@seznam.cz>

--- src/torchmetrics/text/infolm.py
+++ src/torchmetrics/text/infolm.py
@@ -28,10 +28,14 @@ from torchmetrics.functional.text.infolm import (
 )
 from torchmetrics.metric import Metric
 from torchmetrics.utilities.data import dim_zero_cat
-from torchmetrics.utilities.imports import _TRANSFORMERS_AVAILABLE
+from torchmetrics.utilities.imports import _MATPLOTLIB_AVAILABLE, _TRANSFORMERS_AVAILABLE
+from torchmetrics.utilities.plot import _AX_TYPE, _PLOT_OUT_TYPE
+
+if not _MATPLOTLIB_AVAILABLE:
+    __doctest_skip__ = ["InfoLM.plot"]
 
 if not _TRANSFORMERS_AVAILABLE:
-    __doctest_skip__ = ["InfoLM"]
+    __doctest_skip__ = ["InfoLM", "InfoLM.plot"]
 
 
 class InfoLM(Metric):
@@ -193,3 +197,46 @@ class InfoLM(Metric):
             return info_lm_score.mean(), info_lm_score
 
         return info_lm_score.mean()
+
+    def plot(
+        self, val: Optional[Union[Tensor, Sequence[Tensor]]] = None, ax: Optional[_AX_TYPE] = None
+    ) -> _PLOT_OUT_TYPE:
+        """Plot a single or multiple values from the metric.
+
+        Args:
+            val: Either a single result from calling `metric.forward` or `metric.compute` or a list of these results.
+                If no value is provided, will automatically call `metric.compute` and plot that result.
+            ax: An matplotlib axis object. If provided will add plot to that axis
+
+        Returns:
+            Figure and Axes object
+
+        Raises:
+            ModuleNotFoundError:
+                If `matplotlib` is not installed
+
+        .. plot::
+            :scale: 75
+
+            >>> # Example plotting a single value
+            >>> from torchmetrics.text.infolm import InfoLM
+            >>> metric = InfoLM('google/bert_uncased_L-2_H-128_A-2', idf=False)
+            >>> preds = ['he read the book because he was interested in world history']
+            >>> target = ['he was interested in world history because he read the book']
+            >>> metric.update(preds, target)
+            >>> fig_, ax_ = metric.plot()
+
+        .. plot::
+            :scale: 75
+
+            >>> # Example plotting multiple values
+            >>> from torchmetrics.text.infolm import InfoLM
+            >>> metric = InfoLM('google/bert_uncased_L-2_H-128_A-2', idf=False)
+            >>> preds = ["this is the prediction", "there is an other sample"]
+            >>> target = ["this is the reference", "there is another one"]
+            >>> values = [ ]
+            >>> for _ in range(10):
+            ...     values.append(metric(preds, target))
+            >>> fig_, ax_ = metric.plot(values)
+        """
+        return self._plot(val, ax)
