9472744e5da0ee7815ec762eba8eb5ba727501a0
Jirka <jirka.borovec@seznam.cz>
2023-04-04 19:04:00 +0200

    .

--- src/torchmetrics/audio/_deprecated.py
+++ src/torchmetrics/audio/_deprecated.py
@@ -84,7 +84,7 @@ class _SignalDistortionRatio(SignalDistortionRatio):
     >>> from torchmetrics.functional import signal_distortion_ratio
     >>> preds = torch.randn(4, 2, 8000)  # [batch, spk, time]
     >>> target = torch.randn(4, 2, 8000)
-    >>> pit = _PermutationInvariantTraining(_signal_distortion_ratio, 'max')
+    >>> pit = _PermutationInvariantTraining(signal_distortion_ratio, 'max')
     >>> pit(preds, target)
     tensor(-11.6051)
     """
