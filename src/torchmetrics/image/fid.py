ca956ff0ba62d9b488cfc1c6a5fbcf9e9cacb580
SkafteNicki <skaftenicki@gmail.com>
2023-04-13 13:01:23 +0200

    fix

--- src/torchmetrics/image/fid.py
+++ src/torchmetrics/image/fid.py
@@ -23,7 +23,7 @@ from torch.nn.functional import adaptive_avg_pool2d
 
 from torchmetrics.metric import Metric
 from torchmetrics.utilities import rank_zero_info
-from torchmetrics.utilities.imports import _MATPLOTLIB_AVAILABLE, _SCIPY_AVAILABLE, _TORCH_FIDELITY_AVAILABLE
+from torchmetrics.utilities.imports import _MATPLOTLIB_AVAILABLE, _TORCH_FIDELITY_AVAILABLE
 from torchmetrics.utilities.plot import _AX_TYPE, _PLOT_OUT_TYPE
 
 if not _MATPLOTLIB_AVAILABLE:
@@ -42,10 +42,6 @@ else:
     interpolate_bilinear_2d_like_tensorflow1x = None
 
     __doctest_skip__ = ["FrechetInceptionDistance", "FrechetInceptionDistance.plot"]
-
-
-if _SCIPY_AVAILABLE:
-    import scipy
 
 
 class NoTrainInceptionV3(_FeatureExtractorInceptionV3):
@@ -156,45 +152,7 @@ class NoTrainInceptionV3(_FeatureExtractorInceptionV3):
         return out[0].reshape(x.shape[0], -1)
 
 
-class MatrixSquareRoot(Function):
-    """Square root of a positive definite matrix.
-
-    All credit to `Square Root of a Positive Definite Matrix`_
-    """
-
-    @staticmethod
-    def forward(ctx: Any, input_data: Tensor) -> Tensor:
-        """Forward pass for the matrix square root."""
-        # TODO: update whenever pytorch gets an matrix square root function
-        # Issue: https://github.com/pytorch/pytorch/issues/9983
-        m = input_data.detach().cpu().numpy().astype(np.float_)
-        scipy_res, _ = scipy.linalg.sqrtm(m, disp=False)
-        sqrtm = torch.from_numpy(scipy_res.real).to(input_data)
-        ctx.save_for_backward(sqrtm)
-        return sqrtm
-
-    @staticmethod
-    def backward(ctx: Any, grad_output: Tensor) -> Tensor:
-        """Backward pass for matrix square root."""
-        if not ctx.needs_input_grad[0]:
-            return None
-        (sqrtm,) = ctx.saved_tensors
-        sqrtm = sqrtm.data.cpu().numpy().astype(np.float_)
-        gm = grad_output.data.cpu().numpy().astype(np.float_)
-
-        # Given a positive semi-definite matrix X,
-        # since X = X^{1/2}X^{1/2}, we can compute the gradient of the
-        # matrix square root dX^{1/2} by solving the Sylvester equation:
-        # dX = (d(X^{1/2})X^{1/2} + X^{1/2}(dX^{1/2}).
-        grad_sqrtm = scipy.linalg.solve_sylvester(sqrtm, sqrtm, gm)
-
-        return torch.from_numpy(grad_sqrtm).to(grad_output)
-
-
-sqrtm = MatrixSquareRoot.apply
-
-
-def _compute_fid(mu1: Tensor, sigma1: Tensor, mu2: Tensor, sigma2: Tensor, eps: float = 1e-6) -> Tensor:
+def _compute_fid(mu1: Tensor, sigma1: Tensor, mu2: Tensor, sigma2: Tensor) -> Tensor:
     r"""Compute adjusted version of `Fid Score`_.
 
     The Frechet Inception Distance between two multivariate Gaussians X_x ~ N(mu_1, sigm_1)
@@ -205,22 +163,15 @@ def _compute_fid(mu1: Tensor, sigma1: Tensor, mu2: Tensor, sigma2: Tensor, eps: 
         sigma1: covariance matrix over activations calculated on predicted (x) samples
         mu2: mean of activations calculated on target (y) samples
         sigma2: covariance matrix over activations calculated on target (y) samples
-        eps: offset constant - used if sigma_1 @ sigma_2 matrix is singular
 
     Returns:
         Scalar value of the distance between sets.
     """
-    diff = mu1 - mu2
+    a = (mu1 - mu2).square().sum(dim=-1)
+    b = sigma1.trace() + sigma2.trace()
+    c = torch.linalg.eigvals(sigma1 @ sigma2).sqrt().real.sum(dim=-1)
 
-    covmean = sqrtm(sigma1.mm(sigma2))
-    # Product might be almost singular
-    if not torch.isfinite(covmean).all():
-        rank_zero_info(f"FID calculation produces singular product; adding {eps} to diagonal of covariance estimates")
-        offset = torch.eye(sigma1.size(0), device=mu1.device, dtype=mu1.dtype) * eps
-        covmean = sqrtm((sigma1 + offset).mm(sigma2 + offset))
-
-    tr_covmean = torch.trace(covmean)
-    return diff.dot(diff) + torch.trace(sigma1) + torch.trace(sigma2) - 2 * tr_covmean
+    return a + b - 2 * c
 
 
 class FrechetInceptionDistance(Metric):
