8393c5e350eae7df82ae3a70a53210f576c23a4f
Daniel Stancl <46073029+stancld@users.noreply.github.com>
2023-04-16 08:37:11 +0200

    Update regex match accordingly in tests/unittests/image/test_psnrb.py

--- tests/unittests/image/test_psnrb.py
+++ tests/unittests/image/test_psnrb.py
@@ -95,5 +95,5 @@ class TestPSNR(MetricTester):
 
 def test_error_on_color_images():
     """Test that appropriate error is raised when color images are passed to PSNRB metric."""
-    with pytest.raises(ValueError, match="``psnrb metric expects grayscale images.*"):
+    with pytest.raises(ValueError, match="`psnrb` metric expects grayscale images.*"):
         peak_signal_noise_ratio_with_blocked_effect(torch.rand(1, 3, 16, 16), torch.rand(1, 3, 16, 16))
