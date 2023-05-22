f07d9e7c009cbf97d1632ee7c534cb3783f3b8ca
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-05-22 11:26:20 -0400

    ruff: PLE (#1796)

--- src/torchmetrics/audio/_deprecated.py
+++ src/torchmetrics/audio/_deprecated.py
@@ -30,7 +30,7 @@ class _PermutationInvariantTraining(PermutationInvariantTraining):
         **kwargs: Any,
     ) -> None:
         _deprecated_root_import_class("PermutationInvariantTraining", "audio")
-        return super().__init__(metric_func=metric_func, mode=mode, eval_func=eval_func, **kwargs)
+        super().__init__(metric_func=metric_func, mode=mode, eval_func=eval_func, **kwargs)
 
 
 class _ScaleInvariantSignalDistortionRatio(ScaleInvariantSignalDistortionRatio):
@@ -50,7 +50,7 @@ class _ScaleInvariantSignalDistortionRatio(ScaleInvariantSignalDistortionRatio):
         **kwargs: Any,
     ) -> None:
         _deprecated_root_import_class("ScaleInvariantSignalDistortionRatio", "audio")
-        return super().__init__(zero_mean=zero_mean, **kwargs)
+        super().__init__(zero_mean=zero_mean, **kwargs)
 
 
 class _ScaleInvariantSignalNoiseRatio(ScaleInvariantSignalNoiseRatio):
@@ -69,7 +69,7 @@ class _ScaleInvariantSignalNoiseRatio(ScaleInvariantSignalNoiseRatio):
         **kwargs: Any,
     ) -> None:
         _deprecated_root_import_class("ScaleInvariantSignalNoiseRatio", "audio")
-        return super().__init__(**kwargs)
+        super().__init__(**kwargs)
 
 
 class _SignalDistortionRatio(SignalDistortionRatio):
@@ -101,7 +101,7 @@ class _SignalDistortionRatio(SignalDistortionRatio):
         **kwargs: Any,
     ) -> None:
         _deprecated_root_import_class("SignalDistortionRatio", "audio")
-        return super().__init__(
+        super().__init__(
             use_cg_iter=use_cg_iter, filter_length=filter_length, zero_mean=zero_mean, load_diag=load_diag, **kwargs
         )
 
@@ -123,4 +123,4 @@ class _SignalNoiseRatio(SignalNoiseRatio):
         **kwargs: Any,
     ) -> None:
         _deprecated_root_import_class("SignalNoiseRatio", "audio")
-        return super().__init__(zero_mean=zero_mean, **kwargs)
+        super().__init__(zero_mean=zero_mean, **kwargs)
