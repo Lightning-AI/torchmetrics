7e5218d396528d19939412640c7720b431f837bf
Jirka B <j.borovec+github@gmail.com>
2023-05-21 22:35:05 -0400

    fixed

--- src/torchmetrics/utilities/__init__.py
+++ src/torchmetrics/utilities/__init__.py
@@ -15,7 +15,7 @@ from torchmetrics.utilities.checks import check_forward_full_state_property  # n
 from torchmetrics.utilities.data import apply_to_collection  # noqa: F401
 from torchmetrics.utilities.distributed import class_reduce, reduce  # noqa: F401
 from torchmetrics.utilities.prints import (
-    _future_warning,  # noqa: F401
+    _future_warning,
     rank_zero_debug,
     rank_zero_info,
     rank_zero_warn,
