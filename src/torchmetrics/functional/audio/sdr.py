fc1e2393565bfa9d93071e5fcb5f0386253b1095
quancs <quancs@qq.com>
2023-05-24 14:21:36 +0800

    +SA-SDR

--- src/torchmetrics/functional/audio/sdr.py
+++ src/torchmetrics/functional/audio/sdr.py
@@ -242,3 +242,47 @@ def scale_invariant_signal_distortion_ratio(preds: Tensor, target: Tensor, zero_
 
     val = (torch.sum(target_scaled**2, dim=-1) + eps) / (torch.sum(noise**2, dim=-1) + eps)
     return 10 * torch.log10(val)
+
+
+def source_aggregated_signal_distortion_ratio(
+    preds: Tensor,
+    target: Tensor,
+    scale_invariant: bool = True,
+    zero_mean: bool = False,
+) -> Tensor:
+    """`Source-aggregated signal-to-distortion ratio`_ (SI-SDR).
+
+    The SA-SDR is proposed to consider a global SDR over all output channels at the same time
+
+    Args:
+        preds: float tensor with shape ``(..., spk, time)``
+        target: float tensor with shape ``(..., spk, time)``
+        scale_invariant: if True, scale the targets of different speakers with the same value.
+        zero_mean: If to zero mean target and preds or not
+
+    Returns:
+        Float tensor with shape ``(...,)`` of SA-SDR values per sample
+    """
+    _check_same_shape(preds, target)
+    if preds.ndim < 2:
+        raise RuntimeError(f'the preds and target should be the shape (..., spk, time), but {preds.shape} found')
+
+    eps = torch.finfo(preds.dtype).eps
+
+    if zero_mean:
+        target = target - torch.mean(target, dim=-1, keepdim=True)
+        preds = preds - torch.mean(preds, dim=-1, keepdim=True)
+
+    if scale_invariant:
+        # scale the targets of different speakers with the same value
+        preds_sum = preds.sum(dim=-2, keepdim=True)
+        target_sum = target.sum(dim=-2, keepdim=True)
+        alpha = ((preds_sum * target_sum).sum(dim=-1, keepdim=True) + eps) / (
+            (target_sum**2).sum(dim=-1, keepdim=True) + eps
+        ) # shape [..., 1, 1]
+        target = alpha * target
+
+    noise = target - preds
+
+    val = (torch.sum(target**2, dim=-1) + eps) / (torch.sum(noise**2, dim=-1) + eps)
+    return 10 * torch.log10(val)
