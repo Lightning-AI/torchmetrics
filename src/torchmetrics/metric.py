77a2276f9697fb874c742072c9fff36efe89a998
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-06-26 11:12:02 +0200

    Update src/torchmetrics/metric.py

--- src/torchmetrics/metric.py
+++ src/torchmetrics/metric.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 # It is needed to distinguish between native float and Metric's' function called float.
 # later, this function was used instead of the built-in float type...
 import builtins
