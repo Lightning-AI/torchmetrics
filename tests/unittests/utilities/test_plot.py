76e85683f7353761ea983f3204ab0717f1f13f61
SkafteNicki <skaftenicki@gmail.com>
2023-07-01 15:50:09 +0200

    more skip

--- tests/unittests/utilities/test_plot.py
+++ tests/unittests/utilities/test_plot.py
@@ -161,7 +161,11 @@ from torchmetrics.text import (
     WordInfoLost,
     WordInfoPreserved,
 )
-from torchmetrics.utilities.imports import _TORCH_GREATER_EQUAL_1_9, _TORCHAUDIO_GREATER_EQUAL_0_10
+from torchmetrics.utilities.imports import (
+    _TORCH_GREATER_EQUAL_1_9,
+    _TORCH_GREATER_EQUAL_1_10,
+    _TORCHAUDIO_GREATER_EQUAL_0_10,
+)
 from torchmetrics.wrappers import (
     BootStrapper,
     ClasswiseWrapper,
@@ -661,7 +665,7 @@ def test_plot_methods(metric_class: object, preds: Callable, target: Callable, n
             lambda: torch.randint(0, 200, (30, 3, 299, 299), dtype=torch.uint8),
             False,
             id="memorization informed frechet inception distance",
-            marks=pytest.mark.skipif(not _TORCH_GREATER_EQUAL_1_9, reason="test requires torch>=1.9"),
+            marks=pytest.mark.skipif(not _TORCH_GREATER_EQUAL_1_10, reason="test requires torch>=1.9"),
         ),
     ],
 )
