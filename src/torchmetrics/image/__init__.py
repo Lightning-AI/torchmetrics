f1b1711a41595ff797cf186225164a923cdacecf
SkafteNicki <skaftenicki@gmail.com>
2023-07-04 15:56:00 +0200

    add init files

--- src/torchmetrics/image/__init__.py
+++ src/torchmetrics/image/__init__.py
@@ -21,6 +21,7 @@ from torchmetrics.image.sam import SpectralAngleMapper
 from torchmetrics.image.ssim import MultiScaleStructuralSimilarityIndexMeasure, StructuralSimilarityIndexMeasure
 from torchmetrics.image.tv import TotalVariation
 from torchmetrics.image.uqi import UniversalImageQualityIndex
+from torchmetrics.image.vif import VisualInformationFidelity
 from torchmetrics.utilities.imports import _LPIPS_AVAILABLE, _TORCH_FIDELITY_AVAILABLE
 
 __all__ = [
