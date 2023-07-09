db0d7f325809d12825d12f0fe7b73eeb6bbe27dc
SkafteNicki <skaftenicki@gmail.com>
2023-07-09 14:09:00 +0200

    fix formatting and add tests

--- src/torchmetrics/functional/detection/iou.py
+++ src/torchmetrics/functional/detection/iou.py
@@ -38,10 +38,7 @@ def _iou_update(
 def _iou_compute(iou: torch.Tensor, labels_eq: bool = True) -> torch.Tensor:
     if labels_eq:
         return iou.diag().mean()
-    if iou.numel() > 0:
-        return iou.mean()
-    else:
-        return torch.tensor(0.0)
+    return iou.mean() if iou.numel() > 0 else torch.tensor(0.0)
 
 
 def intersection_over_union(
