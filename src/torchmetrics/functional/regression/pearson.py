595c26b53de17b9c0f407058fd8a7bb78191bee8
SkafteNicki <skaftenicki@gmail.com>
2023-07-17 21:02:17 +0200

    add warning

--- src/torchmetrics/functional/regression/pearson.py
+++ src/torchmetrics/functional/regression/pearson.py
@@ -11,12 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import math
 from typing import Tuple
 
 import torch
 from torch import Tensor
 
 from torchmetrics.functional.regression.utils import _check_data_shape_to_num_outputs
+from torchmetrics.utilities import rank_zero_warn
 from torchmetrics.utilities.checks import _check_same_shape
 
 
@@ -97,6 +99,15 @@ def _pearson_corrcoef_compute(
     if var_x.dtype == torch.float16 and var_x.device == torch.device("cpu"):
         var_x = var_x.bfloat16()
         var_y = var_y.bfloat16()
+
+    bound = math.sqrt(torch.finfo(var_x.dtype).eps)
+    if (var_x < bound).any() or (var_y < bound).any():
+        rank_zero_warn(
+            "The variance of predictions or target is close to zero. This can cause instability in Pearson correlation"
+            "coefficient, leading to wrong results. Consider re-scaling the input if possible or computing using a"
+            f"larger dtype (currently using {var_x.dtype}).",
+            UserWarning,
+        )
 
     corrcoef = (corr_xy / (var_x * var_y).sqrt()).squeeze()
     return torch.clamp(corrcoef, -1.0, 1.0)
