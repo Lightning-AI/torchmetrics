29913069a8ce0874f2c97c4afb7991baa5a55abe
Ian Maquignaz <ian.maquignaz@gmail.com>
2023-07-27 02:11:07 -0400

    Fixed CLIPScore import in docs (#1943)
    (cherry picked from commit dc25a6e740087e0dd3306e80cff8f50b3e012578)

--- src/torchmetrics/multimodal/clip_score.py
+++ src/torchmetrics/multimodal/clip_score.py
@@ -76,7 +76,7 @@ class CLIPScore(Metric):
     Example:
         >>> import torch
         >>> _ = torch.manual_seed(42)
-        >>> from torchmetrics.multimodal import CLIPScore
+        >>> from torchmetrics.multimodal.clip_score import CLIPScore
         >>> metric = CLIPScore(model_name_or_path="openai/clip-vit-base-patch16")
         >>> score = metric(torch.randint(255, (3, 224, 224)), "a photo of a cat")
         >>> print(score.detach())
@@ -148,7 +148,7 @@ class CLIPScore(Metric):
 
             >>> # Example plotting a single value
             >>> import torch
-            >>> from torchmetrics.multimodal import CLIPScore
+            >>> from torchmetrics.multimodal.clip_score import CLIPScore
             >>> metric = CLIPScore(model_name_or_path="openai/clip-vit-base-patch16")
             >>> metric.update(torch.randint(255, (3, 224, 224)), "a photo of a cat")
             >>> fig_, ax_ = metric.plot()
@@ -158,7 +158,7 @@ class CLIPScore(Metric):
 
             >>> # Example plotting multiple values
             >>> import torch
-            >>> from torchmetrics.multimodal import CLIPScore
+            >>> from torchmetrics.multimodal.clip_score import CLIPScore
             >>> metric = CLIPScore(model_name_or_path="openai/clip-vit-base-patch16")
             >>> values = [ ]
             >>> for _ in range(10):
