62e18da97544a0e5ef80d07140b31720fb66a88b
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-08-07 12:47:50 +0200

    link

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -439,6 +439,8 @@ linkcheck_ignore = [
     "https://ieeexplore.ieee.org/abstract/document/1576816",
     # Performance measurement in blind audio source separation
     "https://ieeexplore.ieee.org/abstract/document/1643671",
+    # A Non-Intrusive Quality and Intelligibility Measure of Reverberant and Dereverberated Speech
+    "https://ieeexplore.ieee.org/abstract/document/5547575",
     # Robust parameter estimation with a small bias against heavy contamination
     "https://www.sciencedirect.com/science/article/pii/S0047259X08000456",
 ]
