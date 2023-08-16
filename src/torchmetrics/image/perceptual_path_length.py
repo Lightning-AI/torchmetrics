c9c3afcfb7e7158bb4bad3f49f90578114ca6b72
SkafteNicki <skaftenicki@gmail.com>
2023-08-16 12:54:47 +0200

    fix warnings

--- src/torchmetrics/image/perceptual_path_length.py
+++ src/torchmetrics/image/perceptual_path_length.py
@@ -17,7 +17,7 @@ from torch import Tensor, nn
 
 from torchmetrics.functional.image.lpips import _LPIPS
 from torchmetrics.functional.image.perceptual_path_length import (
-    _GeneratorType,
+    GeneratorType,
     _perceptual_path_length_validate_arguments,
     _validate_generator_model,
     perceptual_path_length,
@@ -157,7 +157,7 @@ class PerceptualPathLength(Metric):
         else:
             raise ValueError(f"sim_net must be a nn.Module or one of 'alex', 'vgg', 'squeeze', got {sim_net}")
 
-    def update(self, generator: _GeneratorType) -> None:
+    def update(self, generator: GeneratorType) -> None:
         """Update the generator model."""
         _validate_generator_model(generator, self.conditional)
         self.generator = generator
