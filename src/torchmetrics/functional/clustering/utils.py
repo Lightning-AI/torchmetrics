e4523d41d04783e4c12b42898ee79952464fc486
Shion <smatsumoto97@gmail.com>
2023-08-24 20:55:35 +0900

    Test contingency matrix calculation

--- src/torchmetrics/functional/clustering/utils.py
+++ src/torchmetrics/functional/clustering/utils.py
@@ -20,7 +20,7 @@ from torchmetrics.utilities.checks import _check_same_shape
 
 
 def calculate_contingency_matrix(
-    preds: Tensor, target: Tensor, eps: Optional[float] = 1e-16, sparse: bool = False
+    preds: Tensor, target: Tensor, eps: Optional[float] = None, sparse: bool = False
 ) -> Tensor:
     """Calculate contingency matrix.
 
@@ -34,9 +34,21 @@ def calculate_contingency_matrix(
     Returns:
         contingency: contingency matrix of shape (n_classes_target, n_classes_preds)
 
+    Example:
+        >>> import torch
+        >>> from torchmetrics.functional.clustering.utils import calculate_contingency_matrix
+        >>> preds = torch.tensor([2, 1, 0, 1, 0])
+        >>> target = torch.tensor([0, 2, 1, 1, 0])
+        >>> calculate_contingency_matrix(preds, target, eps=1e-16)
+        tensor([[1.0000e+00, 1.0000e-16, 1.0000e+00],
+                [1.0000e+00, 1.0000e+00, 1.0000e-16],
+                [1.0000e-16, 1.0000e+00, 1.0000e-16]])
+
     """
     if eps is not None and sparse is True:
         raise ValueError("Cannot specify `eps` and return sparse tensor.")
+    if preds.ndim != 1 or target.ndim != 1:
+        raise ValueError(f"Expected 1d `preds` and `target` but got {preds.ndim} and {target.dim}.")
 
     preds_classes, preds_idx = torch.unique(preds, return_inverse=True)
     target_classes, target_idx = torch.unique(target, return_inverse=True)
@@ -45,13 +57,23 @@ def calculate_contingency_matrix(
     n_classes_target = target_classes.size(0)
 
     contingency = torch.sparse_coo_tensor(
-        torch.stack((target_idx, preds_idx)), torch.ones(target_idx.size(0)), (n_classes_target, n_classes_preds)
+        torch.stack(
+            (
+                target_idx,
+                preds_idx,
+            )
+        ),
+        torch.ones(target_idx.size(0)),
+        (
+            n_classes_target,
+            n_classes_preds,
+        ),
     )
 
     if not sparse:
         contingency = contingency.to_dense()
-    if eps:
-        contingency = contingency + eps
+        if eps:
+            contingency = contingency + eps
 
     return contingency
 
