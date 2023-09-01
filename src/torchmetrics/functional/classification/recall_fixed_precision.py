67113757e939c45eb8485cb82ee78a8ee6950f4f
Balázs Glávits <balazs@glavits.hu>
2023-09-01 14:52:04 +0000

    Improved the performance of RecallAtFixedPrecision for large batch sizes (#2042)
    Co-authored-by: Nicki Skafte Detlefsen <skaftenicki@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    (cherry picked from commit 694399abc0f4647847abb9bb8fbdc9732aa307aa)

--- src/torchmetrics/functional/classification/recall_fixed_precision.py
+++ src/torchmetrics/functional/classification/recall_fixed_precision.py
@@ -37,21 +37,39 @@ from torchmetrics.functional.classification.precision_recall_curve import (
 from torchmetrics.utilities.enums import ClassificationTask
 
 
+def _lexargmax(x: Tensor) -> Tensor:
+    """Returns the index of the maximum value in a list of tuples according to lexicographic ordering.
+
+    Based on https://stackoverflow.com/a/65615160
+
+    """
+    idx: Optional[Tensor] = None
+    for k in range(x.shape[1]):
+        col: Tensor = x[idx, k] if idx is not None else x[:, k]
+        z = torch.where(col == col.max())[0]
+        idx = z if idx is None else idx[z]
+        if len(idx) < 2:
+            break
+    if idx is None:
+        raise ValueError("Failed to extract index")
+    return idx
+
+
 def _recall_at_precision(
     precision: Tensor,
     recall: Tensor,
     thresholds: Tensor,
     min_precision: float,
 ) -> Tuple[Tensor, Tensor]:
-    try:
-        max_recall, _, best_threshold = max(
-            (r, p, t) for p, r, t in zip(precision, recall, thresholds) if p >= min_precision
-        )
+    max_recall = torch.tensor(0.0, device=recall.device, dtype=recall.dtype)
+    best_threshold = torch.tensor(0)
 
-    except ValueError:
-        max_recall = torch.tensor(0.0, device=recall.device, dtype=recall.dtype)
-        best_threshold = torch.tensor(0)
-
+    zipped_len = min(t.shape[0] for t in (recall, precision, thresholds))
+    zipped = torch.vstack((recall[:zipped_len], precision[:zipped_len], thresholds[:zipped_len])).T
+    zipped_masked = zipped[zipped[:, 1] >= min_precision]
+    if zipped_masked.shape[0] > 0:
+        idx = _lexargmax(zipped_masked)[0]
+        max_recall, _, best_threshold = zipped_masked[idx]
     if max_recall == 0.0:
         best_threshold = torch.tensor(1e6, device=thresholds.device, dtype=thresholds.dtype)
 
