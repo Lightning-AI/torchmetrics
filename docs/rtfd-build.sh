e2150c5f17a08d54a9497754fd1478f435937be2
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-09-21 22:48:18 +0200

    docs: switch to new Lit template (#2097)

---
+++ docs/rtfd-build.sh
@@ -0,0 +1,12 @@
+# building for PRs and skip stable and latest states
+
+if ! [ $READTHEDOCS_VERSION == "latest" -o $READTHEDOCS_VERSION == "stable" ];
+then
+    cd ./docs ;
+    export SPHINX_FETCH_ASSETS=0 ;
+    make html --jobs $(nproc) ;
+    ls -lh build
+else
+    echo "Void build... :-]" ;
+    mkdir -p ./docs/build/html
+fi
