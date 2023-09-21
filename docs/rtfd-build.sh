43b5981ebf98f86d7602901882d2ed76ab79d606
Jirka <jirka.borovec@seznam.cz>
2023-09-21 15:51:32 +0200

    sh

--- docs/rtfd-build.sh
+++ docs/rtfd-build.sh
@@ -2,10 +2,10 @@
 
 if ! [ $READTHEDOCS_VERSION == "latest" -o $READTHEDOCS_VERSION == "stable" ];
 then
-    cd ./docs/source-pytorch ;
+    cd ./docs/source ;
     export SPHINX_FETCH_ASSETS=0 ;
-    make html --jobs  $(nproc) ;
-    ls -lh ../build
+    make html --jobs $(nproc) ;
+    ls -lh build
 else
     echo "Void build... :-]" ;
     mkdir -p ./docs/build/html
