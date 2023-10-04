71cc055a9c80a9e9458ea6a916c8c53edabd1c7a
Shion <smatsumoto97@gmail.com>
2023-10-04 21:12:18 +0900

    remove update called

--- src/torchmetrics/metric.py
+++ src/torchmetrics/metric.py
@@ -167,11 +167,6 @@ class Metric(Module, ABC):
         self._cache: Optional[Dict[str, Union[List[Tensor], Tensor]]] = None
 
     @property
-    def _update_called(self) -> bool:
-        # TODO: this is needed for internal lightning, remove after v0.12 and update on lightning side
-        return self._update_count > 0
-
-    @property
     def update_called(self) -> bool:
         """Returns `True` if `update` or `forward` has been called initialization or last `reset`."""
         return self._update_count > 0
