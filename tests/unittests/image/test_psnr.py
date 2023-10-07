3730257cdadc0d0e1174a4a192e058fb3d6755ff
stancld <daniel.stancl@gmail.com>
2023-10-07 13:53:03 +0200

    Skip half_cpu tests as operations are newly supported

--- tests/unittests/image/test_psnr.py
+++ tests/unittests/image/test_psnr.py
@@ -21,6 +21,7 @@ import torch
 from skimage.metrics import peak_signal_noise_ratio as skimage_peak_signal_noise_ratio
 from torchmetrics.functional import peak_signal_noise_ratio
 from torchmetrics.image import PeakSignalNoiseRatio
+from torchmetrics.utilities.imports import _TORCH_GREATER_EQUAL_2_1
 
 from unittests import BATCH_SIZE, NUM_BATCHES
 from unittests.helpers import seed_all
@@ -127,6 +128,7 @@ class TestPSNR(MetricTester):
 
     # PSNR half + cpu does not work due to missing support in torch.log
     @pytest.mark.xfail(reason="PSNR metric does not support cpu + half precision")
+    @pytest.mark.skipif(_TORCH_GREATER_EQUAL_2_1, reason="PyTorch>=2.1 supports a lot of cpu + half operations.")
     def test_psnr_half_cpu(self, preds, target, data_range, reduction, dim, base, ref_metric):
         """Test dtype support of the metric on CPU."""
         self.run_precision_test_cpu(
