eca986f7d999a0033f8387545a895b7c8595a765
Shion <smatsumoto97@gmail.com>
2023-10-09 22:32:55 +0900

    define input class rmsesw

--- tests/unittests/image/test_rmse_sw.py
+++ tests/unittests/image/test_rmse_sw.py
@@ -11,17 +11,25 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 from functools import partial
+from typing import NamedTuple
 
 import pytest
 import sewar
 import torch
+from torch import Tensor
 from torchmetrics.functional import root_mean_squared_error_using_sliding_window
 from torchmetrics.image import RootMeanSquaredErrorUsingSlidingWindow
 
-from unittests import BATCH_SIZE, NUM_BATCHES, _Input
+from unittests import BATCH_SIZE, NUM_BATCHES
 from unittests.helpers.testers import MetricTester
+
+
+class _Input(NamedTuple):
+    preds: Tensor
+    target: Tensor
+    window_size: int
+
 
 _inputs = []
 for size, channel, window_size, dtype in [
