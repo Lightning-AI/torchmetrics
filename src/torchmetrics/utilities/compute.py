b698f877a24331a2a0c3cb08f1948cce6b4bda5f
akiyuki ishikawa <aki.y.ishikawa@gmail.com>
2023-11-02 21:30:12 +0900

    Fix type error

--- src/torchmetrics/utilities/compute.py
+++ src/torchmetrics/utilities/compute.py
@@ -52,7 +52,7 @@ def _safe_divide(num: Tensor, denom: Tensor, zero_division: float = 0) -> Tensor
     """
     num = num if num.is_floating_point() else num.float()
     denom = denom if denom.is_floating_point() else denom.float()
-    return torch.where(denom != 0, num / denom, zero_division)
+    return torch.where(denom != float(0), num / denom, float(zero_division))
 
 
 def _adjust_weights_safe_divide(
