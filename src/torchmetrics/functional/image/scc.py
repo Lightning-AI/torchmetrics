33b6d9073660b179159a8a5602c796f6137fbcfa
HoseinAkbarzadeh <hoseinakbarzadeh75@gmail.com>
2023-11-30 15:46:35 +0000

    fixing the variable name mistake.

--- src/torchmetrics/functional/image/scc.py
+++ src/torchmetrics/functional/image/scc.py
@@ -102,7 +102,7 @@ def _signal_convolve_2d(input_img: Tensor, kernel: Tensor) -> Tensor:
 
 def _hp_2d_laplacian(input_img: Tensor, kernel: Tensor) -> Tensor:
     """Applies 2-D Laplace filter to the input tensor with the given high pass filter."""
-    return _signal_convolve_2d(input, kernel) * 2.0
+    return _signal_convolve_2d(input_img, kernel) * 2.0
 
 
 def _local_variance_covariance(preds: Tensor, target: Tensor, window: Tensor) -> Tuple[Tensor, Tensor, Tensor]:
