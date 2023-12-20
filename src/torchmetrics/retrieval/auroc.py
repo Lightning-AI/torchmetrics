1ed4128e579f56b725e460137147f690f29b9cdd
Nicki Skafte <skaftenicki@gmail.com>
2023-12-20 14:45:56 +0100

    fix spelling when skipping

--- src/torchmetrics/retrieval/auroc.py
+++ src/torchmetrics/retrieval/auroc.py
@@ -22,7 +22,7 @@ from torchmetrics.utilities.imports import _MATPLOTLIB_AVAILABLE
 from torchmetrics.utilities.plot import _AX_TYPE, _PLOT_OUT_TYPE
 
 if not _MATPLOTLIB_AVAILABLE:
-    __doctest_skip__ = ["RetrievalMAP.plot"]
+    __doctest_skip__ = ["RetrievalAUROC.plot"]
 
 
 class RetrievalAUROC(RetrievalMetric):
