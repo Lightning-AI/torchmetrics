Fix backprop in LPIPS (#2326)
* fixes + tests
* fix doctests

(cherry picked from commit 18b181d522a53beaa3a1ae39ca6e2c8323958a68)

src/torchmetrics/functional/image/lpips.py
@@ -275,7 +275,7 @@ class _LPIPS(nn.Module):
             net: Indicate backbone to use, choose between ['alex','vgg','squeeze']
             spatial: If input should be spatial averaged
             pnet_rand: If backbone should be random or use imagenet pre-trained weights
-            pnet_tune: If backprop should be enabled
+            pnet_tune: If backprop should be enabled for both backbone and linear layers
             use_dropout: If dropout layers should be added
             model_path: Model path to load pretained models from
             eval_mode: If network should be in evaluation mode
@@ -326,6 +326,10 @@ class _LPIPS(nn.Module):
 
         if eval_mode:
             self.eval()
+
+        if not self.pnet_tune:
+            for param in self.parameters():
+                param.requires_grad = False
 
     def forward(
         self, in0: Tensor, in1: Tensor, retperlayer: bool = False, normalize: bool = False
@@ -423,7 +427,7 @@ def learned_perceptual_image_patch_similarity(
         >>> img1 = (torch.rand(10, 3, 100, 100) * 2) - 1
         >>> img2 = (torch.rand(10, 3, 100, 100) * 2) - 1
         >>> learned_perceptual_image_patch_similarity(img1, img2, net_type='squeeze')
-        tensor(0.1008, grad_fn=<DivBackward0>)
+        tensor(0.1008)
 
     """
     net = _NoTrainLpips(net=net_type).to(device=img1.device, dtype=img1.dtype)