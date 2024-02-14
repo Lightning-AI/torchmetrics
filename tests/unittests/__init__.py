d2cd8a7b992106846e72840e44f33d5d29e32439
Jirka <jirka.borovec@seznam.cz>
2024-02-14 23:26:21 +0100

    _cachier_hash_func

--- tests/unittests/__init__.py
+++ tests/unittests/__init__.py
@@ -1,4 +1,7 @@
+import functools
+import hashlib
 import os.path
+import pickle
 from typing import NamedTuple
 
 import numpy
@@ -25,7 +28,24 @@ _PATH_UNITTESTS = os.path.dirname(__file__)
 _PATH_ALL_TESTS = os.path.dirname(_PATH_UNITTESTS)
 _PATH_TEST_CACHE = os.getenv("PYTEST_REFERENCE_CACHE", os.path.join(_PATH_ALL_TESTS, "_reference-cache"))
 
-_reference_cachier = cachier(cache_dir=_PATH_TEST_CACHE)
+
+def _convert_list_to_tuple(val):
+    return tuple(_convert_list_to_tuple(x) for x in val) if isinstance(val, list) else val
+
+
+def _cachier_hash_func(args, kwds):
+    # convert lists to tuples to make them hashable
+    args = _convert_list_to_tuple(args)
+    kwds = {k: _convert_list_to_tuple(v) for k, v in kwds.items()}
+    # use pickle to hash the arguments
+    key = functools._make_key(args, kwds, typed=True)
+    hash_sha256 = hashlib.sha256()
+    for item in key:
+        hash_sha256.update(pickle.dumps(item))
+    return hash_sha256.hexdigest()
+
+
+_reference_cachier = cachier(cache_dir=_PATH_TEST_CACHE, hash_func=_cachier_hash_func)
 
 
 if torch.cuda.is_available():
