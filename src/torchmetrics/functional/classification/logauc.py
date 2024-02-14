0efa0dd755ad6b8b634a0e04c3b9b42981177535
Nicki Skafte <skaftenicki@gmail.com>
2024-02-14 08:43:18 +0100

    something is working

--- src/torchmetrics/functional/classification/logauc.py
+++ src/torchmetrics/functional/classification/logauc.py
@@ -11,21 +11,25 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from torchmetrics.functional.classification.roc import binary_roc, multiclass_roc, multilabel_roc
+from typing import List, Optional, Tuple, Union
 
-
-from torchmetrics.utilities.compute import _auc_compute_without_check
-from typing import Union, Optional, Tuple, List
-from torch import Tensor
-import torch
 import numpy as np
+import torch
+from torch import Tensor
 from typing_extensions import Literal
+
+from torchmetrics.functional.classification.roc import binary_roc, multiclass_roc, multilabel_roc
+from torchmetrics.utilities.compute import _auc_compute_without_check
 
 
 def _interpolate(newpoints: Tensor, x: Tensor, y: Tensor) -> Tensor:
     """Interpolate the points (x, y) to the newpoints using linear interpolation."""
     # TODO: Add native torch implementation
-    return torch.from_numpy(np.interp(newpoints.numpy(), x.numpy(), y.numpy()))
+    device = newpoints.device
+    newpoints_n = newpoints.cpu().numpy()
+    x_n = x.cpu().numpy()
+    y_n = y.cpu().numpy()
+    return torch.from_numpy(np.interp(newpoints_n, x_n, y_n)).to(device)
 
 
 def _binary_logauc_compute(
@@ -33,20 +37,21 @@ def _binary_logauc_compute(
     tpr: Tensor,
     fpr_range: Tuple[float, float] = (0.001, 0.1),
 ) -> Tensor:
-    tpr = torch.cat([tpr, _interpolate(torch.tensor(fpr_range), fpr, tpr)]).sort().values
-    fpr = torch.cat([fpr, torch.tensor(fpr_range)]).sort().values
+    fpr_range = torch.tensor(fpr_range).to(fpr.device)
+    tpr = torch.cat([tpr, _interpolate(fpr_range, fpr, tpr)]).sort().values
+    fpr = torch.cat([fpr, fpr_range]).sort().values
 
     log_fpr = torch.log10(fpr)
     bounds = torch.log10(torch.tensor(fpr_range))
 
-    lower_bound_idx = torch.where(log_fpr == bounds[0])[0]
-    upper_bound_idx = torch.where(log_fpr == bounds[1])[0]
+    lower_bound_idx = torch.where(log_fpr == bounds[0])[0][-1]
+    upper_bound_idx = torch.where(log_fpr == bounds[1])[0][-1]
 
-    trimmed_fpr = fpr[lower_bound_idx:upper_bound_idx+1]
-    trimmed_tpr = tpr[lower_bound_idx:upper_bound_idx+1]
+    trimmed_log_fpr = log_fpr[lower_bound_idx : upper_bound_idx + 1]
+    trimmed_tpr = tpr[lower_bound_idx : upper_bound_idx + 1]
 
     # compute area and rescale it to the range of fpr
-    area = _auc_compute_without_check(trimmed_fpr, trimmed_tpr) / (bounds[1] - bounds[0])
+    area = _auc_compute_without_check(trimmed_log_fpr, trimmed_tpr, 1.0) / (bounds[1] - bounds[0])
     return area
 
 
@@ -62,10 +67,8 @@ def binary_logauc(
     return _binary_logauc_compute(fpr, tpr, fpr_range)
 
 
-def _multiclass_logauc_compute(
-
-) -> Tensor:
-    pass   
+def _multiclass_logauc_compute() -> Tensor:
+    pass
 
 
 def multiclass_logauc(
@@ -81,9 +84,8 @@ def multiclass_logauc(
     fpr, tpr, _ = multiclass_roc(preds, target, num_classes, thresholds, average, ignore_index, validate_args)
     return _multiclass_logauc_compute(fpr, tpr, fpr_range)
 
-def _multilabel_logauc_compute(
 
-) -> Tensor:
+def _multilabel_logauc_compute() -> Tensor:
     pass
 
 
@@ -94,12 +96,11 @@ def multilabel_logauc(
     thresholds: Optional[Union[int, List[float], Tensor]] = None,
     fpr_range: Tuple[float, float] = (0.001, 0.1),
     ignore_index: Optional[int] = None,
-    validate_args: bool = True,   
-) -> Tensor
+    validate_args: bool = True,
+) -> Tensor:
     fpr, tpr, _ = multilabel_roc(preds, target, num_labels, thresholds, ignore_index, validate_args)
     return _multilabel_logauc_compute(fpr, tpr, fpr_range)
 
-def logauc(
-    
-) -> Tensor:
+
+def logauc() -> Tensor:
     pass
--- tests/unittests/classification/test_logauc.py
+++ tests/unittests/classification/test_logauc.py
@@ -11,5 +11,118 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from functools import partial
+
+import pytest
+from scipy.special import expit as sigmoid
+from scipy.special import softmax
 from tdc.evaluator import range_logAUC
-from torchmetrics.functional.classification.logauc import binary_locauc, multiclass_locauc, multilabel_locauc
+from torchmetrics.functional.classification.logauc import binary_logauc, multiclass_logauc, multilabel_logauc
+
+from unittests import NUM_CLASSES
+from unittests.classification.inputs import _binary_cases, _multiclass_cases, _multilabel_cases
+from unittests.helpers import seed_all
+from unittests.helpers.testers import MetricTester, inject_ignore_index, remove_ignore_index
+
+seed_all(42)
+
+
+def _binary_compare_implementation(preds, target, fpr_range):
+    preds = preds.flatten().numpy()
+    target = target.flatten().numpy()
+    if not ((preds > 0) & (preds < 1)).all():
+        preds = sigmoid(preds)
+    return range_logAUC(target, preds, FPR_range=fpr_range)
+
+
+@pytest.mark.parametrize("inputs", (_binary_cases[1], _binary_cases[2], _binary_cases[4], _binary_cases[5]))
+class TestBinaryAUROC(MetricTester):
+    """Test class for `BinaryAUROC` metric."""
+
+    # @pytest.mark.parametrize("max_fpr", [None, 0.8, 0.5])
+    # @pytest.mark.parametrize("ignore_index", [None, -1])
+    # @pytest.mark.parametrize("ddp", [pytest.param(True, marks=pytest.mark.DDP), False])
+    # def test_binary_auroc(self, inputs, ddp, max_fpr, ignore_index):
+    #     """Test class implementation of metric."""
+    #     preds, target = inputs
+    #     if ignore_index is not None:
+    #         target = inject_ignore_index(target, ignore_index)
+    #     self.run_class_metric_test(
+    #         ddp=ddp,
+    #         preds=preds,
+    #         target=target,
+    #         metric_class=BinaryAUROC,
+    #         reference_metric=partial(_sklearn_auroc_binary, max_fpr=max_fpr, ignore_index=ignore_index),
+    #         metric_args={
+    #             "max_fpr": max_fpr,
+    #             "thresholds": None,
+    #             "ignore_index": ignore_index,
+    #         },
+    #     )
+
+    @pytest.mark.parametrize("fpr_range", [(0.001, 0.1), (0.01, 0.1), (0.1, 0.2)])
+    def test_binary_auroc_functional(self, inputs, fpr_range):
+        """Test functional implementation of metric."""
+        preds, target = inputs
+        self.run_functional_metric_test(
+            preds=preds,
+            target=target,
+            metric_functional=binary_logauc,
+            reference_metric=partial(_binary_compare_implementation, fpr_range=fpr_range),
+            metric_args={
+                "fpr_range": fpr_range,
+                "thresholds": None,
+            },
+        )
+
+    # def test_binary_auroc_differentiability(self, inputs):
+    #     """Test the differentiability of the metric, according to its `is_differentiable` attribute."""
+    #     preds, target = inputs
+    #     self.run_differentiability_test(
+    #         preds=preds,
+    #         target=target,
+    #         metric_module=BinaryAUROC,
+    #         metric_functional=binary_auroc,
+    #         metric_args={"thresholds": None},
+    #     )
+
+    # @pytest.mark.parametrize("dtype", [torch.half, torch.double])
+    # def test_binary_auroc_dtype_cpu(self, inputs, dtype):
+    #     """Test dtype support of the metric on CPU."""
+    #     preds, target = inputs
+
+    #     if (preds < 0).any() and dtype == torch.half:
+    #         pytest.xfail(reason="torch.sigmoid in metric does not support cpu + half precision")
+    #     self.run_precision_test_cpu(
+    #         preds=preds,
+    #         target=target,
+    #         metric_module=BinaryAUROC,
+    #         metric_functional=binary_auroc,
+    #         metric_args={"thresholds": None},
+    #         dtype=dtype,
+    #     )
+
+    # @pytest.mark.skipif(not torch.cuda.is_available(), reason="test requires cuda")
+    # @pytest.mark.parametrize("dtype", [torch.half, torch.double])
+    # def test_binary_auroc_dtype_gpu(self, inputs, dtype):
+    #     """Test dtype support of the metric on GPU."""
+    #     preds, target = inputs
+    #     self.run_precision_test_gpu(
+    #         preds=preds,
+    #         target=target,
+    #         metric_module=BinaryAUROC,
+    #         metric_functional=binary_auroc,
+    #         metric_args={"thresholds": None},
+    #         dtype=dtype,
+    #     )
+
+    # @pytest.mark.parametrize("threshold_fn", [lambda x: x, lambda x: x.numpy().tolist()], ids=["as tensor", "as list"])
+    # def test_binary_auroc_threshold_arg(self, inputs, threshold_fn):
+    #     """Test that different types of `thresholds` argument lead to same result."""
+    #     preds, target = inputs
+
+    #     for pred, true in zip(preds, target):
+    #         _, _, t = binary_roc(pred, true, thresholds=None)
+    #         ap1 = binary_auroc(pred, true, thresholds=None)
+    #         ap2 = binary_auroc(pred, true, thresholds=threshold_fn(t.flip(0)))
+    #         assert torch.allclose(ap1, ap2)
