c9f8bc1161606107770bef0aead864be23882300
Jirka <jirka.borovec@seznam.cz>
2024-02-27 01:16:31 +0100

    simplify

--- tests/unittests/image/test_scc.py
+++ tests/unittests/image/test_scc.py
@@ -36,7 +36,7 @@ _inputs = [
 _kernels = [torch.tensor([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])]
 
 
-def _reference_scc(preds, target, hp_filter, window_size, reduction):
+def _reference_sewar_scc(preds, target, hp_filter, window_size, reduction):
     """Wrapper around reference implementation of scc from sewar."""
     preds = torch.movedim(preds, 1, -1)
     target = torch.movedim(target, 1, -1)
@@ -52,10 +52,10 @@ def _reference_scc(preds, target, hp_filter, window_size, reduction):
     return None
 
 
-def _reference_scc_simple(preds, target):
+def _reference_sewar_scc_simple(preds, target):
     """Reference implementation of SCC from sewar."""
     hp_filter = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
-    return _reference_scc(preds, target, hp_filter, window_size=8, reduction="mean")
+    return _reference_sewar_scc(preds, target, hp_filter, window_size=8, reduction="mean")
 
 
 @pytest.mark.parametrize("preds, target", [(i.preds, i.target) for i in _inputs])
@@ -68,7 +68,7 @@ class TestSpatialCorrelationCoefficient(MetricTester):
     def test_scc(self, preds, target, ddp):
         """Test SpatialCorrelationCoefficient class usage."""
         self.run_class_metric_test(
-            ddp, preds, target, metric_class=SpatialCorrelationCoefficient, reference_metric=_reference_scc_simple
+            ddp, preds, target, metric_class=SpatialCorrelationCoefficient, reference_metric=_reference_sewar_scc_simple
         )
 
     @pytest.mark.parametrize("hp_filter", _kernels)
@@ -76,14 +76,11 @@ class TestSpatialCorrelationCoefficient(MetricTester):
     @pytest.mark.parametrize("reduction", ["mean", "none"])
     def test_scc_functional(self, preds, target, hp_filter, window_size, reduction):
         """Test SpatialCorrelationCoefficient functional usage."""
+        kwargs = {"hp_filter": hp_filter, "window_size": window_size, "reduction": reduction}
         self.run_functional_metric_test(
             preds,
             target,
             metric_functional=spatial_correlation_coefficient,
-            reference_metric=partial(_reference_scc, hp_filter=hp_filter, window_size=window_size, reduction=reduction),
-            metric_args={
-                "hp_filter": hp_filter,
-                "window_size": window_size,
-                "reduction": reduction,
-            },
+            reference_metric=partial(_reference_sewar_scc, **kwargs),
+            metric_args=kwargs,
         )
