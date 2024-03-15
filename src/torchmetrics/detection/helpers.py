b6fe6bb5cf35a337a00b8755c09189e789a22c23
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2024-03-15 22:59:27 +0100

    typing: mark line ignores instead of ignore whole files (#2452)

--- src/torchmetrics/detection/helpers.py
+++ src/torchmetrics/detection/helpers.py
@@ -87,13 +87,15 @@ def _fix_empty_tensors(boxes: Tensor) -> Tensor:
     return boxes
 
 
-def _validate_iou_type_arg(iou_type: Union[Literal["bbox", "segm"], Tuple[str]] = "bbox") -> Tuple[str]:
+def _validate_iou_type_arg(
+    iou_type: Union[Literal["bbox", "segm"], Tuple[str]] = "bbox",
+) -> Tuple[str]:
     """Validate that iou type argument is correct."""
     allowed_iou_types = ("segm", "bbox")
     if isinstance(iou_type, str):
         iou_type = (iou_type,)
     if any(tp not in allowed_iou_types for tp in iou_type):
         raise ValueError(
-            f"Expected argument `iou_type` to be one of {allowed_iou_types} or a list of, but got {iou_type}"
+            f"Expected argument `iou_type` to be one of {allowed_iou_types} or a tuple of, but got {iou_type}"
         )
     return iou_type
