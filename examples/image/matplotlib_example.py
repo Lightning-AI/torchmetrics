7f3792cf05d78b5fa3cda5ac71a39ec0b7b26242
baskrahmer <baskrahmer@gmail.com>
2024-05-13 14:51:48 +0200

    Add animation example

---
+++ examples/image/matplotlib_example.py
@@ -0,0 +1,25 @@
+"""
+Example using animations
+===============================
+"""
+# %%
+# Example of making sliders in Matplotlib (source: https://sphinx-gallery.github.io/stable/auto_examples/plot_8_animations.html)
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+# Adapted from
+# https://matplotlib.org/gallery/animation/basic_example.html
+
+
+def _update_line(num):
+    line.set_data(data[..., :num])
+    return (line,)
+
+
+fig, ax = plt.subplots()
+data = np.random.RandomState(0).rand(2, 25)
+(line,) = ax.plot([], [], "r-")
+ax.set(xlim=(0, 1), ylim=(0, 1))
+ani = animation.FuncAnimation(fig, _update_line, 25, interval=100, blit=True)
