834f2a19c2928fef7ac84b97f9bdc621c7c81097
Jirka <jirka.borovec@seznam.cz>
2024-05-23 08:25:36 +0200

    import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 import glob
-import inspect
 import os
 import re
 import shutil
