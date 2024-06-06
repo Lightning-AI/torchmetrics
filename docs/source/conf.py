27a301a572cf6bd459ba3f5aed7d71f424adbbd1
Bas Krahmer <baskrahmer@gmail.com>
2024-06-06 13:35:52 +0200

    Add custom css gallery thumbnails (#2589)
    * Add custom css gallery thumbnails
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Nicki Skafte Detlefsen <skaftenicki@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -219,6 +219,7 @@ html_logo = "_static/images/logo.svg"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
