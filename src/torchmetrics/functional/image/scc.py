c8dd9e4fa3487af5579a8debcdd4034e688decf6
RatishT <r.k.thakoersingh@student.tudelft.nl>
2024-06-26 08:56:24 +0200

    Fix pylint warning `redefined-outer-name`

--- src/torchmetrics/functional/image/scc.py
+++ src/torchmetrics/functional/image/scc.py
@@ -73,19 +73,19 @@ def _scc_update(preds: Tensor, target: Tensor, hp_filter: Tensor, window_size: i
     return preds, target, hp_filter
 
 
-def _symmetric_reflect_pad_2d(input_img: Tensor, pad: Union[int, Tuple[int, ...]]) -> Tensor:
+def _symmetric_reflect_pad_2d(input_img: Tensor, padding: Union[int, Tuple[int, ...]]) -> Tensor:
     """Applies symmetric padding to the 2D image tensor input using ``reflect`` mode (d c b a | a b c d | d c b a)."""
-    if isinstance(pad, int):
-        pad = (pad, pad, pad, pad)
-    if len(pad) != 4:
-        raise ValueError(f"Expected padding to have length 4, but got {len(pad)}")
+    if isinstance(padding, int):
+        padding = (padding, padding, padding, padding)
+    if len(padding) != 4:
+        raise ValueError(f"Expected padding to have length 4, but got {len(padding)}")
 
-    left_pad = input_img[:, :, :, 0 : pad[0]].flip(dims=[3])
-    right_pad = input_img[:, :, :, -pad[1] :].flip(dims=[3])
+    left_pad = input_img[:, :, :, 0 : padding[0]].flip(dims=[3])
+    right_pad = input_img[:, :, :, -padding[1] :].flip(dims=[3])
     padded = torch.cat([left_pad, input_img, right_pad], dim=3)
 
-    top_pad = padded[:, :, 0 : pad[2], :].flip(dims=[2])
-    bottom_pad = padded[:, :, -pad[3] :, :].flip(dims=[2])
+    top_pad = padded[:, :, 0 : padding[2], :].flip(dims=[2])
+    bottom_pad = padded[:, :, -padding[3] :, :].flip(dims=[2])
     return torch.cat([top_pad, padded, bottom_pad], dim=2)
 
 
