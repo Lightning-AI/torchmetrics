b6a3447bc1d353b7cd3c49abfd00f37f07b6cc38
ndrwrbgs <10776890+ndrwrbgs@users.noreply.github.com>
2024-07-28 00:56:33 -0500

    Stay as close to the original version as possible

--- src/torchmetrics/utilities/compute.py
+++ src/torchmetrics/utilities/compute.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Optional, Tuple, Union
+from typing import Optional, Tuple
 
 import torch
 from torch import Tensor
@@ -43,7 +43,7 @@ def _safe_xlogy(x: Tensor, y: Tensor) -> Tensor:
     return res
 
 
-def _safe_divide(num: Tensor, denom: Tensor, zero_division: Union[float, Tensor] = 0.0) -> Tensor:
+def _safe_divide(num: Tensor, denom: Tensor, zero_division: float = 0.0) -> Tensor:
     """Safe division, by preventing division by zero.
 
     Function will cast to float if input is not already to secure backwards compatibility.
@@ -56,9 +56,9 @@ def _safe_divide(num: Tensor, denom: Tensor, zero_division: Union[float, Tensor]
     """
     num = num if num.is_floating_point() else num.float()
     denom = denom if denom.is_floating_point() else denom.float()
-    zero_division = torch.as_tensor(zero_division, dtype=num.dtype).to(num.device, non_blocking=True)
-
-    return torch.where(denom != 0, num / denom, zero_division)
+    zero_division_tensor = torch.tensor(zero_division, dtype=num.dtype).to(num.device, non_blocking=True)
+    
+    return torch.where(denom != 0, num / denom, zero_division_tensor)
 
 
 def _adjust_weights_safe_divide(
