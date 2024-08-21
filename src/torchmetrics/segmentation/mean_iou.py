ef88d970671d0345d72b99b8150085aa24277f48
Vitaliy Kinakh <vitaliy.kinakh@unige.ch>
2024-08-21 16:32:00 +0200

    Fix MeanIoU (#2558)
    - use sum reduce function for score
    - add state `num_batches` to keep number of processed batches
    - add increment of `num_batches` in every `update` call
    - in `compute` return sum of scores divided by number of processed batches

--- src/torchmetrics/segmentation/mean_iou.py
+++ src/torchmetrics/segmentation/mean_iou.py
@@ -110,7 +110,8 @@ class MeanIoU(Metric):
         self.input_format = input_format
 
         num_classes = num_classes - 1 if not include_background else num_classes
-        self.add_state("score", default=torch.zeros(num_classes if per_class else 1), dist_reduce_fx="mean")
+        self.add_state("score", default=torch.zeros(num_classes if per_class else 1), dist_reduce_fx="sum")
+        self.add_state("num_batches", default=torch.tensor(0), dist_reduce_fx="sum")
 
     def update(self, preds: Tensor, target: Tensor) -> None:
         """Update the state with the new data."""
@@ -119,10 +120,11 @@ class MeanIoU(Metric):
         )
         score = _mean_iou_compute(intersection, union, per_class=self.per_class)
         self.score += score.mean(0) if self.per_class else score.mean()
+        self.num_batches += 1
 
     def compute(self) -> Tensor:
         """Update the state with the new data."""
-        return self.score  # / self.num_batches
+        return self.score / self.num_batches
 
     def plot(self, val: Union[Tensor, Sequence[Tensor], None] = None, ax: Optional[_AX_TYPE] = None) -> _PLOT_OUT_TYPE:
         """Plot a single or multiple values from the metric.
