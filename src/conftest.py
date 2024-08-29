b94c7a2f3042c987574866d9eff35b29ef6fb536
jirka <jirka.borovec@seznam.cz>
2024-08-29 02:11:52 +0200

    lint

--- src/conftest.py
+++ src/conftest.py
@@ -1,16 +1,22 @@
+from pathlib import Path
+from typing import Optional
+
 from lightning_utilities.core.imports import package_available
 
 testing = package_available("pytest") and package_available("doctest")
 
 if testing:
     import doctest
+
     import pytest
 
     MANUAL_SEED = doctest.register_optionflag("MANUAL_SEED")
 
     @pytest.fixture(autouse=True)
-    def reset_random_seed(seed: int = 42):
+    def reset_random_seed(seed: int = 42) -> None:  # noqa: PT004
+        """Reset the random seed before running each doctest."""
         import random
+
         import numpy as np
         import torch
 
@@ -19,13 +25,18 @@ if testing:
         torch.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
 
-    def pytest_collect_file(parent, path):
-        if path.ext == ".py":
-            return DoctestModule.from_parent(parent, fspath=path)
-
     class DoctestModule(pytest.Module):
-        def collect(self):
+        """A custom module class that augments collected doctests with the reset_random_seed fixture."""
+
+        def collect(self) -> GeneratorExit:
+            """Augment collected doctests with the reset_random_seed fixture."""
             for item in super().collect():
                 if isinstance(item, pytest.DoctestItem):
                     item.add_marker(pytest.mark.usefixtures("reset_random_seed"))
                 yield item
+
+    def pytest_collect_file(parent: Path, path: Path) -> Optional[DoctestModule]:
+        """Collect doctests and add the reset_random_seed fixture."""
+        if path.ext == ".py":
+            return DoctestModule.from_parent(parent, fspath=path)
+        return None
