51567b25cba7668b3f538c6a4ea6d695aab41953
jirka <jirka.borovec@seznam.cz>
2024-08-30 22:15:35 +0200

    hamming

--- src/torchmetrics/audio/__init__.py
+++ src/torchmetrics/audio/__init__.py
@@ -28,8 +28,16 @@ from torchmetrics.utilities.imports import (
     _ONNXRUNTIME_AVAILABLE,
     _PESQ_AVAILABLE,
     _PYSTOI_AVAILABLE,
+    _SCIPI_AVAILABLE,
     _TORCHAUDIO_AVAILABLE,
 )
+
+if _SCIPI_AVAILABLE:
+    import scipy.signal
+
+    # back compatibility patch due to SMRMpy using scipy.signal.hamming
+    if not hasattr(scipy.signal, "hamming"):
+        scipy.signal.hamming = scipy.signal.windows.hamming
 
 __all__ = [
     "PermutationInvariantTraining",
