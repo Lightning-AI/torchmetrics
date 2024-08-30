089d644980486affd1593ac7a01c49fbe06c0449
jirka <jirka.borovec@seznam.cz>
2024-08-30 20:36:41 +0200

    atol = 1e-7

--- tests/unittests/classification/test_f_beta.py
+++ tests/unittests/classification/test_f_beta.py
@@ -86,6 +86,7 @@ def _reference_sklearn_fbeta_score_binary(preds, target, sk_fn, ignore_index, mu
 )
 class TestBinaryFBetaScore(MetricTester):
     """Test class for `BinaryFBetaScore` metric."""
+    atol = 1e-7
 
     @pytest.mark.parametrize("ignore_index", [None, -1])
     @pytest.mark.parametrize("multidim_average", ["global", "samplewise"])
@@ -258,6 +259,7 @@ def _reference_sklearn_fbeta_score_multiclass(
 )
 class TestMulticlassFBetaScore(MetricTester):
     """Test class for `MulticlassFBetaScore` metric."""
+    atol = 1e-7
 
     @pytest.mark.parametrize("ignore_index", [None, 0, -1])
     @pytest.mark.parametrize("multidim_average", ["global", "samplewise"])
@@ -515,6 +517,7 @@ def _reference_sklearn_fbeta_score_multilabel(
 )
 class TestMultilabelFBetaScore(MetricTester):
     """Test class for `MultilabelFBetaScore` metric."""
+    atol = 1e-7
 
     @pytest.mark.parametrize("ddp", [pytest.param(True, marks=pytest.mark.DDP), False])
     @pytest.mark.parametrize("ignore_index", [None, -1])
