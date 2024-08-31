Fix: Corrected MulticlassRecall macro average calculation when ignore_index is specified

src/torchmetrics/utilities/compute.py
@@ -61,7 +61,7 @@ def _safe_divide(num: Tensor, denom: Tensor, zero_division: float = 0.0) -> Tens
 
 
 def _adjust_weights_safe_divide(
-    score: Tensor, average: Optional[str], multilabel: bool, tp: Tensor, fp: Tensor, fn: Tensor, top_k: int = 1
+    score: Tensor, average: Optional[str], multilabel: bool, tp: Tensor, fp: Tensor, fn: Tensor, ignore_index:Optional[int] = None, top_k: int = 1
 ) -> Tensor:
     if average is None or average == "none":
         return score
@@ -71,6 +71,10 @@ def _adjust_weights_safe_divide(
         weights = torch.ones_like(score)
         if not multilabel:
             weights[tp + fp + fn == 0 if top_k == 1 else tp + fn == 0] = 0.0
+            
+    if ignore_index is not None and 0 <= ignore_index < len(score):
+        weights[ignore_index] = 0.0
+    
     return _safe_divide(weights * score, weights.sum(-1, keepdim=True)).sum(-1)
 
 