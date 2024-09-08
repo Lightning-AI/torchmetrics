e0ea066d5044a8fc564385e41e4e769e8b48fab3
Nicki Skafte <skaftenicki@gmail.com>
2024-09-08 14:41:08 +0200

    deprecate old implementation

--- tests/unittests/test_deprecated.py
+++ tests/unittests/test_deprecated.py
@@ -1,5 +1,7 @@
 import pytest
 import torch
+from torchmetrics.classification import Dice
+from torchmetrics.functional.classification import dice
 from torchmetrics.functional.regression import kl_divergence
 from torchmetrics.regression import KLDivergence
 
@@ -14,3 +16,15 @@ def test_deprecated_kl_divergence_input_order():
 
     with pytest.deprecated_call(match="The input order and naming in metric `KLDivergence` is set to be deprecated.*"):
         KLDivergence()
+
+
+def test_deprecated_dice_from_classification():
+    """Ensure that the deprecated `dice` metric from classification raises a warning."""
+    preds = torch.randn(10, 2)
+    target = torch.randint(0, 2, (10,))
+
+    with pytest.deprecated_call(match="The `dice` metrics is being deprecated from the classification subpackage.*"):
+        dice(preds, target)
+
+    with pytest.deprecated_call(match="The `dice` metrics is being deprecated from the classification subpackage.*"):
+        Dice()
