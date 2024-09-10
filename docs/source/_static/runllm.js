03aebe732833fd687f0b817b25a1228163134a92
Wei Chen <wei@aqueducthq.com>
2024-09-10 16:36:45 -0700

    Update name

--- docs/source/_static/runllm.js
+++ docs/source/_static/runllm.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
     script.src = "https://widget.runllm.com";
 
     script.setAttribute("runllm-keyboard-shortcut", "Mod+j"); // cmd-j or ctrl-j to open the widget.
-    script.setAttribute("runllm-name", "Torch Metrics");
+    script.setAttribute("runllm-name", "TorchMetrics");
     script.setAttribute("runllm-position", "BOTTOM_RIGHT");
     script.setAttribute("runllm-assistant-id", "244");
 
