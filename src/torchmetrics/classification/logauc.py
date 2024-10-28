ad054d105f80808cc8a256536625037f2787bffe
Nicki Skafte <skaftenicki@gmail.com>
2024-10-28 10:50:31 +0100

    plotting methods

--- src/torchmetrics/classification/logauc.py
+++ src/torchmetrics/classification/logauc.py
@@ -242,6 +242,48 @@ class MulticlassLogAUC(MulticlassROC):
         fpr, tpr, _ = super().compute()
         return _reduce_logauc(fpr, tpr, fpr_range=self.fpr_range, average=self.average2)
 
+    def plot(  # type: ignore[override]
+        self, val: Optional[Union[Tensor, Sequence[Tensor]]] = None, ax: Optional[_AX_TYPE] = None
+    ) -> _PLOT_OUT_TYPE:
+        """Plot a single or multiple values from the metric.
+
+        Args:
+            val: Either a single result from calling `metric.forward` or `metric.compute` or a list of these results.
+                If no value is provided, will automatically call `metric.compute` and plot that result.
+            ax: An matplotlib axis object. If provided will add plot to that axis
+
+        Returns:
+            Figure and Axes object
+
+        Raises:
+            ModuleNotFoundError:
+                If `matplotlib` is not installed
+
+        .. plot::
+            :scale: 75
+
+            >>> # Example plotting a single
+            >>> import torch
+            >>> from torchmetrics.classification import MulticlassLogAUC
+            >>> metric = MulticlassLogAUC(num_classes=3)
+            >>> metric.update(torch.randn(20, 3), torch.randint(3,(20,)))
+            >>> fig_, ax_ = metric.plot()
+
+        .. plot::
+            :scale: 75
+
+            >>> # Example plotting multiple values
+            >>> import torch
+            >>> from torchmetrics.classification import MulticlassLogAUC
+            >>> metric = MulticlassLogAUC(num_classes=3)
+            >>> values = [ ]
+            >>> for _ in range(10):
+            ...     values.append(metric(torch.randn(20, 3), torch.randint(3, (20,))))
+            >>> fig_, ax_ = metric.plot(values)
+
+        """
+        return self._plot(val, ax)
+
 
 class MultilabelLogAUC(MultilabelROC):
     r"""Compute the `Log AUC`_ score for multiclass classification tasks.
@@ -355,6 +397,48 @@ class MultilabelLogAUC(MultilabelROC):
         """Computes the log AUC score."""
         fpr, tpr, _ = super().compute()
         return _reduce_logauc(fpr, tpr, fpr_range=self.fpr_range, average=self.average2)
+
+    def plot(  # type: ignore[override]
+        self, val: Optional[Union[Tensor, Sequence[Tensor]]] = None, ax: Optional[_AX_TYPE] = None
+    ) -> _PLOT_OUT_TYPE:
+        """Plot a single or multiple values from the metric.
+
+        Args:
+            val: Either a single result from calling `metric.forward` or `metric.compute` or a list of these results.
+                If no value is provided, will automatically call `metric.compute` and plot that result.
+            ax: An matplotlib axis object. If provided will add plot to that axis
+
+        Returns:
+            Figure and Axes object
+
+        Raises:
+            ModuleNotFoundError:
+                If `matplotlib` is not installed
+
+        .. plot::
+            :scale: 75
+
+            >>> # Example plotting a single
+            >>> import torch
+            >>> from torchmetrics.classification import MultilabelLogAUC
+            >>> metric = MultilabelLogAUC(num_labels=3)
+            >>> metric.update(torch.rand(20,3), torch.randint(2, (20,3)))
+            >>> fig_, ax_ = metric.plot()
+
+        .. plot::
+            :scale: 75
+
+            >>> # Example plotting multiple values
+            >>> import torch
+            >>> from torchmetrics.classification import MultilabelLogAUC
+            >>> metric = MultilabelLogAUC(num_labels=3)
+            >>> values = [ ]
+            >>> for _ in range(10):
+            ...     values.append(metric(torch.rand(20,3), torch.randint(2, (20,3))))
+            >>> fig_, ax_ = metric.plot(values)
+
+        """
+        return self._plot(val, ax)
 
 
 class LogAUC(_ClassificationTaskWrapper):
