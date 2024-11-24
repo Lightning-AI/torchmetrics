89a616ab3ad07ad31f850a96bd7e2f0ce7049dfa
meng song <mengsong@192.168.1.113>
2024-11-24 16:50:52 +0800

    unittest .

--- src/torchmetrics/functional/classification/precision_recall_curve.py
+++ src/torchmetrics/functional/classification/precision_recall_curve.py
@@ -182,9 +182,14 @@ def _binary_precision_recall_curve_format(
         preds = preds[idx]
         target = target[idx]
 
-    out_of_bounds = (preds < 0) | (preds > 1)
-    out_of_bounds = out_of_bounds.any()
-    preds = torch.where(out_of_bounds, preds.sigmoid(), preds)
+    # "sigmoid_cpu" not implemented for 'Half'
+    if preds.dtype != torch.float16 or preds.device != torch.device("cpu"):
+      out_of_bounds = (preds < 0) | (preds > 1)
+      out_of_bounds = out_of_bounds.any()
+      preds = torch.where(out_of_bounds, preds.sigmoid(), preds)
+    else:
+      if not torch.all((preds >= 0) * (preds <= 1)):
+          preds = preds.sigmoid()
 
     thresholds = _adjust_threshold_arg(thresholds, preds.device)
     return preds, target, thresholds
@@ -763,9 +768,14 @@ def _multilabel_precision_recall_curve_format(
     preds = preds.transpose(0, 1).reshape(num_labels, -1).T
     target = target.transpose(0, 1).reshape(num_labels, -1).T
 
-    out_of_bounds = (preds < 0) | (preds > 1)
-    out_of_bounds = out_of_bounds.any()
-    preds = torch.where(out_of_bounds, preds.sigmoid(), preds)
+    # "sigmoid_cpu" not implemented for 'Half'
+    if preds.dtype != torch.float16 or preds.device != torch.device("cpu"):
+      out_of_bounds = (preds < 0) | (preds > 1)
+      out_of_bounds = out_of_bounds.any()
+      preds = torch.where(out_of_bounds, preds.sigmoid(), preds)
+    else:
+      if not torch.all((preds >= 0) * (preds <= 1)):
+          preds = preds.sigmoid()
 
     thresholds = _adjust_threshold_arg(thresholds, preds.device)
     if ignore_index is not None and thresholds is not None:
