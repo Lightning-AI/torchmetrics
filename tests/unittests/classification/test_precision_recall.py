ddd5fb6e42a505eb748dab5bf2a37b984359b118
rittik9 <rittikpanda24@gmail.com>
2024-11-28 03:47:22 +0530

    fix test precision recall

--- tests/unittests/classification/test_precision_recall.py
+++ tests/unittests/classification/test_precision_recall.py
@@ -439,8 +439,10 @@ def test_top_k(
 
     result = expected_prec if metric_class.__name__ == "MulticlassPrecision" else expected_recall
 
-    assert torch.equal(class_metric.compute(), result)
-    assert torch.equal(metric_fn(preds, target, top_k=k, average=average, num_classes=3), result)
+    assert torch.allclose(class_metric.compute(), result, atol=1e-4, rtol=1e-4)
+    assert torch.allclose(
+        metric_fn(preds, target, top_k=k, average=average, num_classes=3), result, atol=1e-4, rtol=1e-4
+    )
 
 
 def _reference_sklearn_precision_recall_multilabel_global(preds, target, sk_fn, ignore_index, average, zero_division):
