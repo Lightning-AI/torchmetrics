99ba6b29d3dcf415a5ae7745fff5eed4cad443b8
Jirka B <j.borovec+github@gmail.com>
2024-12-19 00:56:09 +0900

    tests

--- src/torchmetrics/utilities/data.py
+++ src/torchmetrics/utilities/data.py
@@ -207,11 +207,12 @@ def _bincount(x: Tensor, minlength: Optional[int] = None) -> Tensor:
 
 
 def _cumsum(x: Tensor, dim: Optional[int] = 0, dtype: Optional[torch.dtype] = None) -> Tensor:
+    """Implement custom cumulative summation for Torch versions which does not support it natively."""
     if torch.are_deterministic_algorithms_enabled() and x.is_cuda and x.is_floating_point() and sys.platform != "win32":
         rank_zero_warn(
-            "You are trying to use a metric in deterministic mode on GPU that uses `torch.cumsum`, which is currently "
-            "not supported. The tensor will be copied to the CPU memory to compute it and then copied back to GPU. "
-            "Expect some slowdowns.",
+            "You are trying to use a metric in deterministic mode on GPU that uses `torch.cumsum`, which is currently"
+            " not supported. The tensor will be copied to the CPU memory to compute it and then copied back to GPU."
+            " Expect some slowdowns.",
             TorchMetricsUserWarning,
         )
         return x.cpu().cumsum(dim=dim, dtype=dtype).to(x.device)
