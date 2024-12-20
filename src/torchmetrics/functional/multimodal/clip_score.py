a33d69a45ea455a38d02d462c6c4c40391b83097
rittik9 <rittikpanda24@gmail.com>
2024-12-21 01:20:18 +0530

    refactor

--- src/torchmetrics/functional/multimodal/clip_score.py
+++ src/torchmetrics/functional/multimodal/clip_score.py
@@ -157,14 +157,8 @@ def _clip_score_update(
     source_modality = _detect_modality(source)
     target_modality = _detect_modality(target)
 
-    if source_modality == "image":
-        source_data = _process_image_data(source)
-    else:
-        source_data = _process_text_data(source)
-    if target_modality == "image":
-        target_data = _process_image_data(target)
-    else:
-        target_data = _process_text_data(target)
+    source_data = _process_image_data(source) if source_modality == "image" else _process_text_data(source) 
+    target_data = _process_image_data(target) if target_modality == "image" else _process_text_data(target)
 
     # Verify matching lengths
     if len(source_data) != len(target_data):
