6034875e6102c7b1d62befdaecfb41c38e7ff983
Nicki Skafte <skaftenicki@gmail.com>
2025-03-17 11:37:41 +0100

    modular interface

--- src/torchmetrics/classification/__init__.py
+++ src/torchmetrics/classification/__init__.py
@@ -32,6 +32,7 @@ from torchmetrics.classification.confusion_matrix import (
     MultilabelConfusionMatrix,
 )
 from torchmetrics.classification.dice import Dice
+from torchmetrics.classification.eer import EER, BinaryEER, MulticlassEER, MultilabelEER
 from torchmetrics.classification.exact_match import ExactMatch, MulticlassExactMatch, MultilabelExactMatch
 from torchmetrics.classification.f_beta import (
     BinaryF1Score,
@@ -131,6 +132,7 @@ from torchmetrics.classification.stat_scores import (
 
 __all__ = [
     "AUROC",
+    "EER",
     "ROC",
     "Accuracy",
     "AveragePrecision",
@@ -140,6 +142,7 @@ __all__ = [
     "BinaryCalibrationError",
     "BinaryCohenKappa",
     "BinaryConfusionMatrix",
+    "BinaryEER",
     "BinaryF1Score",
     "BinaryFBetaScore",
     "BinaryFairness",
@@ -178,6 +181,7 @@ __all__ = [
     "MulticlassCalibrationError",
     "MulticlassCohenKappa",
     "MulticlassConfusionMatrix",
+    "MulticlassEER",
     "MulticlassExactMatch",
     "MulticlassF1Score",
     "MulticlassFBetaScore",
@@ -202,6 +206,7 @@ __all__ = [
     "MultilabelAveragePrecision",
     "MultilabelConfusionMatrix",
     "MultilabelCoverageError",
+    "MultilabelEER",
     "MultilabelExactMatch",
     "MultilabelF1Score",
     "MultilabelFBetaScore",
