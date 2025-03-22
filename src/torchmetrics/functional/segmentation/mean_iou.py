b6d40e2db234db2bfd3308179aca76a35947d5b3
Isalia20 <irakli.salia854@gmail.com>
2025-03-23 03:07:18 +0400

    normalize whitespaces

--- src/torchmetrics/functional/segmentation/mean_iou.py
+++ src/torchmetrics/functional/segmentation/mean_iou.py
@@ -153,10 +153,10 @@ def mean_iou(
         >>> mean_iou(preds, target, num_classes=5, input_format = "index")
         tensor([0.3617, 0.3128, 0.3047, 0.3499])
         >>> mean_iou(preds, target, num_classes=5, per_class=True, input_format="index")
-        tensor([[0.3617, 0.3617, -1.000, -1.000, -1.000],
-                [0.3128, 0.3128, -1.000, -1.000, -1.000],
-                [0.2727, 0.3366, -1.000, -1.000, -1.000],
-                [0.3756, 0.3242, -1.000, -1.000, -1.000]]))
+        tensor([[ 0.3617,  0.3617, -1.0000, -1.0000, -1.0000],
+                [ 0.3128,  0.3128, -1.0000, -1.0000, -1.0000],
+                [ 0.2727,  0.3366, -1.0000, -1.0000, -1.0000],
+                [ 0.3756,  0.3242, -1.0000, -1.0000, -1.0000]])
 
     """
     _mean_iou_validate_args(num_classes, include_background, per_class, input_format)
