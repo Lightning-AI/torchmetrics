8f977b2a31b85e18143cd31af030bb5f059f5b35
Irakli Salia <65120973+Isalia20@users.noreply.github.com>
2025-03-24 19:39:56 +0400

    Update src/torchmetrics/detection/iou.py
    Co-authored-by: Nicki Skafte Detlefsen <skaftenicki@gmail.com>

--- src/torchmetrics/detection/iou.py
+++ src/torchmetrics/detection/iou.py
@@ -20,6 +20,7 @@ from torch import Tensor
 from torchmetrics.detection.helpers import _fix_empty_tensors, _input_validator
 from torchmetrics.functional.detection.iou import _iou_compute, _iou_update
 from torchmetrics.metric import Metric
+from torchmetrics.utilities.data import dim_zero_cat
 from torchmetrics.utilities.imports import _MATPLOTLIB_AVAILABLE, _TORCHVISION_AVAILABLE
 from torchmetrics.utilities.plot import _AX_TYPE, _PLOT_OUT_TYPE
 
