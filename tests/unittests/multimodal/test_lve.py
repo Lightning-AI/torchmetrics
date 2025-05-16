e2e1801a773bdb336fb7b692cbfea72685e8ef93
rittik9 <rittikpanda24@gmail.com>
2025-05-16 21:40:39 +0000

    fix unittests

--- tests/unittests/multimodal/test_lve.py
+++ tests/unittests/multimodal/test_lve.py
@@ -34,17 +34,18 @@ class _InputVertices(NamedTuple):
     vertices_gt: Tensor
 
 
-_random_input = _InputVertices(
-    vertices_pred=torch.randn(10, 100, 3),
-    vertices_gt=torch.randn(10, 100, 3),
-)
+def _generate_vertices(batch_size: int = 1) -> _InputVertices:
+    """Generate random vertices for testing."""
+    return _InputVertices(
+        vertices_pred=torch.randn(batch_size, 10, 100, 3),  
+        vertices_gt=torch.randn(batch_size, 10, 100, 3)   
+    )
 
 
 def _reference_lip_vertex_error(vertices_pred, vertices_gt, mouth_map):
     """Reference implementation for Lip Vertex Error.
 
     This uses a numpy implementation for validation.
-
     """
     min_frames = min(vertices_pred.shape[0], vertices_gt.shape[0])
     vertices_pred = vertices_pred[:min_frames].numpy()
@@ -57,7 +58,6 @@ def _reference_lip_vertex_error(vertices_pred, vertices_gt, mouth_map):
     lve = np.mean(l2_dis_mouth_max)
     return torch.tensor(lve)
 
-
 class TestLipVertexError(MetricTester):
     """Test class for `LipVertexError` metric."""
 
@@ -65,7 +65,7 @@ class TestLipVertexError(MetricTester):
     def test_lip_vertex_error(self, ddp):
         """Test class implementation of metric."""
         mouth_map = [0, 1, 2, 3, 4]
-        vertices_pred, vertices_gt = _random_input
+        vertices_pred, vertices_gt = _generate_vertices(batch_size=4)  
 
         self.run_class_metric_test(
             ddp=ddp,
@@ -79,7 +79,7 @@ class TestLipVertexError(MetricTester):
     def test_lip_vertex_error_functional(self):
         """Test functional implementation of metric."""
         mouth_map = [0, 1, 2, 3, 4]
-        vertices_pred, vertices_gt = _random_input
+        vertices_pred, vertices_gt = _generate_vertices(batch_size=4)  
 
         self.run_functional_metric_test(
             preds=vertices_pred,
@@ -92,7 +92,7 @@ class TestLipVertexError(MetricTester):
     def test_lip_vertex_error_differentiability(self):
         """Test the differentiability of the metric, according to its `is_differentiable` attribute."""
         mouth_map = [0, 1, 2, 3, 4]
-        vertices_pred, vertices_gt = _random_input
+        vertices_pred, vertices_gt = _generate_vertices(batch_size=4)  
 
         self.run_differentiability_test(
             preds=vertices_pred,
@@ -135,8 +135,8 @@ class TestLipVertexError(MetricTester):
     def test_plot_method(self):
         """Test the plot method of LipVertexError."""
         metric = LipVertexError(mouth_map=[0, 1, 2, 3, 4])
-        vertices_pred, vertices_gt = _random_input
-        metric.update(vertices_pred, vertices_gt)
+        vertices_pred, vertices_gt = _generate_vertices()  
+        metric.update(vertices_pred[0], vertices_gt[0])  
         fig, ax = metric.plot()
         assert isinstance(fig, plt.Figure)
         assert isinstance(ax, matplotlib.axes.Axes)
