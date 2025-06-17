897960f50182fabca6c617ccc394ac73577cd137
Jirka B <j.borovec+github@gmail.com>
2025-06-17 12:45:38 +0200

    _TRANSFORMERS_BETWEEN_4_51_AND_4_52

--- tests/unittests/utilities/test_plot.py
+++ tests/unittests/utilities/test_plot.py
@@ -21,7 +21,7 @@ import numpy as np
 import pytest
 import torch
 from torch import tensor
-from unittests._helpers import _TORCH_LESS_THAN_2_1, _TRANSFORMERS_EQUAL_4_52
+from unittests._helpers import _TORCH_LESS_THAN_2_1, _TRANSFORMERS_BETWEEN_4_51_AND_4_52
 
 from torchmetrics import MetricCollection
 from torchmetrics.aggregation import MaxMetric, MeanMetric, MinMetric, SumMetric
@@ -649,7 +649,7 @@ _text_input_4 = lambda: [["there is a cat on the mat", "a cat is on the mat"]]
                 RuntimeError,
                 # todo: if the transformers compatibility issue present in next feature release,
                 #  consider bumping also torch min versions in the metrics implementations
-                condition=_TORCH_LESS_THAN_2_1 and _TRANSFORMERS_EQUAL_4_52,
+                condition=_TORCH_LESS_THAN_2_1 and _TRANSFORMERS_BETWEEN_4_51_AND_4_52,
                 reason="could be due to torch compatibility issues with transformers",
             ),
         ),
@@ -776,7 +776,7 @@ def test_plot_methods_special_image_metrics(metric_class, preds, target, index_0
     RuntimeError,
     # todo: if the transformers compatibility issue present in next feature release,
     #  consider bumping also torch min versions in the metrics implementations
-    condition=_TORCH_LESS_THAN_2_1 and _TRANSFORMERS_EQUAL_4_52,
+    condition=_TORCH_LESS_THAN_2_1 and _TRANSFORMERS_BETWEEN_4_51_AND_4_52,
     reason="could be due to torch compatibility issues with transformers",
 )
 def test_plot_methods_special_text_metrics():
