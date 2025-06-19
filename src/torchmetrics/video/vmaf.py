4c5d50799547b4360ab67676d879e468b4b17841
Jirka B <j.borovec+github@gmail.com>
2025-06-19 12:08:59 +0200

    torch.manual_seed(42), torch.cuda.manual_seed_all(42)

--- src/torchmetrics/video/vmaf.py
+++ src/torchmetrics/video/vmaf.py
@@ -87,8 +87,7 @@ class VideoMultiMethodAssessmentFusion(Metric):
     Example:
         >>> import torch
         >>> from torchmetrics.video import VideoMultiMethodAssessmentFusion
-        >>> torch.manual_seed(42)
-        >>> torch.cuda.manual_seed_all(42)
+        >>> _, _ = torch.manual_seed(42), torch.cuda.manual_seed_all(42)
         >>> # 2 videos, 3 channels, 10 frames, 32x32 resolution
         >>> preds = torch.rand(2, 3, 10, 32, 32, generator=torch.manual_seed(42))
         >>> target = torch.rand(2, 3, 10, 32, 32, generator=torch.manual_seed(43))
