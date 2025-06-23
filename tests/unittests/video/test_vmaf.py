7a956cdb0677a3cbb901319f6738489724d60b3f
Rittik Panda <rittikpanda24@gmail.com>
2025-06-24 02:39:09 +0530

    Test: address ordering results for DDP without features test in VMAF (#3144)

--- tests/unittests/video/test_vmaf.py
+++ tests/unittests/video/test_vmaf.py
@@ -108,6 +108,7 @@ class TestVMAF(MetricTester):
             metric_class=VideoMultiMethodAssessmentFusion,
             reference_metric=partial(_reference_vmaf_with_features if features else _reference_vmaf_no_features),
             metric_args={"features": features},
+            check_ddp_sorting=True,
         )
 
     def test_vmaf_functional(self, preds, target, features):
