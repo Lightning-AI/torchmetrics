fix: fix masks for detecting near-zero variance

src/torchmetrics/functional/regression/pearson.py
@@ -125,9 +125,9 @@ def _pearson_corrcoef_compute(
     if (
         (var_x < bound).any()
         or (var_y < bound).any()
-        or torch.isnan(var_x).any()
-        or torch.isnan(var_y).any()
-        or torch.isnan(corr_xy).any()
+        or ~torch.isfinite(var_x).any()
+        or ~torch.isfinite(var_y).any()
+        or ~torch.isfinite(corr_xy).any()
     ):
         rank_zero_warn(
             "The variance of predictions or target is close to zero. This can cause instability in Pearson correlation"
@@ -135,7 +135,9 @@ def _pearson_corrcoef_compute(
             f"larger dtype (currently using {var_x.dtype}). Setting the correlation coefficient to nan.",
             UserWarning,
         )
-    zero_var_mask = (var_x < bound) | (var_y < bound) | torch.isnan(var_x) | torch.isnan(var_y) | torch.isnan(corr_xy)
+    zero_var_mask = (
+        (var_x < bound) | (var_y < bound) | ~torch.isfinite(var_x) | ~torch.isfinite(var_y) | ~torch.isfinite(corr_xy)
+    )
     corrcoef = torch.full_like(corr_xy, float("nan"), device=corr_xy.device, dtype=corr_xy.dtype)
     valid_mask = ~zero_var_mask
     if valid_mask.any():