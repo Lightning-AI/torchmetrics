3020c166cb196346ba50034177647475b1f488d5
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2025-08-06 17:56:26 +0300

    tests: patch strange error for `transformers` on Windows (#3208)
    * patch strange error for `transformers` on Windows
    * _IS_WINDOWS
    
    (cherry picked from commit 565f5713eeca40f74ef714ec4da994353983a71b)

--- tests/unittests/_helpers/__init__.py
+++ tests/unittests/_helpers/__init__.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import random
+import sys
 
 import numpy
 import torch
@@ -31,7 +32,9 @@ def seed_all(seed):
 __all__ = ["seed_all", "skip_on_connection_issues", "skip_on_cuda_oom", "skip_on_running_out_of_memory"]
 
 
+_IS_WINDOWS = sys.platform.startswith("win32")
 _SKLEARN_GREATER_EQUAL_1_3 = RequirementCache("scikit-learn>=1.3.0")
 _SKLEARN_GREATER_EQUAL_1_7 = RequirementCache("scikit-learn>=1.7.0")
 _TORCH_LESS_THAN_2_1 = RequirementCache("torch<2.1.0")
-_TRANSFORMERS_RANGE_LT_4_50_LE_4_53 = RequirementCache("transformers>=4.50.0,<4.54.0")
+_TRANSFORMERS_RANGE_GE_4_50_LT_4_54 = RequirementCache("transformers>=4.50.0,<4.54.0")
+_TRANSFORMERS_GREATER_EQUAL_4_54 = RequirementCache("transformers>=4.54.0")
