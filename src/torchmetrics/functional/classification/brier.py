Fix bugs in metric and decomposition calculation
The mean Brier score and it's decomposition should satisfy the following equation Brier = Uncertainty - Resolution + Reliability. After inspecting the results on a toy test case, the Uncertainty was estimated wrongly with a negative sign, also the Brier score formula was slightly wrong (f-1)^2=f^2-2f+1 not (f-1)^2=f^2-2f causing it to also be negative. With these fixes the decomposition equation is satisfied.

src/torchmetrics/functional/classification/brier.py
@@ -77,7 +77,7 @@ def _brier_decomposition(
     dist_mean = confusion_matrix / (confusion_matrix.sum(dim=1, keepdim=True) + 1.0e-7)
 
     # Uncertainty: quadratic entropy of the average label distribution
-    uncertainty = -torch.sum(pbar**2)
+    uncertainty = torch.sum(pbar**2)
 
     # Resolution: expected quadratic divergence of predictive to mean
     resolution = (pbar.unsqueeze(1) - dist_mean) ** 2
@@ -115,7 +115,7 @@ def _mean_brier_score(labels: torch.Tensor, probabilities: torch.Tensor = None) 
 
     # Gather the probabilities corresponding to the true labels
     plabel = flat_probabilities[torch.arange(len(flat_labels)), flat_labels]
-    out = torch.sum(flat_probabilities**2, dim=-1) - 2 * plabel
+    out = torch.sum(flat_probabilities**2, dim=-1) - 2 * plabel + 1
 
     return out.view(labels.shape).mean()
 