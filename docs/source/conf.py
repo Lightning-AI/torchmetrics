c2a9504d7e6c35116870579b6a61f41f1ec9179c
bhimrazy <bhimrajyadav977@gmail.com>
2025-11-28 14:18:32 +0545

    fix link check

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -462,5 +462,6 @@ linkcheck_ignore = [
     "https://code.visualstudio.com",
     "https://code.visualstudio.com/.*",
     # Ignore Medium articles which return 403 to automated clients
-    # "https://jonathan-hui.medium.com/map-mean-average-precision-for-object-detection-45c121a31173",
+    # Mean-Average-Precision (mAP) and Mean-Average-Recall  (mAR)
+    "https://jonathan-hui.medium.com/map-mean-average-precision-for-object-detection-45c121a31173",
 ]
