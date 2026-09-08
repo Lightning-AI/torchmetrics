40f94b22aad4dcb3f93df35d6a35490403d60187
bhimrazy <bhimrajyadav977@gmail.com>
2026-09-08 14:08:46 +0545

    chore: fix _sync_dist comment typo and drop unused _TORCHMETRICS_GREATER_EQUAL_1_6

--- src/torchmetrics/utilities/imports.py
+++ src/torchmetrics/utilities/imports.py
@@ -20,7 +20,6 @@ from lightning_utilities.core.imports import RequirementCache
 
 _PYTHON_VERSION = f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
 _TORCH_GREATER_EQUAL_2_8 = RequirementCache("torch>=2.8.0")
-_TORCHMETRICS_GREATER_EQUAL_1_6 = RequirementCache("torchmetrics>=1.7.0")
 
 _NLTK_AVAILABLE = RequirementCache("nltk")
 _ROUGE_SCORE_AVAILABLE = RequirementCache("rouge_score")
